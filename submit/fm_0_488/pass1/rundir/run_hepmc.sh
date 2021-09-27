f70a5c62350e399476b7bc23bfd35438e9b432b9
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-09-27 12:08:45 -0400

    add pass1 for 0-4.88fm hijing

---
+++ submit/fm_0_488/pass1/rundir/run_hepmc.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+export HOME=/sphenix/u/${LOGNAME}
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.8
+
+echo running: run_hepmc.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+  cd $_CONDOR_SCRATCH_DIR
+  rsync -av /sphenix/u/sphnxpro/MDC1/submit/fm_0_488/pass1/rundir/* .
+else
+ echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of events
+# $2: hepmc input file
+# $3: output file
+# $4: no events to skip
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(hepmc file\): $2
+echo arg3 \(output file\): $3
+echo arg5 \(skip\): $4
+echo arg6 \(output dir\): $5
+echo running root.exe -q -b Fun4All_G4_Pass1.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
+root.exe -q -b  Fun4All_G4_Pass1.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
+echo "script done"
