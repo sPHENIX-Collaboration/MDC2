4d10b7c10aeec67d32d2a6076276d1ba96a637f4
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-09-27 12:11:44 -0400

    add pass2 for 0-4.88fm hijing

---
+++ submit/fm_0_488/pass2/rundir/run_pileup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+export HOME=/sphenix/u/${LOGNAME}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.2
+echo running: run_pileup.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+  cd $_CONDOR_SCRATCH_DIR
+  rsync -av /sphenix/u/sphnxpro/MDC1/submit/fm_0_488/pass2/rundir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+    getinputfiles.pl -filelist $3
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+else
+ echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of output events
+# $2: input file
+# $3: background listfile
+# $4: output directory
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(output events\) : $1
+echo arg2 \(input file\): $2
+echo arg3 \(background listfile\): $3
+echo arg4 \(output dir\): $4
+echo running root.exe -q -b Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\"\)
+root.exe -q -b  Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\"\)
+echo "script done"
