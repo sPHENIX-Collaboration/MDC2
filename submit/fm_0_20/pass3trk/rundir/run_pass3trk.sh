8ba668f8471838140e2caa2ae626db264fe83757
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-09-28 16:26:51 -0400

    fix typo in path (MDC1 -> MDC2)

--- submit/fm_0_20/pass3trk/rundir/run_pass3trk.sh
+++ submit/fm_0_20/pass3trk/rundir/run_pass3trk.sh
@@ -9,7 +9,7 @@ echo running: run_pass3trk.sh $*
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
 then
     cd $_CONDOR_SCRATCH_DIR
-    rsync -av /sphenix/u/sphnxpro/MDC1/submit/fm_0_20/pass3trk/rundir/* .
+    rsync -av /sphenix/u/sphnxpro/MDC2/submit/fm_0_20/pass3trk/rundir/* .
     getinputfiles.pl $2
     if [ $? -ne 0 ]
     then
