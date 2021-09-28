fb25cb61433246ad393b4424938f86454f3c7c69
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-09-28 12:16:57 -0400

    pass3trk rundir for 0-20fm

---
+++ submit/fm_0_20/pass3trk/rundir/run_pass3trk.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+export HOME=/sphenix/u/${LOGNAME}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.8
+echo running: run_pass3trk.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av /sphenix/u/sphnxpro/MDC2/submit/fm_0_20/pass2/rundir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of output events
+# $2: input file
+# $3: output file
+# $4: output directory
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(output events\) : $1
+echo arg2 \(input file\): $2
+echo arg3 \(output file\): $3
+echo arg4 \(output dir\): $4
+echo running root.exe -q -b Fun4All_G4_sPHENIX.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+root.exe -q -b  Fun4All_G4_sPHENIX.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+echo "script done"
