986e6ba70930824d8943952afc4ed032b7f53f08
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-09-27 23:27:12 -0400

    add HF_pp200_signal pass1 condor

---
+++ submit/HF_pp200_signal/pass1/condor/run_all.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Long;
+
+my $test;
+my $incremental;
+GetOptions("test"=>\$test, "increment"=>\$incremental);
+if ($#ARGV < 1)
+{
+    print "usage: run_all.pl <number of jobs> <\"CharmD0\", \"BottomD0\" or \"MinBias\" production>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $quarkfilter = $ARGV[1];
+if ($quarkfilter  ne "CharmD0" && $quarkfilter  ne "BottomD0" && $quarkfilter  ne "MinBias")
+{
+    print "second argument has to be either Charm, Bottom or MinBias\n";
+    exit(1);
+}
+my $runnumber = 2;
+my $events = 1000;
+open(F,"outdir.txt");
+my $outdir=<F>;
+chomp  $outdir;
+close(F);
+mkpath($outdir);
+my $localdir=`pwd`;
+chomp $localdir;
+my $logdir = sprintf("%s/log",$localdir);
+my $nsubmit = 0;
+my $njob = 0;
+for (my $isub = 0; $isub < $maxsubmit; $isub++)
+{
+    my $jobfile = sprintf("%s/condor-%s-%010d-%05d.job",$logdir,$quarkfilter,$runnumber,$njob);
+    while (-f $jobfile)
+    {
+	$njob++;
+	$jobfile = sprintf("%s/condor-%s-%010d-%05d.job",$logdir,$quarkfilter,$runnumber,$njob);
+    }
+    print "using jobfile $jobfile\n";
+    my $upperfilter = uc $quarkfilter;
+    my $outfile = sprintf("DST_HF_%s_pythia8-%010d-%05d.root",$upperfilter,$runnumber,$njob);
+    my $fulloutfile = sprintf("%s/%s",$outdir,$outfile);
+    print "out: $fulloutfile\n";
+    if (! -f $fulloutfile)
+    {
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	system("perl run_condor.pl $events $quarkfilter $outdir $outfile $runnumber $njob $tstflag");
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    exit(0);
+	}
+    }
+    else
+    {
+	print "output file already exists\n";
+    }
+}
---
+++ submit/HF_pp200_signal/pass1/condor/run_condor.pl
@@ -0,0 +1,72 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+GetOptions("test"=>\$test);
+if ($#ARGV < 3)
+{
+    print "usage: run_condor.pl <events> <quarkfilter> <outdir> <outfile> <skip> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+else
+{
+    print "running condor\n";
+}
+my $localdir=`pwd`;
+chomp $localdir;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_hfprod.sh",$rundir);
+my $nevents = $ARGV[0];
+my $quarkfilter = $ARGV[1];
+my $dstoutdir = $ARGV[2];
+my $dstoutfile = $ARGV[3];
+my $runnumber = $ARGV[4];
+my $sequence = $ARGV[5];
+my $suffix = sprintf("%010d-%05d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log",$localdir);
+mkpath($logdir);
+my $condorlogdir = sprintf("/tmp/HF_pp200_signal");
+mkpath($condorlogdir);
+my $jobfile = sprintf("%s/condor-%s-%s.job",$logdir,$quarkfilter,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor-%s-%s.log",$condorlogdir,$quarkfilter,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor-%s-%s.err",$logdir,$quarkfilter,$suffix);
+my $outfile = sprintf("%s/condor-%s-%s.out",$logdir,$quarkfilter,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $quarkfilter $dstoutfile $dstoutdir\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+print F "accounting_group = group_sphenix.prod\n";
+print F "request_memory = 4GB\n";
+print F "Priority 	= 20\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+if (defined $test)
+{
+    print "would submit $jobfile\n";
+}
+else
+{
+    system("condor_submit $jobfile");
+}
