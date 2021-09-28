a1bfa25726107c002da0264394618d52ca639075
Chris Pinkenburg <pinkenburg@bnl.gov>
2021-09-28 15:46:15 -0400

    add truth assoc container with other truth output to truth file

--- submit/fm_0_20/pass3trk/rundir/run_pass3trk.sh
+++ submit/fm_0_20/pass3trk/rundir/run_pass3trk.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/bash
+
 export HOME=/sphenix/u/${LOGNAME}
+
 source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc1.8
+
 echo running: run_pass3trk.sh $*
 
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
 then
     cd $_CONDOR_SCRATCH_DIR
-    rsync -av /sphenix/u/sphnxpro/MDC2/submit/fm_0_20/pass2/rundir/* .
+    rsync -av /sphenix/u/sphnxpro/MDC1/submit/fm_0_20/pass3trk/rundir/* .
     getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+    getinputfiles.pl $3
     if [ $? -ne 0 ]
     then
 	echo error from getinputfiles.pl $2, exiting
@@ -18,17 +27,17 @@ else
 fi
 
 # arguments 
-# $1: number of output events
-# $2: input file
-# $3: output file
-# $4: output directory
+# $1: number of events
+# $2: track g4hits input file
+# $3: truth g4hits input file
+# $4: output dir
 
 echo 'here comes your environment'
 printenv
-echo arg1 \(output events\) : $1
-echo arg2 \(input file\): $2
-echo arg3 \(output file\): $3
+echo arg1 \(events\) : $1
+echo arg2 \(track g4hits file\): $2
+echo arg3 \(truth g4hits file\): $3
 echo arg4 \(output dir\): $4
-echo running root.exe -q -b Fun4All_G4_sPHENIX.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
-root.exe -q -b  Fun4All_G4_sPHENIX.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+echo running root.exe -q -b Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\"\)
+root.exe -q -b  Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\"\)
 echo "script done"
