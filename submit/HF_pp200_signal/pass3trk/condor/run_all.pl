a44e2386d2992cd8a1563be07a756c79f1327c05
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-10-01 12:24:12 -0400

    Add pass3trk for pythia d0

---
+++ submit/HF_pp200_signal/pass3trk/condor/run_all.pl
@@ -0,0 +1,133 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber = 2;
+my $test;
+my $incremental;
+GetOptions("test"=>\$test, "increment"=>\$incremental);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+my $maxsubmit = $ARGV[0];
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+mkpath($outdir);
+
+
+my %outfiletype = ();
+$outfiletype{"DST_TRKR_CLUSTER"} = 1;
+$outfiletype{"DST_TRUTH"} = 1;
+
+my %trkhash = ();
+my %truthhash = ();
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_G4HIT' and filename like '%pythia8_d0%' and runnumber = $runnumber order by filename") || die $DBI::error;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::error;
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like '%pythia8_d0%'and runnumber = $runnumber");
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::error;
+my $ncal = $getfiles->rows;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $trkhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+$gettruthfiles->execute() || die $DBI::error;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    $truthhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$gettruthfiles->finish();
+#print "input files: $ncal, truth: $ntruth\n";
+foreach my $segment (sort keys %trkhash)
+{
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $trkhash{$segment};
+    print "found $lfn\n";
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+        my $foundall = 1;
+	foreach my $type (sort keys %outfiletype)
+	{
+            my $lfn =  sprintf("%s_pythia8_d0-%010d-%05d.root",$type,$runnumber,$segment);
+	    $chkfile->execute($lfn);
+	    if ($chkfile->rows > 0)
+	    {
+		next;
+	    }
+	    else
+	    {
+		$foundall = 0;
+		last;
+	    }
+	}
+	if ($foundall == 1)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %d %d %s", $outevents, $lfn, $truthhash{sprintf("%05d",$segment)}, $outdir, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    exit(0);
+	}
+    }
+}
+
+$chkfile->finish();
+$dbh->disconnect;
---
+++ submit/HF_pp200_signal/pass3trk/condor/run_condor.pl
@@ -0,0 +1,69 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+GetOptions("test"=>\$test);
+if ($#ARGV < 3)
+{
+    print "usage: run_condor.pl <events> <trk infile> <truth infile> <outdir> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass3trk.sh",$rundir);
+my $nevents = $ARGV[0];
+my $infile0 = $ARGV[1];
+my $infile1 = $ARGV[2];
+my $dstoutdir = $ARGV[3];
+my $runnumber = $ARGV[4];
+my $sequence = $ARGV[5];
+my $suffix = sprintf("%010d-%05d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log",$localdir);
+mkpath($logdir);
+my $condorlogdir = sprintf("/tmp/pythia8_d0/pass3trk");
+mkpath($condorlogdir);
+my $jobfile = sprintf("%s/condor-%s.job",$logdir,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor-%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor-%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor-%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile0 $infile1 $dstoutdir\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+print F "accounting_group = group_sphenix.prod\n";
+print F "request_memory = 3GB\n";
+print F "Priority 	= 26\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+if (defined $test)
+{
+    print "would submit $jobfile\n";
+}
+else
+{
+    system("condor_submit $jobfile");
+}
