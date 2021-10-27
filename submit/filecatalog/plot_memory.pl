e508de62792bbe7640916cc61db75f166457cdc4
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-10-26 22:33:58 -0400

    Add time saving options to rerun existing file, grep for type in case of multiple condor logs in one dir if needed

--- submit/filecatalog/plot_memory.pl
+++ submit/filecatalog/plot_memory.pl
@@ -5,19 +5,22 @@ use warnings;
 use Getopt::Long;
 
 my $count;
+my $runremlist;
+my $filetype;
+GetOptions("cnt:i"=>\$count,"f:s"=>\$runremlist, "type:s"=>\$filetype);
 
-GetOptions("cnt:i"=>\$count);
-
-if ($#ARGV < 0)
+if ($#ARGV < 0 && !defined $runremlist)
 {
     print "usage: plot_memory.pl <condor logdir>\n";
     print "-cnt : max number of condor logs to analyze\n";
+    print "-f : analyze previous listfile\n";
+    print "-type: part of filename to grep for (default all logs in condor dir\n";
     exit(1);
 }
 
 my $logdir = $ARGV[0]; 
 
-if (! -d $logdir)
+if (defined $logdir && ! -d $logdir)
 {
     print "$logdir does not exist\n";
     exit(2);
@@ -25,11 +28,21 @@ if (! -d $logdir)
 
 #my $cmd = sprintf("cat %s/*.log | grep 'Memory (MB)          :' | awk '{print \$4}' | sort -n > memory",$logdir);
 
-my $cmd = sprintf("find %s/ -name '*.log' |",$logdir);
 
-my $runremlist = "memory.list";
+if (! defined $runremlist)
+{
+    $runremlist = "memory.list";
+    my $cmd;
+    if (defined $filetype)
+    {
+	$cmd = sprintf("find %s/ -name '*%s*.log' |",$logdir,$filetype);
+    }
+    else
+    {
+	$cmd = sprintf("find %s/ -name '*.log' |",$logdir);
+    }
 
-if (-f $runremlist)
+    if (-f $runremlist)
 {
     unlink $runremlist;
 }
@@ -55,8 +68,9 @@ while (my $file = <F>)
 }
 close(F);
 close(F2);
+}
 
-$cmd = sprintf("root.exe plotmem.C\\(\\\"memory.list\\\"\\)");
+my $cmd = sprintf("root.exe plotmem.C\\(\\\"%s\\\"\\)",$runremlist);
 
 print "$cmd\n";
 
