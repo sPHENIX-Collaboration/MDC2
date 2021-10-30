a4ea8d268897062bea3fb169bdf957cdce62e79d
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-10-30 13:34:30 -0400

    Add concurrency limit of 1000

--- submit/HF_pp200_signal/pass2/condor/run_condor.pl
+++ submit/HF_pp200_signal/pass2/condor/run_condor.pl
@@ -57,6 +57,7 @@ print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
 print F "accounting_group = group_sphenix.prod\n";
 print F "request_memory = 2GB\n";
 print F "Priority 	= 26\n";
+print F "concurrency_limits = PHENIX_1000\n";
 print F "job_lease_duration = 3600\n";
 print F "Queue 1\n";
 close(F);
