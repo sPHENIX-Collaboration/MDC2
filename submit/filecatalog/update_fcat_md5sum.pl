627ba832db89e93edc595d0aa9b71abf55d305f8
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-11-01 09:19:43 -0400

    select dcache dir to run on

--- submit/filecatalog/update_fcat_md5sum.pl
+++ submit/filecatalog/update_fcat_md5sum.pl
@@ -9,10 +9,42 @@ use DBI;
 use Digest::MD5  qw(md5 md5_hex md5_base64);
 use Env;
 
+my $test;
+my $all;
+GetOptions("test"=>\$test, "all"=>\$all);
+
+if ($#ARGV < 0)
+{
+    print "usage: update_fcat_md5sum.pl <dcache dir>\n";
+    print "parameters:\n";
+    print "--all : check all files\n";
+    print "--test: run in test mode - no changes\n";
+    exit(1);
+}
+
 my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
 
-my $getfiles = $dbh->prepare("select lfn,full_file_path from files where md5 is null"); 
+my $getfiles;
+if (defined $all)
+{
+    $getfiles = $dbh->prepare("select lfn,full_file_path from files where md5 is null"); 
+}
+else
+{
+    my $dcachedir = $ARGV[0];
+    if (! -d $dcachedir)
+    {
+	print "could not find directory $dcachedir\n";
+	exit(1);
+    }
+    if ($dcachedir !~ /pnfs/)
+    {
+	print "only pnfs (dcache) dirs allowed\n";
+	exit(1);
+    }
+    $getfiles = $dbh->prepare("select lfn,full_file_path from files where md5 is null and full_file_path like '$dcachedir/%'");
+}
 
 my $updatemd5 = $dbh->prepare("update files set md5=? where lfn=?");
 $getfiles->execute()|| die $DBI::error;
@@ -27,7 +59,7 @@ while (my @res = $getfiles->fetchrow_array())
     }
     if (-f $fullfile)
     {
-	print "handling $lfn\n";
+	print "handling $fullfile\n";
 	my $copycmd = sprintf("env LD_LIBRARY_PATH=/usr/lib64:%s xrdcp --nopbar --retry 3 root://dcsphdoor02.rcf.bnl.gov:1095%s /tmp", $LD_LIBRARY_PATH, $fullfile);
 	system($copycmd);
 	my $localfile = sprintf("/tmp/%s",$lfn);
