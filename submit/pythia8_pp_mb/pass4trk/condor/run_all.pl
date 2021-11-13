c3b801cee479014d096f7b8a7f4721b2f8830ba3
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-11-13 12:06:11 -0500

    use pythia8_pp_mb for this type

--- submit/pythia8_pp_mb/pass4trk/condor/run_all.pl
+++ submit/pythia8_pp_mb/pass4trk/condor/run_all.pl
@@ -43,7 +43,7 @@ mkpath($outdir);
 
 my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
-my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like '%pythia8_mb_3MHz%' and runnumber = $runnumber order by filename") || die $DBI::error;
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like '%pythia8_pp_mb_3MHz%' and runnumber = $runnumber order by filename") || die $DBI::error;
 my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::error;
 my $nsubmit = 0;
 $getfiles->execute() || die $DBI::error;
@@ -54,7 +54,7 @@ while (my @res = $getfiles->fetchrow_array())
     {
 	my $runnumber = int($2);
 	my $segment = int($3);
-	my $outfilename = sprintf("DST_Tracks_pythia8_mb_3MHz-%010d-%05d.root",$runnumber,$segment);
+	my $outfilename = sprintf("DST_Tracks_pythia8_pp_mb_3MHz-%010d-%05d.root",$runnumber,$segment);
 	$chkfile->execute($outfilename);
 	if ($chkfile->rows > 0)
 	{
