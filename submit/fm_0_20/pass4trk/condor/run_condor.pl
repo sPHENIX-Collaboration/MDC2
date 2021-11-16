3fcd4f703fdcfabc3f0b6d264d34eb37e080ceb1
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-11-15 22:14:37 -0500

    request only 8GB of memory

--- submit/fm_0_20/pass4trk/condor/run_condor.pl
+++ submit/fm_0_20/pass4trk/condor/run_condor.pl
@@ -54,7 +54,7 @@ print F "Log  		= $condorlogfile\n";
 print F "Initialdir  	= $rundir\n";
 print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
 print F "accounting_group = group_sphenix.prod\n";
-print F "request_memory = 9GB\n";
+print F "request_memory = 8GB\n";
 print F "Priority 	= 25\n";
 print F "job_lease_duration = 3600\n";
 print F "Queue 1\n";
