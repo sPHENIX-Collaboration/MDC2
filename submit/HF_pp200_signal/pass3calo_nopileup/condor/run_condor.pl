4e48a1822f39c6e3e2549dd38ac0176e82a105fd
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-11-16 20:53:57 -0500

    Add pass3calo_nopileup for HF_pp200_signal

---
+++ submit/HF_pp200_signal/pass3calo_nopileup/condor/run_condor.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+GetOptions("test"=>\$test);
+if ($#ARGV < 3)
+{
+    print "usage: run_condor.pl <events>  <quarkfilter> <g4hit infile> <vertex infile> <outfile> <outdir> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass3calo.sh",$rundir);
+my $nevents = $ARGV[0];
+my $quarkfilter = $ARGV[1];
+my $infile0 = $ARGV[2];
+my $dstoutfile = $ARGV[3];
+my $dstoutdir = $ARGV[4];
+my $runnumber = $ARGV[5];
+my $sequence = $ARGV[6];
+my $suffix = sprintf("_%s-%010d-%05d",$quarkfilter,$runnumber,$sequence);
+my $logdir = sprintf("%s/log",$localdir);
+mkpath($logdir);
+my $condorlogdir = sprintf("/tmp/HF_pp200_signal/pass3calo_nopileup");
+mkpath($condorlogdir);
+my $jobfile = sprintf("%s/condor%s.job",$logdir,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile0 $dstoutfile $dstoutdir\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+print F "accounting_group = group_sphenix.prod\n";
+print F "request_memory = 1500MB\n";
+print F "Priority 	= 25\n";
+print F "concurrency_limits = PHENIX_100\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+if (defined $test)
+{
+    print "would submit $jobfile\n";
+}
+else
+{
+    system("condor_submit $jobfile");
+}
