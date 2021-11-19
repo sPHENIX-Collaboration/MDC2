08484865df3eea362a8d7e7d1a45c5c39d0b6ab3
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-11-18 19:32:08 -0500

    Add pass3_nopileup

---
+++ submit/HF_pp200_signal/pass3_nopileup/rundir/run_pass3_nopileup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+export HOME=/sphenix/u/${LOGNAME}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.2
+
+echo running: run_pass4trk.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av /sphenix/u/sphnxpro/MDC2/submit/HF_pp200_signal/pass4trk/rundir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+fi
+# arguments 
+# $1: number of events
+# $2: calo g4hits input file
+# $3: vertex input file
+# $4: output file
+# $5: output dir
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(trkr cluster file\): $2
+echo arg3 \(output file\): $3
+echo arg4 \(output dir\): $4
+echo running root.exe -q -b Fun4All_G4_Trkr.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+root.exe -q -b  Fun4All_G4_Trkr.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+echo "script done"
