f7bb1f06103fd97a697bdcd9f6c77cdf0fb571b4
sPHENIX Production Account <pinkenburg@bnl.gov>
2021-11-24 11:18:35 -0500

    rename DST_TRKR_CLUSTER to DST_TRKR_HIT

--- submit/fm_0_20/pass4trk/condor/run_all.pl
+++ submit/fm_0_20/pass4trk/condor/run_all.pl
@@ -43,7 +43,7 @@ mkpath($outdir);
 
 my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
-my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%' and runnumber = $runnumber order by filename") || die $DBI::error;
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_HIT' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%' and runnumber = $runnumber order by filename") || die $DBI::error;
 my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::error;
 my $nsubmit = 0;
 $getfiles->execute() || die $DBI::error;
