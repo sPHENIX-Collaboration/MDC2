d8b7e706982fe6dd837f87d4b48e5f51ba5022c4
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-01-14 17:44:42 -0500

    Add pass2 25kHz for 0-20fm

---
+++ submit/fm_0_20/pass2_25kHz/rundir/run_pileup.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+export USER="$(id -u -n)"
+export HOME=/sphenix/u/${USER}
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.3
+hostname
+echo running: run_pileup.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+  cd $_CONDOR_SCRATCH_DIR
+  rsync -av /sphenix/u/sphnxpro/MDC2/submit/fm_0_20/pass2_25kHz/rundir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+    getinputfiles.pl -filelist $3
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+else
+ echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of output events
+# $2: input file
+# $3: background listfile
+# $4: output directory
+# $5: run number
+# $6: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(output events\) : $1
+echo arg2 \(input file\): $2
+echo arg3 \(background listfile\): $3
+echo arg4 \(output dir\): $4
+echo arg5 \(runnumber\): $5
+echo arg6 \(sequence\): $6
+
+runnumber=$(printf "%010d" $5)
+sequence=$(printf "%05d" $6)
+filename=fm_0_20_pass2_25kHz
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+echo running root.exe -q -b Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\"\)
+prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b  Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\"\)
+
+mkdir -p /sphenix/user/sphnxpro/prmon/fm_0_20/pass2_25kHz
+
+rsync -av $txtfilename /sphenix/user/sphnxpro/prmon/fm_0_20/pass2_25kHz
+rsync -av $jsonfilename /sphenix/user/sphnxpro/prmon/fm_0_20/pass2_25kHz
+
+echo "script done"
