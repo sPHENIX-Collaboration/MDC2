4c9b13318d197344e62f2a6b5af0fb35d529e838
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-01-15 10:39:19 -0500

    save output to lustre

--- submit/pythia8_pp_mb/pass1/rundir/run_hepmc_pp.sh
+++ submit/pythia8_pp_mb/pass1/rundir/run_hepmc_pp.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
 export HOME=/sphenix/u/${LOGNAME}
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.3
@@ -19,19 +23,32 @@ fi
 # $2: hepmc input file
 # $3: output file
 # $4: no events to skip
+# $5: output dir
+# $6: runnumber
+# $7: sequence
 
 echo 'here comes your environment'
 printenv
 echo arg1 \(events\) : $1
 echo arg2 \(hepmc file\): $2
 echo arg3 \(output file\): $3
-echo arg5 \(skip\): $4
-echo arg6 \(output dir\): $5
-echo running root.exe -q -b Fun4All_G4_Pass1_pp.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
+echo arg4 \(skip\): $4
+echo arg5 \(output dir\): $5
+echo arg6 \(runnumber\): $6
+echo arg7 \(sequence\): $7
 
-prmon  --filename $3.txt --json-summary $3.json -- root.exe -q -b  Fun4All_G4_Pass1_pp.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
+runnumber=$(printf "%010d" $6)
+sequence=$(printf "%05d" $7)
+filename=pythia8_pp_mb_pass1
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+echo running prmon --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_Pass1_pp.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
+
+prmon --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b  Fun4All_G4_Pass1_pp.C\($1,\"$2\",\"$3\",\"\",$4,\"$5\"\)
 
 mkdir -p /sphenix/user/sphnxpro/prmon/pythia8_pp_mb/pass1
-rsync -av $3.txt /sphenix/user/sphnxpro/prmon/pythia8_pp_mb/pass1
-rsync -av $3.json /sphenix/user/sphnxpro/prmon/pythia8_pp_mb/pass1
+rsync -av $txtfilename /sphenix/user/sphnxpro/prmon/pythia8_pp_mb/pass1
+rsync -av $jsonfilename /sphenix/user/sphnxpro/prmon/pythia8_pp_mb/pass1
 echo "script done"
