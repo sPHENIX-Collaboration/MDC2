12b22e9d9d7a379d56d2693ef650cd68c7f0c390
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-01-20 19:06:19 -0500

    fix name of S3 storage

--- submit/fm_0_20/pass2/condor/run_all.pl
+++ submit/fm_0_20/pass2/condor/run_all.pl
@@ -41,7 +41,7 @@ chomp $outdir;
 if ($outdir =~ /lustre/)
 {
     my $storedir = $outdir;
-    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro\/dcsphst004/storage/;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
     my $makedircmd = sprintf("mcs3 mb %s",$storedir);
     system($makedircmd);
 }
