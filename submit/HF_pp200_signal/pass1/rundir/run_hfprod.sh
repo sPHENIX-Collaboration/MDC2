54d53214dbaabdd5f6e234f4a18d7c06cc70fe9b
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-01-21 20:37:37 -0500

    fix printout

--- submit/HF_pp200_signal/pass1/rundir/run_hfprod.sh
+++ submit/HF_pp200_signal/pass1/rundir/run_hfprod.sh
@@ -43,7 +43,7 @@ filename=HF_pp200_signal_pass1_$2
 txtfilename=${filename}-${runnumber}-${sequence}.txt
 jsonfilename=${filename}-${runnumber}-${sequence}.json
 
-echo running root.exe -q -b Fun4All_G4_HF_pp_signal.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+echo running prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_HF_pp_signal.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
 prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_HF_pp_signal.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
 
 mkdir -p /sphenix/user/sphnxpro/prmon/HF_pp200_signal/pass1_$2
