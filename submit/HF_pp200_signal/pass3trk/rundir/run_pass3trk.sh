00e11e922105ad9a31e6b9aa37b9d3c9c92bcb74
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-02-15 14:05:27 -0500

    add user name setting

--- submit/HF_pp200_signal/pass3trk/rundir/run_pass3trk.sh
+++ submit/HF_pp200_signal/pass3trk/rundir/run_pass3trk.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
 export HOME=/sphenix/u/${LOGNAME}
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.4
