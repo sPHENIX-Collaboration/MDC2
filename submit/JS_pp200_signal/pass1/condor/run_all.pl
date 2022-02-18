a0ccaa98bbe4a685f04c6ffcc68050dab0d0f4b9
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-02-18 16:38:37 -0500

    add jet .4 sample

---
+++ submit/JS_pp200_signal/pass1/condor/run_all.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Long;
+
+my $test;
+my $incremental;
+my $killexist;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "killexist" => \$killexist);
+if ($#ARGV < 1)
+{
+    print "usage: run_all.pl <number of jobs> <\"Jet04\" production>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--killexist : delete output file if it already exists (but no jobfile)\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $jettrigger = $ARGV[1];
+my $filetype="pythia8";
+if ($jettrigger  ne "Jet04")
+{
+    print "second argument has to be Jet04";
+    exit(1);
+}
+$filetype=sprintf("%s_%s",$filetype,$jettrigger);
+my $runnumber = 3;
+my $events = 1000;
+open(F,"outdir.txt");
+my $outdir=<F>;
+chomp  $outdir;
+close(F);
+$outdir = sprintf("%s/%s",$outdir,lc $jettrigger);
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+    mkpath($outdir);
+}
+my $localdir=`pwd`;
+chomp $localdir;
+my $logdir = sprintf("%s/log",$localdir);
+my $nsubmit = 0;
+my $njob = 0;
+for (my $isub = 0; $isub < $maxsubmit; $isub++)
+{
+    my $jobfile = sprintf("%s/condor_%s-%010d-%05d.job",$logdir,$jettrigger,$runnumber,$njob);
+    while (-f $jobfile)
+    {
+	$njob++;
+	$jobfile = sprintf("%s/condor_%s-%010d-%05d.job",$logdir,$jettrigger,$runnumber,$njob);
+    }
+    print "using jobfile $jobfile\n";
+    my $outfile = sprintf("G4Hits_%s-%010d-%05d.root",$filetype, $runnumber,$njob);
+    my $fulloutfile = sprintf("%s/%s",$outdir,$outfile);
+    print "out: $fulloutfile\n";
+    if (defined $killexist)
+    {
+	if (-f $fulloutfile)
+	{
+	    unlink  $fulloutfile;
+	}
+    }
+    if (! -f $fulloutfile)
+    {
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	system("perl run_condor.pl $events $jettrigger $outdir $outfile $runnumber $njob $tstflag");
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    exit(0);
+	}
+    }
+    else
+    {
+	print "output file already exists\n";
+	$njob++;
+    }
+}
---
+++ submit/JS_pp200_signal/pass1/condor/run_condor.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+GetOptions("test"=>\$test);
+if ($#ARGV < 3)
+{
+    print "usage: run_condor.pl <events> <jettrigger> <outdir> <outfile> <skip> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+else
+{
+    print "running condor\n";
+}
+my $localdir=`pwd`;
+chomp $localdir;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_hfprod.sh",$rundir);
+my $nevents = $ARGV[0];
+my $jettrigger = $ARGV[1];
+my $dstoutdir = $ARGV[2];
+my $dstoutfile = $ARGV[3];
+my $runnumber = $ARGV[4];
+my $sequence = $ARGV[5];
+my $suffix = sprintf("%010d-%05d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log",$localdir);
+mkpath($logdir);
+my $condorlogdir = sprintf("/tmp/HF_pp200_signal/pass1");
+mkpath($condorlogdir);
+my $jobfile = sprintf("%s/condor_%s-%s.job",$logdir,$jettrigger,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor_%s-%s.log",$condorlogdir,$jettrigger,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor_%s-%s.err",$logdir,$jettrigger,$suffix);
+my $outfile = sprintf("%s/condor_%s-%s.out",$logdir,$jettrigger,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $jettrigger $dstoutfile $dstoutdir $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+#print F "accounting_group = group_sphenix.prod\n";
+print F "accounting_group = group_sphenix.mdc2\n";
+print F "accounting_group_user = sphnxpro\n";
+print F "Requirements = (CPU_Type == \"mdc2\")\n";
+print F "request_memory = 4000MB\n";
+print F "Priority 	= 20\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+if (defined $test)
+{
+    print "would submit $jobfile\n";
+}
+else
+{
+    system("condor_submit $jobfile");
+}
