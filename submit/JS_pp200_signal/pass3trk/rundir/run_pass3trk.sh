e95e7179fe74a3320ce829e31a406890de7e62c4
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-02-22 20:23:15 -0500

    add jet pass3trk

---
+++ submit/JS_pp200_signal/pass3trk/rundir/run_pass3trk.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/bash
+
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+export HOME=/sphenix/u/${LOGNAME}
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.4
+
+echo running: run_pass3trk.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av /sphenix/u/sphnxpro/MDC2/submit/JS_pp200_signal/pass3trk/rundir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+    getinputfiles.pl $3
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $3, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of events
+# $2: track g4hits input file
+# $3: truth g4hits input file
+# $4: output dir
+# $5: jet trigger
+# $6: run number
+# $7: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(track g4hits file\): $2
+echo arg3 \(truth g4hits file\): $3
+echo arg4 \(output dir\): $4
+echo arg5 \(jettrigger\): $5
+echo arg6 \(runnumber\): $6
+echo arg7 \(sequence\): $7
+
+runnumber=$(printf "%010d" $6)
+sequence=$(printf "%05d" $7)
+filename=JS_pp200_signal_pass3trk_$5
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+
+echo running prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\",\"$5\"\)
+prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b  Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\",\"$5\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/JS_pp200_signal/pass3trk_$5
+if [ ! -d $rsyncdirname ]
+then
+mkdir -p $rsyncdirname
+fi
+
+rsync -av $txtfilename $rsyncdirname
+rsync -av $jsonfilename $rsyncdirname
+
+echo "script done"
