f95f2bf2225f925419ce6011b0837dd41d5aa59f
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-02-28 14:28:15 -0500

    Add JS Jet04 passes

--- cronjobs/autopilot.pl
+++ cronjobs/autopilot.pl
@@ -5,13 +5,23 @@ use warnings;
 use Cwd;
 
 my $submittopdir = "/sphenix/u/sphnxpro/MDC2/submit";
-my @submitdir = ("HF_pp200_signal/pass2/condor",
+my @submitdir = (
+"fm_0_20/pass2/condor",
+"fm_0_20/pass3trk/condor",
+"fm_0_20/pass3calo/condor",
+"fm_0_20/pass4trk/condor",
+"HF_pp200_signal/pass2/condor",
 "HF_pp200_signal/pass3trk/condor",
 "HF_pp200_signal/pass3calo/condor",
-"HF_pp200_signal/pass4trk/condor"
+"HF_pp200_signal/pass4trk/condor",
+"JS_pp200_signal/pass2/condor",
+"JS_pp200_signal/pass3trk/condor",
+"JS_pp200_signal/pass3calo/condor",
+"JS_pp200_signal/pass4trk/condor"
 );
 
-my @quarkfilters = ("Charm");
+my @quarkfilters = ("Bottom");
+my @jettriggers = ("Jet04");
 
 foreach my $subdir (@submitdir)
 {
@@ -30,6 +40,15 @@ foreach my $subdir (@submitdir)
     if ($newdir =~ /HF_pp200_signal/)
     {
 	foreach my $qf (@quarkfilters)
+	{
+	    my $submitcmd = sprintf("perl run_all.pl 2000 %s -inc",$qf);
+	    print "executing $submitcmd in $newdir\n";
+	    system($submitcmd);
+	}
+    }
+    elsif ($newdir =~ /JS_pp200_signal/)
+    {
+	foreach my $qf (@jettriggers)
 	{
 	    my $submitcmd = sprintf("perl run_all.pl 2000 %s -inc",$qf);
 	    print "executing $submitcmd in $newdir\n";
