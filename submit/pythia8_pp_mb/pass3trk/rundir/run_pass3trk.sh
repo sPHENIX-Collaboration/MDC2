c8690032711865b83383dfd4c9fd5f2b7dfbd642
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-03-03 12:47:05 -0500

    prepare for run=3

--- submit/pythia8_pp_mb/pass3trk/rundir/run_pass3trk.sh
+++ submit/pythia8_pp_mb/pass3trk/rundir/run_pass3trk.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
 
-export HOME=/sphenix/u/${LOGNAME}
-
-source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.2
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.5
 
 echo running: run_pass3trk.sh $*
 
@@ -31,6 +32,8 @@ fi
 # $2: track g4hits input file
 # $3: truth g4hits input file
 # $4: output dir
+# $5: run number
+# $6: sequence
 
 echo 'here comes your environment'
 printenv
@@ -38,6 +41,23 @@ echo arg1 \(events\) : $1
 echo arg2 \(track g4hits file\): $2
 echo arg3 \(truth g4hits file\): $3
 echo arg4 \(output dir\): $4
-echo running root.exe -q -b Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\"\)
-root.exe -q -b  Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\"\)
+echo arg5 \(runnumber\): $5
+echo arg6 \(sequence\): $6
+
+runnumber=$(printf "%010d" $5)
+sequence=$(printf "%05d" $6)
+filename=pythia8_pp_mb_pass3trk
+
+echo running running prmon --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\"\)
+running prmon --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b  Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$3\",\"\",\"\",0,\"$4\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/pythia8_pp_mb/pass3trk
+if [ ! -d $rsyncdirname ]
+then
+  mkdir -p $rsyncdirname
+fi
+
+rsync -av $txtfilename $rsyncdirname
+rsync -av $jsonfilename $rsyncdirname
+
 echo "script done"
