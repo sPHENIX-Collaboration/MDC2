662aa03e1145fcf1fa1901d09026176225b45d72
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-04-25 14:53:36 -0400

    propagate jet trigger

--- submit/fm_0_20/pass3_embed_jet/rundir/run_embed.sh
+++ submit/fm_0_20/pass3_embed_jet/rundir/run_embed.sh
@@ -60,8 +60,9 @@ fi
 # $5: truth g4hits embed file
 # $6: vertex embed file
 # $7: output directory
-# $8: run number
-# $9: sequence
+# $8: jettrigger
+# $9: run number
+# $10: sequence
 
 echo 'here comes your environment'
 printenv
@@ -72,12 +73,13 @@ echo arg4 \(track g4hits embed file\): $4
 echo arg5 \(truth g4hits embed file\): $5
 echo arg6 \(vertex embed file\): $6
 echo arg7 \(output dir\): $7
-echo arg8 \(runnumber\): $8
-echo arg9 \(sequence\): $9
+echo arg8 \(jettrigger\): $8
+echo arg9 \(runnumber\): $9
+echo arg10 \(sequence\): $10
 
-runnumber=$(printf "%010d" $8)
-sequence=$(printf "%05d" $9)
-filename=fm_0_20_pass3_embed_jet04
+runnumber=$(printf "%010d" $9)
+sequence=$(printf "%05d" $10)
+filename=fm_0_20_pass3_embed_$8
 
 txtfilename=${filename}-${runnumber}-${sequence}.txt
 jsonfilename=${filename}-${runnumber}-${sequence}.json
@@ -85,7 +87,7 @@ jsonfilename=${filename}-${runnumber}-${sequence}.json
 echo running root.exe -q -b Fun4All_G4_Embed.C\($1,\"$2\",\"$3\",\"$4\",\"$5\",\"$6\",0,\"$7\"\)
 prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b  Fun4All_G4_Embed.C\($1,\"$2\",\"$3\",\"$4\",\"$5\",\"$6\",0,\"$7\"\)
 
-rsyncdirname=/sphenix/user/sphnxpro/prmon/fm_0_20/pass3_embed_jet04
+rsyncdirname=/sphenix/user/sphnxpro/prmon/fm_0_20/pass3_embed_jet
 if [ ! -d $rsyncdirname ]
 then
   mkdir -p $rsyncdirname
