1242ed428b711610f9382dda80236b046f2f58bc
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-04-28 12:13:53 -0400

    Add pass3 calo for js embedding

---
+++ submit/JS_pp200_signal/pass3calo_embed/condor/run_all.pl
@@ -0,0 +1,137 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber=4;
+my $test;
+my $incremental;
+GetOptions("test"=>\$test, "increment"=>\$incremental);
+if ($#ARGV < 1)
+{
+    print "usage: run_all.pl <number of jobs> <\"Jet04\" production>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $jettrigger = $ARGV[1];
+if ($jettrigger  ne "Jet04")
+{
+    print "second argument has to be Jet04\n";
+    exit(1);
+}
+
+my $outfilelike = sprintf("pythia8_%s_sHijing_0_20fm_50kHz_bkg_0_20fm",$jettrigger);
+
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/%s",$outdir,lc $jettrigger);
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+  mkpath($outdir);
+}
+
+my %calohash = ();
+my %vtxhash = ();
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_CALO_G4HIT' and filename like '%$outfilelike%' and runnumber = $runnumber order by filename") || die $DBI::error;
+#print "select filename,segment from datasets where dsttype = 'DST_CALO_G4HIT' and filename like '%pythia8_$jettrigger%' and runnumber = $runnumber order by filename\n";
+
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::error;
+my $getvtxfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_VERTEX' and filename like '%$outfilelike%' and runnumber = $runnumber");
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::error;
+my $ncal = $getfiles->rows;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $calohash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+$getvtxfiles->execute() || die $DBI::error;
+my $nvtx = $getvtxfiles->rows;
+while (my @res = $getvtxfiles->fetchrow_array())
+{
+    $vtxhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getvtxfiles->finish();
+print "input files: $ncal, vtx: $nvtx\n";
+foreach my $segment (sort keys %calohash)
+{
+    if (! exists $vtxhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $calohash{$segment};
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+	my $outfilename = sprintf("DST_CALO_CLUSTER_%s-%010d-%05d.root",$outfilelike,$runnumber,$segment);
+	$chkfile->execute($outfilename);
+	if ($chkfile->rows > 0)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %s %d %d %s", $outevents, $jettrigger, $lfn, $vtxhash{sprintf("%05d",$segment)}, $outfilename, $outdir, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    exit(0);
+	}
+    }
+}
+$chkfile->finish();
+$dbh->disconnect;
---
+++ submit/JS_pp200_signal/pass3calo_embed/rundir/run_pass3calo.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n mdc2.8
+
+echo running: run_pass3calo.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+    getinputfiles.pl $3
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+    hostname
+    exit -1
+fi
+# arguments 
+# $1: number of events
+# $2: calo g4hits input file
+# $3: vertex input file
+# $4: output file
+# $5: output dir
+# $6: jet trigger
+# $7: run number
+# $8: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(calo g4hits file\): $2
+echo arg3 \(vertex file\): $3
+echo arg4 \(output file\): $4
+echo arg5 \(output dir\): $5
+echo arg6 \(jettrigger\): $6
+echo arg7 \(runnumber\): $7
+echo arg8 \(sequence\): $8
+
+runnumber=$(printf "%010d" $7)
+sequence=$(printf "%05d" $8)
+filename=JS_pp200_signal_pass2_embed_$6
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+echo running prmon --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_Calo.C\($1,\"$2\",\"$3\",\"$4\",\"$5\"\)
+prmon --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b  Fun4All_G4_Calo.C\($1,\"$2\",\"$3\",\"$4\",\"$5\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/JS_pp200_signal/pass3calo_embed_$6
+if [ ! -d $rsyncdirname ]
+then
+mkdir -p $rsyncdirname
+fi
+
+rsync -av $txtfilename $rsyncdirname
+rsync -av $jsonfilename $rsyncdirname
+
+echo "script done"
