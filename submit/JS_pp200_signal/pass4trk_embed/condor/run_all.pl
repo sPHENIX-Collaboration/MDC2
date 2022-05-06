7458f189cc2a9898847fc4d4592ee2bf13c8f9e1
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-05-06 11:21:23 -0400

    Add Jet15

--- submit/JS_pp200_signal/pass4trk_embed/condor/run_all.pl
+++ submit/JS_pp200_signal/pass4trk_embed/condor/run_all.pl
@@ -15,7 +15,7 @@ my $incremental;
 GetOptions("test"=>\$test, "increment"=>\$incremental);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Jet04\" production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Jet04\", \"Jet15\" production>\n";
     print "parameters:\n";
     print "--increment : submit jobs while processing running\n";
     print "--test : dryrun - create jobfiles\n";
@@ -32,9 +32,10 @@ if ($hostname !~ /phnxsub/)
 
 my $maxsubmit = $ARGV[0];
 my $jettrigger = $ARGV[1];
-if ($jettrigger  ne "Jet04")
+if ($jettrigger  ne "Jet04" &&
+    $jettrigger  ne "Jet15")
 {
-    print "second argument has to be Jet04\n";
+    print "second argument has to be Jet04 or Jet15\n";
     exit(1);
 }
 
