31caf25d468171f4d82074afd67f402cc0becbf0
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-07-09 11:49:11 -0400

    Add PhotonJet, run = 40

--- submit/JS_pp200_signal/pass5jet/condor/run_all.pl
+++ submit/JS_pp200_signal/pass5jet/condor/run_all.pl
@@ -9,13 +9,13 @@ use DBI;
 
 
 my $outevents = 0;
-my $runnumber=4;
+my $runnumber=40;
 my $test;
 my $incremental;
 GetOptions("test"=>\$test, "increment"=>\$incremental);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Jet04\" production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Jet04\", \"PhotonJet\" production>\n";
     print "parameters:\n";
     print "--increment : submit jobs while processing running\n";
     print "--test : dryrun - create jobfiles\n";
@@ -32,11 +32,13 @@ if ($hostname !~ /phnxsub/)
 
 my $maxsubmit = $ARGV[0];
 my $jettrigger = $ARGV[1];
-if ($jettrigger  ne "Jet04")
+if ($jettrigger  ne "Jet04" &&
+    $jettrigger  ne "PhotonJet")
 {
-    print "second argument has to be Jet04\n";
+    print "second argument has to be Jet04 or PhotonJet\n";
     exit(1);
 }
+
 my $jettriggerWithMHz = sprintf("%s_3MHz",$jettrigger);
 
 if (! -f "outdir.txt")
