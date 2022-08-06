23bf43b5aa684d1a60a016edf1c5a0d957708fed
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-08-06 12:17:50 -0400

    add pythia8 mb pass4_job0

---
+++ submit/pythia8_pp_mb/pass4_job0/rundir/Fun4All_G4_sPHENIX_job0.C
@@ -0,0 +1,129 @@
+// local macros
+#include <G4Setup_sPHENIX.C>
+#include <G4_Bbc.C>
+#include <G4_Global.C>
+#include <G4_Production.C>
+#include <G4_Tracking.C>
+
+#include <ffamodules/FlagHandler.h>
+
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+
+R__LOAD_LIBRARY(libffamodules.so)
+R__LOAD_LIBRARY(libfun4all.so)
+
+//________________________________________________________________________________________________
+int Fun4All_G4_sPHENIX_job0(
+  const int nEvents = 0,
+  const int nSkipEvents = 0,
+  const char* inputFile = "DST_TRKR_HIT_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000004-00000.root",
+  const char* outputFile = "DST_TRKR_CLUSTER_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000004-00000.root",
+    const string &outdir = ".")
+{
+
+  // print inputs
+  std::cout << "Fun4All_G4_sPHENIX_job0 - nEvents: " << nEvents << std::endl;
+  std::cout << "Fun4All_G4_sPHENIX_job0 - nSkipEvents: " << nSkipEvents << std::endl;
+  std::cout << "Fun4All_G4_sPHENIX_job0 - inputFile: " << inputFile << std::endl;
+  std::cout << "Fun4All_G4_sPHENIX_job0 - outputFile: " << outputFile << std::endl;
+
+  // set up production relatedstuff
+  Enable::PRODUCTION = true;
+  Enable::DSTOUT = true;
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  // options
+  Enable::PIPE = true;
+  Enable::BBC = true;
+  Enable::MAGNET = true;
+  Enable::PLUGDOOR = false;
+
+  // enable all absorbers
+  // this is equivalent to the old "absorberactive" flag
+  Enable::ABSORBER = true;
+
+  // central tracking
+  Enable::MVTX = true;
+  Enable::INTT = true;
+  Enable::TPC = true;
+  Enable::MICROMEGAS = true;
+  Enable::BLACKHOLE = true;
+
+  // TPC
+  G4TPC::ENABLE_STATIC_DISTORTIONS = false;
+  G4TPC::ENABLE_TIME_ORDERED_DISTORTIONS = false;
+  G4TPC::ENABLE_CORRECTIONS = false;
+  G4TPC::DO_HIT_ASSOCIATION = false;
+
+  // tracking configuration
+  G4TRACKING::use_full_truth_track_seeding = false;
+
+  // do not initialize magnetic field in ACTS
+  G4TRACKING::init_acts_magfield = false;
+  
+  // server
+  auto se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  // make sure to printout random seeds for reproducibility
+  PHRandomSeed::Verbosity(1);
+
+  FlagHandler *flag = new FlagHandler();
+  se->registerSubsystem(flag);
+
+  // clustering
+  Mvtx_Clustering();
+  Intt_Clustering();
+  TPC_Clustering();
+  Micromegas_Clustering();
+
+  // needed for makeActsGeometry, used in clustering
+  TrackingInit();
+  
+  // input manager
+  auto in = new Fun4AllDstInputManager("DSTin");
+  in->fileopen(inputFile);
+  se->registerInputManager(in);
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+
+  // output manager
+  /* all the nodes from DST and RUN are saved to the output */
+    string FullOutFile = DstOut::OutputFile;
+  auto out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+  out->AddNode("Sync");
+  out->AddNode("EventHeader");
+  out->AddNode("TRKR_CLUSTER"); 
+  out->AddNode("TRKR_CLUSTERCROSSINGASSOC");
+  se->registerOutputManager(out);
+
+  // skip events if any specified
+  if( nSkipEvents > 0 )
+  { se->skip( nSkipEvents ); }
+
+  // process events
+  se->run(nEvents);
+
+  // terminate
+  se->End();
+  se->PrintTimer();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+  gSystem->Exit(0);
+  return 0;
+}
