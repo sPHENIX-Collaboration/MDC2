90e6921d0c404f05248a76f3b789bd1b7574f984
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-08-08 15:18:45 -0400

    plugdoor run 42

--- submit/pythia8_pp_mb/pass1/rundir/Fun4All_G4_Pass1_pp.C
+++ submit/pythia8_pp_mb/pass1/rundir/Fun4All_G4_Pass1_pp.C
@@ -173,8 +173,8 @@ int Fun4All_G4_Pass1_pp(
   Enable::EPD = true;
 
   //! forward flux return plug door. Out of acceptance and off by default.
-  //Enable::PLUGDOOR = true;
-  Enable::PLUGDOOR_BLACKHOLE = true;
+  Enable::PLUGDOOR = true;
+  //Enable::PLUGDOOR_BLACKHOLE = true;
 
   //Enable::BEAMLINE = true;
   G4BEAMLINE::skin_thickness = 0.5;
@@ -190,7 +190,7 @@ int Fun4All_G4_Pass1_pp(
   //---------------
   // World Settings
   //---------------
-  //G4WORLD::PhysicsList = "FTFP_BERT_HP"; //FTFP_BERT_HP best for calo
+  G4WORLD::PhysicsList = "FTFP_BERT_HP"; //FTFP_BERT_HP best for calo
   //  G4WORLD::WorldMaterial = "G4_AIR"; // set to G4_GALACTIC for material scans
 
   //---------------
