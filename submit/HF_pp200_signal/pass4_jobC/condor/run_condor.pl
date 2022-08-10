1bd0912fc3d401d291b4876100ea1047365a5963
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-08-10 10:55:35 -0400

    set priorities to 900+

--- submit/HF_pp200_signal/pass4_jobC/condor/run_condor.pl
+++ submit/HF_pp200_signal/pass4_jobC/condor/run_condor.pl
@@ -59,7 +59,7 @@ print F "accounting_group = group_sphenix.mdc2\n";
 print F "accounting_group_user = sphnxpro\n";
 print F "Requirements = (CPU_Type == \"mdc2\")\n";
 print F "request_memory = 2048MB\n";
-print F "Priority 	= 27\n";
+print F "Priority 	= 906\n";
 print F "job_lease_duration = 3600\n";
 print F "Queue 1\n";
 close(F);
