b90573a406954b7d0f25fd9447ee6f75f198daba
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-08-11 16:11:42 -0400

    Add single particle pass1

---
+++ submit/single_particle/pass1/condor/run_condor.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+GetOptions("test"=>\$test);
+if ($#ARGV < 3)
+{
+    print "usage: run_condor.pl <events> <jettrigger> <outdir> <outfile> <skip> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+else
+{
+    print "running condor\n";
+}
+my $localdir=`pwd`;
+chomp $localdir;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass1_single.sh",$rundir);
+my $nevents = $ARGV[0];
+my $particle = $ARGV[1];
+my $pmin = $ARGV[2];
+my $pmax = $ARGV[3];
+my $dstoutdir = $ARGV[4];
+my $dstoutfile = $ARGV[5];
+my $runnumber = $ARGV[6];
+my $sequence = $ARGV[7];
+my $suffix = sprintf("%010d-%05d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log",$localdir);
+mkpath($logdir);
+my $condorlogdir = sprintf("/tmp/single_particle/pass1");
+mkpath($condorlogdir);
+my $partprop = sprintf("%s_%d_%d",$particle,$pmin,$pmax);
+my $jobfile = sprintf("%s/condor_%s-%s.job",$logdir,$partprop,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor_%s-%s.log",$condorlogdir,$partprop,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor_%s-%s.err",$logdir,$partprop,$suffix);
+my $outfile = sprintf("%s/condor_%s-%s.out",$logdir,$partprop,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $particle $pmin $pmax $dstoutfile $dstoutdir $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+#print F "accounting_group = group_sphenix.prod\n";
+print F "accounting_group = group_sphenix.mdc2\n";
+print F "accounting_group_user = sphnxpro\n";
+print F "Requirements = (CPU_Type == \"mdc2\")\n";
+print F "request_memory = 4500MB\n";
+print F "Priority 	= 801\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+if (defined $test)
+{
+    print "would submit $jobfile\n";
+}
+else
+{
+    system("condor_submit $jobfile");
+}
---
+++ submit/single_particle/pass1/rundir/run_pass1_single.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+
+hostname
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+else
+    echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of events
+# $2: particle
+# $3: pmin
+# $4: pmax
+# $5: output file
+# $6: output dir
+# $7: runnumber
+# $8: sequence
+
+echo 'here comes your environment'
+
+printenv
+
+echo arg1 \(events\) : $1
+echo arg2 \(particle\): $2
+echo arg3 \(pmin (MeV)\): $3
+echo arg4 \(pmax (MeV)\): $4
+echo arg5 \(output file\): $5
+echo arg6 \(output dir\): $6
+echo arg7 \(runnumber\): $7
+echo arg8 \(sequence\): $8
+
+runnumber=$(printf "%010d" $7)
+sequence=$(printf "%05d" $8)
+filename=single_$2_$3_$4
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+echo running prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_JS_pp_signal.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_JS_pp_signal.C\($1,\"$2\",\"$3\",\"\",0,\"$4\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/single/pass1_$2_$3_$4
+
+if [ ! -d $rsyncdirname ]
+then
+mkdir -p $rsyncdirname
+fi
+rsync -av $txtfilename $rsyncdirname
+rsync -av $jsonfilename $rsyncdirname
+
+
+echo "script done"
