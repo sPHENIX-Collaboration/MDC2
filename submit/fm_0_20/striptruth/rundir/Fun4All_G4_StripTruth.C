ca120ba702816e3ab448d58ead96c354410b064a
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-08-12 10:11:32 -0400

    Add conversion of DST_TRUTH back to DST_TRUTH_G4HIT to 0-20fm

---
+++ submit/fm_0_20/striptruth/rundir/Fun4All_G4_StripTruth.C
@@ -0,0 +1,65 @@
+#ifndef MACRO_FUN4ALLG4SPHENIX_C
+#define MACRO_FUN4ALLG4SPHENIX_C
+
+#include <GlobalVariables.C>
+
+#include <G4_Production.C>
+
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+
+void Fun4All_G4_StripTruth(    
+  const int nEvents = 1,
+  const string &inputFile = "DST_TRKR_G4HIT_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000002-00003.root",
+  const string &outputFile = "G4sPHENIX.root",
+  const string &outdir = "./"
+  )
+{
+  gSystem->Load("libg4dst.so");
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+  // set up production relatedstuff
+  Enable::PRODUCTION = true;
+  //======================
+  // Write the DST
+  //======================
+
+  Enable::DSTOUT = true;
+  Enable::DSTOUT_COMPRESS = false;
+  DstOut::OutputFile = outputFile;
+  DstOut::OutputDir = outdir;
+
+  Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTin");
+  in->fileopen(inputFile);
+  se->registerInputManager(in);
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+  string FullOutFile = DstOut::OutputFile;
+  Fun4AllOutputManager *out = new Fun4AllDstOutputManager("DSTout",FullOutFile);
+  out->StripNode("TRKR_HITTRUTHASSOC");
+  out->StripRunNode("CYLINDERCELLGEOM_SVTX");
+  out->StripRunNode("CYLINDERGEOM_MICROMEGAS_FULL");
+  out->StripRunNode("DEADMAP_INTT");
+  out->StripRunNode("G4CELLPARAM_INTT");
+  out->StripRunNode("G4CELLPARAM_TPC");
+  out->StripRunNode("G4TPCPADPLANE");
+  se->registerOutputManager(out);
+  se->run(nEvents);
+  se->End();
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+  gSystem->Exit(0);
+}
+
+#endif
