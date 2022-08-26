db0de9372fd34fcd1f7979e56a0990bd0b39824d
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-08-26 17:10:25 -0400

    fix tracking

--- submit/JS_pp200_signal/pass4_jobA/condor/run_all.pl
+++ submit/JS_pp200_signal/pass4_jobA/condor/run_all.pl
@@ -15,7 +15,7 @@ my $incremental;
 GetOptions("test"=>\$test, "increment"=>\$incremental);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Jet04\", \"PhotonJet\" production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Jet10\", <\"Jet30\", \"PhotonJet\" production>\n";
     print "parameters:\n";
     print "--increment : submit jobs while processing running\n";
     print "--test : dryrun - create jobfiles\n";
@@ -36,7 +36,7 @@ if ($jettrigger  ne "Jet10" &&
     $jettrigger  ne "Jet30" &&
     $jettrigger  ne "PhotonJet")
 {
-    print "second argument has to be Jet04 or PhotonJet\n";
+    print "second argument has to be Jet10, Jet30 or PhotonJet\n";
     exit(1);
 }
 
