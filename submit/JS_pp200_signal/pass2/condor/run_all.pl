fba5c9b8793c0af16d455e8e5405f1a7807f2a53
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-08-26 16:29:15 -0400

    switch to Jet30, Jet10 search for DST_TRUTH if DST_TRUTH_G4HIT not found

--- submit/JS_pp200_signal/pass2/condor/run_all.pl
+++ submit/JS_pp200_signal/pass2/condor/run_all.pl
@@ -15,7 +15,7 @@ my $incremental;
 GetOptions("test"=>\$test, "increment"=>\$incremental);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Jet04\", \"PhotonJet\" production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Jet10\", \"Jet30\", \"PhotonJet\" production>\n";
     print "parameters:\n";
     print "--increment : submit jobs while processing running\n";
     print "--test : dryrun - create jobfiles\n";
@@ -32,10 +32,11 @@ if ($hostname !~ /phnxsub/)
 
 my $maxsubmit = $ARGV[0];
 my $jettrigger = $ARGV[1];
-if ($jettrigger  ne "Jet04" &&
+if ($jettrigger  ne "Jet10" &&
+    $jettrigger  ne "Jet30" &&
     $jettrigger  ne "PhotonJet")
 {
-    print "second argument has to be Jet04 or PhotonJet\n";
+    print "second argument has to be Jet10, Jet30 or PhotonJet\n";
     exit(1);
 }
 
@@ -63,7 +64,7 @@ my %outfiletype = ();
 $outfiletype{"DST_BBC_G4HIT"} = 1;
 $outfiletype{"DST_CALO_G4HIT"} = 1;
 $outfiletype{"DST_TRKR_G4HIT"} = 1;
-$outfiletype{"DST_TRUTH_G4HIT"} = 1;
+$outfiletype{"DST_TRUTH_G4HIT"} = "DST_TRUTH";
 $outfiletype{"DST_VERTEX"} = 1;
 
 my $localdir=`pwd`;
@@ -111,7 +112,16 @@ while (my @res = $getfiles->fetchrow_array())
 		}
 		else
 		{
-		    print "missing $outlfn\n";
+		    my $newlfn = $outfiletype{$type};
+		    if ($newlfn ne "1")
+		    {
+			$lfn =~ s/$type/$outfiletype{$type}/;
+			$chkfile->execute($lfn);
+			if ($chkfile->rows > 0)
+			{
+			    next;
+			}
+		    }
 		    $foundall = 0;
 		    last;
 		}
