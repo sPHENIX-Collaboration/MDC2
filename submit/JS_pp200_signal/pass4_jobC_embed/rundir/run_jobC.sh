d796cf1bf729e30d6bda48ba6be6c80b4318dfdc
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-09-10 20:33:09 -0400

    use ana.319, use cdb

--- submit/JS_pp200_signal/pass4_jobC_embed/rundir/run_jobC.sh
+++ submit/JS_pp200_signal/pass4_jobC_embed/rundir/run_jobC.sh
@@ -11,7 +11,7 @@ this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 echo running: $this_script $*
 
-source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.319
 
 
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
@@ -35,8 +35,9 @@ fi
 # $2: trkr seed input file
 # $3: output file
 # $4: output dir
-# $5: run number
-# $6: sequence
+# $5: jet trigger
+# $6: run number
+# $7: sequence
 
 echo 'here comes your environment'
 printenv
@@ -44,11 +45,12 @@ echo arg1 \(events\) : $1
 echo arg2 \(trkr seed file\): $2
 echo arg3 \(output file\): $3
 echo arg4 \(output dir\): $4
-echo arg5 \(runnumber\): $5
-echo arg6 \(sequence\): $6
+echo arg5 \(jet trigger\): $5
+echo arg6 \(runnumber\): $6
+echo arg7 \(sequence\): $7
 
-runnumber=$(printf "%010d" $5)
-sequence=$(printf "%05d" $6)
+runnumber=$(printf "%010d" $6)
+sequence=$(printf "%05d" $7)
 filename=JS_pp200_signal_pass4_jobC_embed_$5
 
 txtfilenameC=${filename}-${runnumber}-${sequence}_C.txt
