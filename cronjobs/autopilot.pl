126bfea7cd0de1705ac7a74f314d6108bdee52b0
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-09-17 11:55:50 -0400

    add JetD0 and re-enable all passes in auto pilot

--- cronjobs/autopilot.pl
+++ cronjobs/autopilot.pl
@@ -12,20 +12,20 @@ my @submitdir = (
 "fm_0_20/pass4_job0/condor",
 "fm_0_20/pass4_jobA/condor",
 "fm_0_20/pass4_jobC/condor",
-"fm_0_20/pass4_nopileup_job0/condor",
-#"fm_0_20/pass4_nopileup_jobA/condor",
-"fm_0_20/pass4_nopileup_jobC/condor",
+"fm_0_20/pass4_job0_nopileup/condor",
+"fm_0_20/pass4_jobA_nopileup/condor",
+"fm_0_20/pass4_jobC_nopileup/condor",
 "fm_0_488/pass2/condor",
 "fm_0_488/pass3trk/condor",
 "fm_0_488/pass3calo/condor",
 "fm_0_488/pass4_job0/condor",
-#"fm_0_488/pass4_jobA/condor",
+"fm_0_488/pass4_jobA/condor",
 "fm_0_488/pass4_jobC/condor",
 "pythia8_pp_mb/pass2/condor",
 "pythia8_pp_mb/pass3trk/condor",
-#"pythia8_pp_mb/pass3calo/condor",
+"pythia8_pp_mb/pass3calo/condor",
 "pythia8_pp_mb/pass4_job0/condor",
-#"pythia8_pp_mb/pass4_jobA/condor",
+"pythia8_pp_mb/pass4_jobA/condor",
 "pythia8_pp_mb/pass4_jobC/condor",
 "HF_pp200_signal/pass2/condor",
 "HF_pp200_signal/pass3trk/condor",
@@ -48,7 +48,7 @@ my @submitdir = (
 "JS_pp200_signal/pass4jet_embed/condor"
 );
 
-my @quarkfilters = ("Charm", "Bottom");
+my @quarkfilters = ("Charm", "Bottom", "JetD0");
 my @jettriggers = ("Jet10", "Jet30", "PhotonJet");
 
 foreach my $subdir (@submitdir)
