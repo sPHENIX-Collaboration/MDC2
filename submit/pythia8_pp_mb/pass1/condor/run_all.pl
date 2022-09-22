afe85f1707f48b2f89de9c9bb5ab38fac78f916b
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-09-22 12:48:07 -0400

    pythia8 pp MB, pass1: switch to event generator, use bulk submission

--- submit/pythia8_pp_mb/pass1/condor/run_all.pl
+++ submit/pythia8_pp_mb/pass1/condor/run_all.pl
@@ -7,7 +7,10 @@ use Getopt::Long;
 
 my $test;
 my $incremental;
-GetOptions("test"=>\$test, "increment"=>\$incremental);
+my $killexist;
+my $runnumber = 50;
+my $events = 1000;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "killexist" => \$killexist);
 if ($#ARGV < 0)
 {
     print "usage: run_all.pl <number of jobs>\n";
@@ -18,18 +21,112 @@ if ($#ARGV < 0)
 }
 
 
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
 my $maxsubmit = $ARGV[0];
-my $pythia_runnumber = 1;
-my $runnumber = 42;
-my $events = 1000;
-my $evtsperfile = 100000;
-my $nmax = $evtsperfile;
-open(F,"outdir.txt");
-my $outdir=<F>;
-chomp  $outdir;
-close(F);
-mkpath($outdir);
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+    mkpath($outdir);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $logdir = sprintf("%s/log",$localdir);
 my $nsubmit = 0;
+my $njob = 0;
+OUTER: for (my $isub = 0; $isub < $maxsubmit; $isub++)
+{
+    my $jobfile = sprintf("%s/condor-%010d-%05d.job",$logdir,$runnumber,$njob);
+    while (-f $jobfile)
+    {
+	$njob++;
+	$jobfile = sprintf("%s/condor-%010d-%05d.job",$logdir,$runnumber,$njob);
+    }
+    print "using jobfile $jobfile\n";
+    my $outfile = sprintf("G4Hits_pythia8_pp_mb-%010d-%05d.root", $runnumber,$njob);
+    my $fulloutfile = sprintf("%s/%s",$outdir,$outfile);
+    print "out: $fulloutfile\n";
+    if (defined $killexist)
+    {
+	if (-f $fulloutfile)
+	{
+	    unlink  $fulloutfile;
+	}
+    }
+    if (! -f $fulloutfile)
+    {
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	system("perl run_condor.pl $events $outdir $outfile $runnumber $njob $tstflag");
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    last OUTER;
+	}
+    }
+    else
+    {
+	print "output file already exists\n";
+	$njob++;
+    }
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
+
+
 for (my $segment=0; $segment<100; $segment++)
 {
     my $pythia8datfile = sprintf("/sphenix/sim/sim01/sphnxpro/MDC1/pythia8_HepMC/data/pythia8_mb-%010d-%05d.dat",$pythia_runnumber, $segment);
