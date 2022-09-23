a9e874e8725d22f13873c2e9ae3178cae344c46c
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-09-23 11:46:37 -0400

    Add table verification scripts

---
+++ submit/filecatalog/find_orphans.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use File::stat;
+use Getopt::Long;
+use DBI;
+
+my $kill;
+my $verbosity;
+GetOptions("kill"=>\$kill, "verbosity"=>\$verbosity);
+if ($#ARGV < 0)
+{
+    print "usage find_orphans.pl <topdir>\n";
+    exit(0);
+}
+
+my $topdir = $ARGV[0];
+
+if (! -d $topdir)
+{
+    print "$topdir does not exist\n";
+    exit(1);
+}
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $checkfcat = $dbh->prepare("select full_file_path from files where lfn = ?");
+
+my %orphans = ();
+open(F,"find $topdir -type f -name '*.root' |");
+while (my $fullfile = <F>)
+{
+    chomp $fullfile;
+    my $lfn = basename($fullfile);
+#    print "$fullfile, lfn: $lfn\n";
+    $checkfcat->execute($lfn);
+    my $found = 0;
+    while (my @res = $checkfcat->fetchrow_array())
+    {
+	if ($fullfile eq $res[0])
+	{
+#	    print "found $fullfile in fcat\n";
+	    $found = 1;
+	}
+	else
+	{
+#	    print "$res[0] does not match $fullfile\n";
+	}
+    }
+    if ($found == 0)
+    {
+	$orphans{$fullfile} = $lfn;
+    }
+}
+close(F);
+open(F,"> remove.sh");
+foreach my $file (sort keys %orphans)
+{
+    print "$file is orphaned\n";
+    print F "rm $file\n";
+}
+close(F);
---
+++ submit/filecatalog/verifydatasets.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use File::stat;
+use Getopt::Long;
+use DBI;
+
+my $kill;
+GetOptions("kill"=>\$kill);
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+
+my $getfiles = $dbh->prepare("select lfn,full_file_path from files where time < now() - interval '2 hours'") || die $DBI::error;
+
+my $getdatasets = $dbh->prepare("select filename from datasets");
+
+my %datasetsfiles = ();
+$getdatasets->execute();
+while (my @res = $getdatasets->fetchrow_array())
+{
+    $datasetsfiles{$res[0]} = 1;
+}
+$getdatasets->finish();
+#my $delfile = $dbh->prepare("delete from files where full_file_path = ?");
+
+$getfiles->execute();
+my $nfiles = $getfiles->rows;
+print "checking $nfiles files\n";
+my $nchk = 0;
+my $ntot = 0;
+my $nfail = 0;
+my %failfiles = ();
+while (my @res = $getfiles->fetchrow_array())
+{
+    $nchk++;
+    $ntot++;
+    if (! exists $datasetsfiles{$res[0]})
+    {
+	print "cannot find $res[0] in datasets\n";
+        $nfail++;
+	$failfiles{$res[1]} = $res[0];
+    }
+    if ($nchk >= 1000)
+    {
+	print "checked $ntot files out of $nfiles\n";
+	$nchk = 0;
+    }
+}
+print "number of failures: $nfail\n";
+open(F,">dataset_missing.sh");
+open(F1,">dataset_missing.sql");
+if ($nfail > 0)
+{
+    print "missing files:\n";
+    foreach my $i (sort keys %failfiles)
+    {
+	print "$i\n";
+        print F "rm $i\n";
+        print F1 "psql -h sphnxdbmaster FileCatalog -c \"delete from files where lfn = \'$failfiles{$i}\' and full_file_path = \'$i\'\"\n";
+    }
+}
+
+close(F);
+close(F1);
+
+$getfiles->finish();
+$dbh->disconnect;
