ccbdc3f1d1ce01ebb2c5a835d93c97522409cfef
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-09-27 09:16:09 -0400

    check only files older 1hour for orphans

---
+++ submit/filecatalog/verifyfcat.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use File::stat;
+use Getopt::Long;
+use DBI;
+
+my $kill;
+GetOptions("kill"=>\$kill);
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+
+my $getfiles = $dbh->prepare("select filename from datasets") || die $DBI::error;
+
+my $getfullfile = $dbh->prepare("select lfn,full_file_path from files");
+#my $delfile = $dbh->prepare("delete from files where full_file_path = ?");
+my %fcatfiles = ();
+$getfullfile->execute();
+while (my @res = $getfullfile->fetchrow_array())
+{
+    $fcatfiles{$res[0]} = $res[1];
+}
+
+
+$getfiles->execute();
+my $nfiles = $getfiles->rows;
+print "checking $nfiles files\n";
+my $nchk = 0;
+my $ntot = 0;
+my $nfail = 0;
+my %failfiles = ();
+while (my @res = $getfiles->fetchrow_array())
+{
+    $nchk++;
+    $ntot++;
+    if (! exists $fcatfiles{$res[0]})
+    {
+	print "cannot find $res[0] in fcat\n";
+        $nfail++;
+	$failfiles{$res[0]} = 1;
+    }
+    if ($nchk >= 1000)
+    {
+	print "checked $ntot files out of $nfiles\n";
+	$nchk = 0;
+    }
+}
+print "number of failures: $nfail\n";
+if ($nfail > 0)
+{
+    print "missing files:\n";
+    foreach my $i (sort keys %failfiles)
+    {
+	print "$i\n";
+    }
+}
+
+$getfiles->finish();
+$getfullfile->finish();
+$dbh->disconnect;
