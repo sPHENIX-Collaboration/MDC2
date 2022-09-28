6cd584ae7366de313f68b542a9832d5c76ee249c
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-09-28 10:53:06 -0400

    use ana.319 HF pass3trk

--- submit/HF_pp200_signal/pass3trk/rundir/run_pass3trk.sh
+++ submit/HF_pp200_signal/pass3trk/rundir/run_pass3trk.sh
@@ -9,7 +9,7 @@ this_script=`readlink -f $this_script`
 this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 
-source /opt/sphenix/core/bin/sphenix_setup.sh -n new
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.319
 
 echo running: $this_script $*
 
