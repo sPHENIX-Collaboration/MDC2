5c727134072ad9a638c93019459b63f2d675e23f
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-10-02 12:35:07 -0400

    use ana.322 for JS pass4jet_embed

--- submit/JS_pp200_signal/pass4jet_embed/condor/run_condor.pl
+++ submit/JS_pp200_signal/pass4jet_embed/condor/run_condor.pl
@@ -33,9 +33,9 @@ if ($sequence < 100)
 }
 my $condorlistfile = sprintf("condor.list");
 my $suffix = sprintf("_%s-%010d-%05d",$jettrigger,$runnumber,$sequence);
-my $logdir = sprintf("%s/log",$localdir);
+my $logdir = sprintf("%s/log/%s",$localdir,$jettrigger);
 mkpath($logdir);
-my $condorlogdir = sprintf("/tmp/JS_pp200_signal/pass4jet_embed");
+my $condorlogdir = sprintf("/tmp/JS_pp200_signal/pass4jet_embed/%s",$jettrigger);
 mkpath($condorlogdir);
 my $jobfile = sprintf("%s/condor%s.job",$logdir,$suffix);
 if (-f $jobfile)
