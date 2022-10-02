2fc51b9da27a4d1ecc1c3c7ca33728cc884b66ed
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-10-02 10:54:38 -0400

    remove pythia8 files from selection

--- submit/fm_0_20/pass3calo/condor/run_all.pl
+++ submit/fm_0_20/pass3calo/condor/run_all.pl
@@ -66,9 +66,9 @@ my %vtxhash = ();
 
 my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::error;
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
-my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_CALO_G4HIT' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%' and runnumber = $inrunnumber order by filename") || die $DBI::error;
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_CALO_G4HIT' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%' and filename not like '%pythia8%' and runnumber = $inrunnumber order by filename") || die $DBI::error;
 my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::error;
-my $getvtxfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_VERTEX' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%' and runnumber = $inrunnumber");
+my $getvtxfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_VERTEX' and filename like '%sHijing_0_20fm_50kHz_bkg_0_20fm%' and filename not like '%pythia8%' and runnumber = $inrunnumber");
 my $nsubmit = 0;
 $getfiles->execute() || die $DBI::error;
 my $ncal = $getfiles->rows;
