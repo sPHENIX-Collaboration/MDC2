82333c487ae1415f9fc7e97413efa3d1a2328050
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-10-03 07:50:51 -0400

    set base prio to 50+ for JS

--- submit/JS_pp200_signal/pass3_job0_nopileup/condor/run_condor.pl
+++ submit/JS_pp200_signal/pass3_job0_nopileup/condor/run_condor.pl
@@ -17,7 +17,7 @@ if ($#ARGV < 3)
 
 my $localdir=`pwd`;
 chomp $localdir;
-my $baseprio = 43;
+my $baseprio = 53;
 my $rundir = sprintf("%s/../rundir",$localdir);
 my $executable = sprintf("%s/run_job0.sh",$rundir);
 my $nevents = $ARGV[0];
