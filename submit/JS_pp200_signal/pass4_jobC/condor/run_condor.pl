067531052b3cb7146c9cfca41f9016701d1e59d0
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-10-04 08:11:12 -0400

    bulkd submission, ana.322 disabled dist corrs for JS pass4_jobC

--- submit/JS_pp200_signal/pass4_jobC/condor/run_condor.pl
+++ submit/JS_pp200_signal/pass4_jobC/condor/run_condor.pl
@@ -7,9 +7,9 @@ use File::Path;
 
 my $test;
 GetOptions("test"=>\$test);
-if ($#ARGV < 3)
+if ($#ARGV < 5)
 {
-    print "usage: run_condor.pl <events> <trk clusters> <outfile> <outdir> <runnumber> <sequence>\n";
+    print "usage: run_condor.pl <events> <trackseeds> <outfile> <outdir> <runnumber> <sequence>\n";
     print "options:\n";
     print "-test: testmode - no condor submission\n";
     exit(-2);
@@ -17,6 +17,7 @@ if ($#ARGV < 3)
 
 my $localdir=`pwd`;
 chomp $localdir;
+my $baseprio = 55;
 my $rundir = sprintf("%s/../rundir",$localdir);
 my $executable = sprintf("%s/run_jobC.sh",$rundir);
 my $nevents = $ARGV[0];
@@ -26,10 +27,16 @@ my $dstoutfile = $ARGV[3];
 my $dstoutdir = $ARGV[4];
 my $runnumber = $ARGV[5];
 my $sequence = $ARGV[6];
-my $suffix = sprintf("%s_3MHz-%010d-%05d",$jettrigger,$runnumber,$sequence);
-my $logdir = sprintf("%s/log",$localdir);
+my $sequence = $ARGV[6];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("%s-%010d-%05d",$jettrigger,$runnumber,$sequence);
+my $logdir = sprintf("%s/log/%s",$localdir,$jettrigger);
 mkpath($logdir);
-my $condorlogdir = sprintf("/tmp/JS_pp200_signal/pass4_jobC");
+my $condorlogdir = sprintf("/tmp/JS_pp200_signal/pass4_jobC/%s",$jettrigger);
 mkpath($condorlogdir);
 my $jobfile = sprintf("%s/condor_%s.job",$logdir,$suffix);
 if (-f $jobfile)
@@ -59,15 +66,19 @@ print F "accounting_group = group_sphenix.mdc2\n";
 print F "accounting_group_user = sphnxpro\n";
 print F "Requirements = (CPU_Type == \"mdc2\")\n";
 print F "request_memory = 2048MB\n";
-print F "Priority 	= 25\n";
+print F "Priority = $baseprio\n";
 print F "job_lease_duration = 3600\n";
 print F "Queue 1\n";
 close(F);
-if (defined $test)
-{
-    print "would submit $jobfile\n";
-}
-else
-{
-    system("condor_submit $jobfile");
-}
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile, $dstoutfile, $dstoutdir, $jettrigger, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio\n";
+close(F);
