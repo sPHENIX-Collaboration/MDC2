95fec61b5733173c7ef7f1524b41e515a6ed337d
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-10-04 09:07:00 -0400

    always run on our hardware bc memory 0-20fm pass2

--- submit/fm_0_20/pass3trk/condor/run_all.pl
+++ submit/fm_0_20/pass3trk/condor/run_all.pl
@@ -19,6 +19,7 @@ if ($#ARGV < 0)
     print "usage: run_all.pl <number of jobs>\n";
     print "parameters:\n";
     print "--increment : submit jobs while processing running\n";
+    print "--shared : submit jobs to shared pool\n";
     print "--test : dryrun - create jobfiles\n";
     exit(1);
 }
@@ -153,7 +154,8 @@ $dbh->disconnect;
 my $jobfile = sprintf("condor.job");
 if (defined $shared)
 {
- $jobfile = sprintf("condor.job.shared");
+# $jobfile = sprintf("condor.job.shared");
+ $jobfile = sprintf("condor.job");
 }
 if (-f $condorlistfile)
 {
