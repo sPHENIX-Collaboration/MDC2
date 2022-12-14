14bfc2c983111fd1ab6d8418a7645bf44f212fb5
sPHENIX Production Account <pinkenburg@bnl.gov>
2022-12-14 18:16:38 -0500

    run 62, ana.335 for JS_pp200_signal/pass3jet_nopileup

--- submit/JS_pp200_signal/pass3jet_nopileup/rundir/run_pass3jet_nopileup.sh
+++ submit/JS_pp200_signal/pass3jet_nopileup/rundir/run_pass3jet_nopileup.sh
@@ -10,7 +10,7 @@ this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 echo running: $this_script $*
 
-source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.322
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.335
 
 
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
@@ -29,6 +29,8 @@ then
     date +%s
 else
     echo condor scratch NOT set
+    hostname
+    exit -1
 fi
 
 # arguments 
