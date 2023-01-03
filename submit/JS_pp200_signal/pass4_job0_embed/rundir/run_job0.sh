27732678996529cf21ad03e74971e7451d41831a
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-01-03 11:40:28 -0500

    use ana.339 for JS_pp200_signal/pass4_job0_embed

--- submit/JS_pp200_signal/pass4_job0_embed/rundir/run_job0.sh
+++ submit/JS_pp200_signal/pass4_job0_embed/rundir/run_job0.sh
@@ -3,15 +3,16 @@ export USER="$(id -u -n)"
 export LOGNAME=${USER}
 export HOME=/sphenix/u/${USER}
 
+hostname
+
 this_script=$BASH_SOURCE
 this_script=`readlink -f $this_script`
 this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 echo running: $this_script $*
 
-source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.335
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.339
 
-hostname
 
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
 then
