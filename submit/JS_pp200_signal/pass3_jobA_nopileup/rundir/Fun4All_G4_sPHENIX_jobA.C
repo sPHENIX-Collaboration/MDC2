29191e06d04c7c273544209eb50bf58f27c594c3
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-01-18 14:13:37 -0500

    use ana.341 for JS_pp200_signal/pass3_jobA_nopileup

--- submit/JS_pp200_signal/pass3_jobA_nopileup/rundir/Fun4All_G4_sPHENIX_jobA.C
+++ submit/JS_pp200_signal/pass3_jobA_nopileup/rundir/Fun4All_G4_sPHENIX_jobA.C
@@ -20,8 +20,8 @@ R__LOAD_LIBRARY(libfun4all.so)
 int Fun4All_G4_sPHENIX_jobA(
   const int nEvents = 0,
   const int nSkipEvents = 0,
-  const string &inputFile = "DST_TRKR_CLUSTER_pythia8_Jet10-0000000040-00000.root",
-  const string &outputFile = "DST_TRACKSEEDS_pythia8_Jet10-0000000040-00000.root",
+  const string &inputFile = "DST_TRKR_CLUSTER_pythia8_Jet10-0000000062-00000.root",
+  const string &outputFile = "DST_TRACKSEEDS_pythia8_Jet10-0000000062-00000.root",
   const string &outdir = "."
   )
 {
