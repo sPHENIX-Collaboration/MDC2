c200b6863e9cceb12a3edb42d69c73bca685c5a3
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-01-18 17:52:17 -0500

    use ana.341 for JS_pp200_signal/pass4_job0_embed

--- submit/JS_pp200_signal/pass4_job0_embed/rundir/Fun4All_G4_sPHENIX_job0.C
+++ submit/JS_pp200_signal/pass4_job0_embed/rundir/Fun4All_G4_sPHENIX_job0.C
@@ -25,8 +25,8 @@ R__LOAD_LIBRARY(libfun4all.so)
 int Fun4All_G4_sPHENIX_job0(
   const int nEvents = 0,
   const int nSkipEvents = 0,
-  const string &inputFile = "DST_TRKR_HIT_pythia8_PhotonJet_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000040-00000.root",
-  const string &outputFile = "DST_TRKR_CLUSTER_pythia8_PhotonJet_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000040-00000.root",
+  const string &inputFile = "DST_TRKR_HIT_pythia8_Jet30_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000062-00000.root",
+  const string &outputFile = "DST_TRKR_CLUSTER_pythia8_Jet30_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000062-00000.root",
   const string &outdir = ".")
 {
 
