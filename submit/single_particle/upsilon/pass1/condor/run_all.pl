5605eaa6ef3893896f0d79669b01899524e18a5d
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-02-06 13:23:54 -0500

    add single_particle/upsilon/pass1

---
+++ submit/single_particle/upsilon/pass1/condor/run_all.pl
@@ -0,0 +1,134 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Long;
+
+my $test;
+my $incremental;
+my $killexist;
+my $runnumber = 63;
+my $events = 100;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "killexist" => \$killexist);
+if ($#ARGV < 3)
+{
+    print "usage: run_all.pl <number of jobs> <particle> <pmin> <pmax>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--killexist : delete output file if it already exists (but no jobfile)\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $particle = lc $ARGV[1];
+my $pmin = $ARGV[2];
+my $pmax = $ARGV[3];
+my $filetype="single";
+my $partprop = sprintf("%s_%d_%d",$particle,$pmin,$pmax);
+$filetype=sprintf("%s_%sMeV",$filetype,$partprop);
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/%s",$outdir,$partprop);
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+    mkpath($outdir);
+}
+my $localdir=`pwd`;
+chomp $localdir;
+my $logdir = sprintf("%s/log",$localdir);
+my $nsubmit = 0;
+my $njob = 0;
+for (my $isub = 0; $isub < $maxsubmit; $isub++)
+{
+    my $jobfile = sprintf("%s/condor_%s-%010d-%05d.job",$logdir,$partprop,$runnumber,$njob);
+    while (-f $jobfile)
+    {
+	$njob++;
+	$jobfile = sprintf("%s/condor_%s-%010d-%05d.job",$logdir,$partprop,$runnumber,$njob);
+    }
+    print "using jobfile $jobfile\n";
+    my $outfile = sprintf("G4Hits_%s-%010d-%05d.root",$filetype, $runnumber,$njob);
+    my $fulloutfile = sprintf("%s/%s",$outdir,$outfile);
+    print "out: $fulloutfile\n";
+    if (defined $killexist)
+    {
+	if (-f $fulloutfile)
+	{
+	    unlink  $fulloutfile;
+	}
+    }
+    if (! -f $fulloutfile)
+    {
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	system("perl run_condor.pl $events $particle $pmin $pmax $outdir $outfile $runnumber $njob $tstflag");
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    last;
+	}
+    }
+    else
+    {
+	print "output file already exists\n";
+	$njob++;
+    }
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
---
+++ submit/single_particle/upsilon/pass1/rundir/run_pass1_upsilon.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.345
+
+hostname
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+else
+    echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of events
+# $2: particle
+# $3: output file
+# $4: output dir
+# $5: runnumber
+# $6: sequence
+
+echo 'here comes your environment'
+
+printenv
+
+echo arg1 \(events\) : $1
+echo arg2 \(particle\): $2
+echo arg3 \(output file\): $3
+echo arg4 \(output dir\): $4
+echo arg5 \(runnumber\): $5
+echo arg6 \(sequence\): $6
+
+runnumber=$(printf "%010d" $5)
+sequence=$(printf "%05d" $6)
+filename=single_$2
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+echo running prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_Upsilon.C\($1,\"$2\",$3,$4,\"$5\",\"$6\"\)
+prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_G4_Upsilon.C\($1,\"$2\",$3,$4,\"$5\",\"$6\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/single/upsilon/pass1_$2
+
+if [ ! -d $rsyncdirname ]
+then
+mkdir -p $rsyncdirname
+fi
+rsync -av $txtfilename $rsyncdirname
+rsync -av $jsonfilename $rsyncdirname
+
+
+echo "script done"
