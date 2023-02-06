2414d88321acb52891eb8c76ebb78388150e489e
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-02-06 12:38:29 -0500

    add HF_pp200_signal/pass3_jobC_nopileup

---
+++ submit/HF_pp200_signal/pass3_jobC_nopileup/condor/run_all.pl
@@ -0,0 +1,170 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber=63;
+my $test;
+my $incremental;
+my $shared;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "shared" => \$shared);
+if ($#ARGV < 1)
+{
+    print "usage: run_all.pl <number of jobs> <\"Charm\", \"CharmD0\", \"Bottom\", \"BottomD0\", \"JetD0\" production>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--shared : submit jobs to shared pool\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $quarkfilter = $ARGV[1];
+if ($quarkfilter  ne "Charm" &&
+    $quarkfilter  ne "CharmD0" &&
+    $quarkfilter  ne "Bottom" &&
+    $quarkfilter  ne "BottomD0" &&
+    $quarkfilter  ne "JetD0")
+{
+    print "second argument has to be either Charm, CharmD0, Bottom, BottomD0 or JetD0\n";
+    exit(1);
+}
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/%s",$outdir,lc $quarkfilter);
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+  mkpath($outdir);
+}
+
+my $quarkfilterWithDash = sprintf("%s-",$quarkfilter);
+
+my %trkhash = ();
+my %clusterhash = ();
+
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRACKSEEDS' and filename like '%pythia8_$quarkfilterWithDash%' and runnumber = $runnumber order by filename") || die $DBI::errstr;
+my $getclusterfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_CALO_CLUSTER' and filename like '%pythia8_$quarkfilterWithDash%' and runnumber = $runnumber");
+
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::errstr;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $trkhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+$getclusterfiles->execute() || die $DBI::errstr;
+my $ncluster = $getclusterfiles->rows;
+while (my @res = $getclusterfiles->fetchrow_array())
+{
+    $clusterhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getclusterfiles->finish();
+foreach my $segment (sort keys %trkhash)
+{
+    if (! exists $clusterhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $trkhash{$segment};
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+	my $outfilename = sprintf("DST_TRACKS_pythia8_%s-%010d-%05d.root",$quarkfilter,$runnumber,$segment);
+	$chkfile->execute($outfilename);
+	if ($chkfile->rows > 0)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %s %d %d %s", $outevents, $quarkfilter, $lfn, $clusterhash{sprintf("%05d",$segment)}, $outfilename, $outdir, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit >= 20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, submitting\n";
+	    last;
+	}
+    }
+}
+$chkfile->finish();
+$dbh->disconnect;
+
+my $jobfile = sprintf("condor.job");
+if (defined $shared)
+{
+ $jobfile = sprintf("condor.job.shared");
+}
+if (! -f $jobfile)
+{
+    print "could not find $jobfile\n";
+    exit(1);
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit $jobfile\n";
+    }
+    else
+    {
+	system("condor_submit $jobfile");
+    }
+}
---
+++ submit/HF_pp200_signal/pass3_jobC_nopileup/rundir/run_pass3_jobC_nopileup.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.345
+
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    echo $2 > inputfiles.list
+    echo $3 >> inputfiles.list
+    getinputfiles.pl --filelist inputfiles.list
+    if [ $? -ne 0 ]
+    then
+        cat inputfiles.list
+	echo error from getinputfiles.pl --filelist inputfiles.list, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+    hostname
+    exit -1
+fi
+
+# arguments 
+# $1: number of events
+# $2: trkr seed input file
+# $3: cluster input file
+# $4: output file
+# $5: output dir
+# $6: quark filter
+# $7: run number
+# $8: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(trkr seed file\): $2
+echo arg3 \(cluster file\): $3
+echo arg4 \(output file\): $4
+echo arg5 \(output dir\): $5
+echo arg6 \(quark filter\): $6
+echo arg7 \(runnumber\): $7
+echo arg8 \(sequence\): $8
+
+runnumber=$(printf "%010d" $7)
+sequence=$(printf "%05d" $8)
+filename=HF_pp200_signal_pass3_jobC_nopileup_$6
+
+txtfilenameC=${filename}-${runnumber}-${sequence}_C.txt
+jsonfilenameC=${filename}-${runnumber}-${sequence}_C.json
+
+echo running prmon --filename $txtfilenameC --json-summary $jsonfilenameC -- root.exe -q -b Fun4All_G4_sPHENIX_jobC.C\($1,0,\"$2\",\"$3\",\"$4\",\"$5\"\)
+prmon --filename $txtfilenameC --json-summary $jsonfilenameC -- root.exe -q -b  Fun4All_G4_sPHENIX_jobC.C\($1,0,\"$2\",\"$3\",\"$4\",\"$5\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/HF_pp200_signal/pass3_jobC_nopileup_$5
+if [ ! -d $rsyncdirname ]
+then
+  mkdir -p $rsyncdirname
+fi
+
+rsync -av $txtfilenameC $rsyncdirname
+rsync -av $jsonfilenameC $rsyncdirname
+
+echo "script done"
