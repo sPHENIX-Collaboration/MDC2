02ebcc10ac7d23f4d7968854ccbda13f52dd3cd6
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-02-28 21:19:13 -0500

    single particle embedding

---
+++ submit/single_particle/pass2_embed/condor/run_all.pl
@@ -0,0 +1,217 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber = 6;
+my $test;
+my $incremental;
+GetOptions("test"=>\$test, "increment"=>\$incremental);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs> <particle> <ptmin> <ptmax>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+my $maxsubmit = $ARGV[0];
+my $particle = lc $ARGV[1];
+my $ptmin = $ARGV[2];
+my $ptmax = $ARGV[3];
+
+my $embedfilelike = sprintf("sHijing_0_20fm_50kHz_bkg_0_20fm");
+my $outfilelike = sprintf("single_%s_%d_%dMeV_%s",$particle,$embedfilelike);
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d/%s",$outdir,$runnumber, lc $particle);
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+  mkpath($outdir);
+}
+
+
+my %outfiletype = ();
+$outfiletype{"DST_BBC_G4HIT"} = 1;
+$outfiletype{"DST_CALO_G4HIT"} = 1;
+$outfiletype{"DST_TRKR_G4HIT"} = 1;
+$outfiletype{"DST_TRUTH_G4HIT"} = 1;
+$outfiletype{"DST_VERTEX"} = 1;
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $nsubmit = 0;
+
+my %trkhash = ();
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_G4HIT' and filename like 'DST_TRKR_G4HIT_$embedfilelike%' and runnumber = $runnumber order by filename") || die $DBI::errstr;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+$getfiles->execute() || die $DBI::errstr;
+my $ncal = $getfiles->rows;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $trkhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+
+my %truthhash = ();
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like 'DST_TRUTH_G4HIT_$embedfilelike%' and runnumber = $runnumber");
+$gettruthfiles->execute() || die $DBI::errstr;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    $truthhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$gettruthfiles->finish();
+
+my %bbchash = ();
+my $getbbcfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_BBC_G4HIT' and filename like 'DST_BBC_G4HIT_$embedfilelike%' and runnumber = $runnumber");
+$getbbcfiles->execute() || die $DBI::errstr;
+my $nbbc = $getbbcfiles->rows;
+while (my @res = $getbbcfiles->fetchrow_array())
+{
+    $bbchash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getbbcfiles->finish();
+
+my %calohash = ();
+my $getcalofiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_CALO_G4HIT' and filename like 'DST_CALO_G4HIT_$embedfilelike%' and runnumber = $runnumber");
+$getcalofiles->execute() || die $DBI::errstr;
+my $ncalo = $getcalofiles->rows;
+while (my @res = $getcalofiles->fetchrow_array())
+{
+    $calohash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getcalofiles->finish();
+
+my %vertexhash = ();
+my $getvertexfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_VERTEX' and filename like 'DST_VERTEX_$embedfilelike%' and runnumber = $runnumber");
+$getvertexfiles->execute() || die $DBI::errstr;
+my $nvertex = $getvertexfiles->rows;
+while (my @res = $getvertexfiles->fetchrow_array())
+{
+    $vertexhash{sprintf("%05d",$res[1])} = $res[0];
+}
+$getvertexfiles->finish();
+
+
+#print "input files: $ncal, truth: $ntruth\n";
+foreach my $segment (sort keys %trkhash)
+{
+    if (! exists $bbchash{$segment})
+    {
+	next;
+    }
+    if (! exists $calohash{$segment})
+    {
+	next;
+    }
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+    if (! exists $vertexhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $trkhash{$segment};
+#    print "found $lfn\n";
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+        my $foundall = 1;
+	foreach my $type (sort keys %outfiletype)
+	{
+            my $lfn =  sprintf("%s_%s-%010d-%05d.root",$outfilelike,$outfilelike,$runnumber,$segment);
+	    $chkfile->execute($lfn);
+	    if ($chkfile->rows > 0)
+	    {
+		next;
+	    }
+	    else
+	    {
+		$foundall = 0;
+		last;
+	    }
+	}
+	if ($foundall == 1)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %d %d %s %s %s %s %s %s %d %d %s", $outevents, $particle, $pmin, $pmax, $lfn, $bbchash{sprintf("%05d",$segment)}, $calohash{sprintf("%05d",$segment)}, $truthhash{sprintf("%05d",$segment)}, $vertexhash{sprintf("%05d",$segment)}, $outdir, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit >= 20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, exiting\n";
+	    last;
+	}
+    }
+}
+
+$chkfile->finish();
+$dbh->disconnect;
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
---
+++ submit/single_particle/pass2_embed/rundir/run_embed_single.sh
@@ -0,0 +1,83 @@
+#! /bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.342
+
+hostname
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    echo $2 > inputfiles.list
+    echo $3 >> inputfiles.list
+    echo $4 >> inputfiles.list
+    echo $5 >> inputfiles.list
+    echo $6 >> inputfiles.list
+    getinputfiles.pl  --filelist inputfiles.list
+    if [ $? -ne 0 ]
+    then
+        cat inputfiles.list
+	echo error from getinputfiles.pl  --filelist inputfiles.list, exiting
+	exit -1
+    fi
+else
+ echo condor scratch NOT set
+ exit -1
+fi
+
+# arguments 
+# $1: number of output events
+# $2: bbc g4hits embed file
+# $3: calo g4hits embed file
+# $4: track g4hits embed file
+# $5: truth g4hits embed file
+# $6: vertex embed file
+# $7: output directory
+# $8: particle
+# $9: ntuple output file
+# $10: run number
+# $11: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(output events\) : $1
+echo arg2 \(bbc g4hits embed file\): $2
+echo arg3 \(calo g4hits embed file\): $3
+echo arg4 \(track g4hits embed file\): $4
+echo arg5 \(truth g4hits embed file\): $5
+echo arg6 \(vertex embed file\): $6
+echo arg7 \(output dir\): $7
+echo arg8 \(particle\): $8
+echo arg9 \(ntuple output file\): $9
+echo arg10 \(runnumber\): $10
+echo arg11 \(sequence\): $11
+
+runnumber=$(printf "%010d" $10)
+sequence=$(printf "%05d" $11)
+filename=hijing_embed_$8
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+echo running root.exe -q -b Fun4All_G4_Pi0_Embed.C\($1,\"$2\",\"$3\",\"$4\",\"$5\",\"$6\",0,\"$7\",\"$8\",\"$9\"\)
+prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b  Fun4All_G4_Pi0_Embed.C\($1,\"$2\",\"$3\",\"$4\",\"$5\",\"$6\",0,\"$7\",\"$8\",\"$9\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/single/hijing_embed_$8
+if [ ! -d $rsyncdirname ]
+then
+  mkdir -p $rsyncdirname
+fi
+
+rsync -av $txtfilename $rsyncdirname
+rsync -av $jsonfilename $rsyncdirname
+
+echo "script done"
