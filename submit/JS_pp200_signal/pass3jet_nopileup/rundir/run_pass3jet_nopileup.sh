42d28592231350f6a36fa21c89cf131d1fc279d1
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-03-13 12:19:42 -0400

    run 6, ana.349 for JS_pp200_signal/pass3jet_nopileup

--- submit/JS_pp200_signal/pass3jet_nopileup/rundir/run_pass3jet_nopileup.sh
+++ submit/JS_pp200_signal/pass3jet_nopileup/rundir/run_pass3jet_nopileup.sh
@@ -3,6 +3,7 @@ export USER="$(id -u -n)"
 export LOGNAME=${USER}
 export HOME=/sphenix/u/${USER}
 
+hostname
 
 this_script=$BASH_SOURCE
 this_script=`readlink -f $this_script`
@@ -10,7 +11,7 @@ this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 echo running: $this_script $*
 
-source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.335
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.349
 
 
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
