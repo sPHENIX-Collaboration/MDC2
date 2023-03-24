4e58355dca693b4ddd0d080833b92e391a7215b6
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-03-24 13:45:36 -0400

    make copy of rawdata/job0 under shrek

---
+++ shrek/rawdata/job0/condor/run_all.pl
@@ -0,0 +1,176 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber=6;
+my $test;
+my $incremental;
+my $overwrite;
+my $shared;
+my $rawdatadir = sprintf("/sphenix/lustre01/sphnxpro/mdc2/rawdata/stripe5");
+my $startrun = 250;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "overwrite"=>\$overwrite, "shared" => \$shared);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--overwrite : overwrite existing jobfiles and restart\n";
+    print "--shared : submit jobs to shared pool\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+#    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d",$outdir,$runnumber);
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+  mkpath($outdir);
+}
+
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $logdir = sprintf("%s/log",$localdir);
+mkpath($logdir);
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_HIT' and filename like 'DST_TRKR_HIT_sHijing_0_20fm_50kHz_bkg_0_20fm%' and runnumber = $runnumber order by filename") || die $DBI::errstr;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::errstr;
+my $icnt = 0;
+my $outseg = 0;
+my @files = ();
+my $nrun = $startrun;
+my $nseg = -1;
+while (my @res = $getfiles->fetchrow_array())
+{
+    my $lfn = $res[0];
+    push(@files,$lfn);
+    $icnt++;
+    if ($icnt < 10)
+    {
+	next;
+    }
+    $icnt = 0;
+    my $outfilename = sprintf("DST_PASS1_CLUSTERS_sHijing_0_20fm_50kHz_bkg_0_20fm-%010d-%05d.root",$runnumber,$outseg);
+    my $dstlistfile = sprintf("%s/condor-%010d-%05d.dstlist",$logdir,$runnumber,$outseg);
+    open(F1,">$dstlistfile");
+    foreach my $bf (@files)
+    {
+	print F1 "$bf\n";
+    }
+    close(F1);
+    @files = ();
+    $outseg++;
+    $chkfile->execute($outfilename);
+    if ($chkfile->rows > 0)
+    {
+	next;
+    }
+    my $tstflag="";
+    if (defined $test)
+    {
+	$tstflag="--test";
+    }
+    elsif (defined $overwrite)
+    {
+	$tstflag="--overwrite";
+    }
+# create run and segment number to pass down
+my $rawfilename;
+do
+ {
+    $nseg++;
+if ($nseg > 16)
+ {
+    $nseg = 0;
+    $nrun++;
+}
+if ($nrun > 299)
+ {
+    $nrun = $startrun;
+}
+    $rawfilename = sprintf("%s/ebdc01_junk-%08d-%04d.evt",$rawdatadir,$nrun,$nseg);
+} until (-f $rawfilename);
+    my $subcmd = sprintf("perl run_condor.pl %d %d %d %s %s %s %d %d %s %s", $outevents, $runnumber, $outseg, $outfilename, $dstlistfile, $outdir, $nrun, $nseg, $rawdatadir, $tstflag);
+    print "cmd: $subcmd\n";
+	system($subcmd);
+    my $exit_value  = $? >> 8;
+    if ($exit_value != 0)
+    {
+	if (! defined $incremental)
+	{
+	    print "error from run_condor.pl\n";
+	    exit($exit_value);
+	}
+    }
+    else
+    {
+	$nsubmit++;
+    }
+    if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit >= 20000)
+    {
+	print "maximum number of submissions $nsubmit reached, submitting\n";
+	last;
+    }
+}
+
+$getfiles->finish();
+$chkfile->finish();
+$dbh->disconnect;
+
+my $jobfile = sprintf("condor.job");
+if (defined $shared)
+{
+ $jobfile = sprintf("condor.job.shared");
+}
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit $jobfile\n";
+    }
+    else
+    {
+	system("condor_submit $jobfile");
+    }
+}
---
+++ shrek/rawdata/job0/rundir/run_job0.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /opt/sphenix/core/bin/sphenix_setup.sh
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+else
+    echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of events
+# $2: outfile name
+# $3: dst file list
+# $4: output dir
+# $5: runnumber
+# $6: sequence
+# $7: raw data input dir
+
+
+echo 'here comes your environment'
+
+printenv
+
+echo arg1 \(events\) : $1
+echo arg2 \(outfile name\) : $2
+echo arg3 \(dst file list\) : $3
+echo arg4 \(output dir\): $4
+echo arg5 \(runnumber\): $5
+echo arg6 \(sequence\): $6
+echo arg7 \(input dir\): $7
+
+runnumber=$(printf "%010d" $5)
+sequence=$(printf "%05d" $6)
+filename=pass1_clustering
+
+txtfilename=${filename}-${runnumber}-${sequence}.txt
+jsonfilename=${filename}-${runnumber}-${sequence}.json
+
+echo running prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_Job0.C\($1,\"$2\",\"$3\",\"$4\",$5,$6,\"$7\"\)
+prmon  --filename $txtfilename --json-summary $jsonfilename -- root.exe -q -b Fun4All_Job0.C\($1,\"$2\",\"$3\",\"$4\",$5,$6,\"$7\"\)
+
+rsyncdirname=/sphenix/user/sphnxpro/prmon/rawdata/pass1_clustering
+
+if [ ! -d $rsyncdirname ]
+then
+mkdir -p $rsyncdirname
+fi
+rsync -av $txtfilename $rsyncdirname
+rsync -av $jsonfilename $rsyncdirname
+
+
+echo "script done"
