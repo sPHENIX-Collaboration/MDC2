47cbcf946c5ef54b0dc74c46ff629394d5cb87d4
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-03-31 11:22:47 -0400

    Add checks for single files

--- submit/filecatalog/add_fcat_to_dataset.pl
+++ submit/filecatalog/add_fcat_to_dataset.pl
@@ -8,7 +8,8 @@ use Getopt::Long;
 
 my $mdc = "mdc2";
 my $test;
-GetOptions("test"=>\$test);
+my $lfn;
+GetOptions("lfn:s" => \$lfn, "test"=>\$test);
 
 my $dcachedir = $ARGV[0];
 if (! -d $dcachedir)
@@ -26,6 +27,10 @@ my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc");
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
 #my $getfiles = $dbh->prepare("select lfn,size,full_file_path from files where lfn = 'DST_TRUTH_G4HIT_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000002-02278.root'");
 my $getfiles = $dbh->prepare("select lfn,size,full_file_path from files where full_file_path like '$dcachedir/%'"); 
+if (defined $lfn)
+{
+    $getfiles = $dbh->prepare("select lfn,size,full_file_path from files where lfn =  '$lfn'");
+}
 #my $getfiles = $dbh->prepare("select lfn,size,full_file_path from files where lfn = 'G4Hits_sHijing_0_20fm-0000000006-18875.root'");
 my $chkfile = $dbh->prepare("select size from datasets where filename = ?");
 my $insertdataset = $dbh->prepare("insert into datasets (filename,runnumber,segment,size,dataset,dsttype,events) values (?,?,?,?,'mdc2',?,?)");
