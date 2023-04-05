cbd929152c63c10244e12282ed73bf0e90ac8bc8
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-04-04 21:39:48 -0400

    use run 6 for input/output files

--- submit/fm_0_20/pass4_jobA/rundir/Fun4All_G4_sPHENIX_jobA.C
+++ submit/fm_0_20/pass4_jobA/rundir/Fun4All_G4_sPHENIX_jobA.C
@@ -23,8 +23,8 @@ R__LOAD_LIBRARY(libfun4all.so)
 int Fun4All_G4_sPHENIX_jobA(
   const int nEvents = 0,
   const int nSkipEvents = 0,
-  const string &inputFile = "DST_TRKR_CLUSTER_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000062-00000.root",
-  const string &outputFile = "DST_TRACKSEEDS_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000062-00000.root",
+  const string &inputFile = "DST_TRKR_CLUSTER_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000006-00000.root",
+  const string &outputFile = "DST_TRACKSEEDS_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000006-00000.root",
   const string &outdir = "."
   )
 {
