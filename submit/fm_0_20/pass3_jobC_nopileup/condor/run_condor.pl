3b13647eda282983bcdb02eec96422b6d685b001
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-06 13:29:20 -0400

    use ana.358 for tracking

--- submit/fm_0_20/pass3_jobC_nopileup/condor/run_condor.pl
+++ submit/fm_0_20/pass3_jobC_nopileup/condor/run_condor.pl
@@ -19,7 +19,7 @@ my $localdir=`pwd`;
 chomp $localdir;
 my $baseprio = 58;
 my $rundir = sprintf("%s/../rundir",$localdir);
-my $executable = sprintf("%s/run_jobC.sh",$rundir);
+my $executable = sprintf("%s/run_pass3_jobC_nopileup_fm_0_20.sh",$rundir);
 my $nevents = $ARGV[0];
 my $infile1 = $ARGV[1];
 my $infile2 = $ARGV[2];
