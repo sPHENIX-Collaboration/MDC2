ca806428aa201748b80551b8b90b375dc482c212
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-07 00:20:10 -0400

    use ana.358 for tracking

--- submit/JS_pp200_signal/pass4_jobA_embed/condor/run_condor.pl
+++ submit/JS_pp200_signal/pass4_jobA_embed/condor/run_condor.pl
@@ -19,7 +19,7 @@ my $localdir=`pwd`;
 chomp $localdir;
 my $baseprio = 55;
 my $rundir = sprintf("%s/../rundir",$localdir);
-my $executable = sprintf("%s/run_jobA_embed.sh",$rundir);
+my $executable = sprintf("%s/run_pass4_jobA_embed_js.sh",$rundir);
 my $nevents = $ARGV[0];
 my $jettrigger = $ARGV[1];
 my $infile = $ARGV[2];
