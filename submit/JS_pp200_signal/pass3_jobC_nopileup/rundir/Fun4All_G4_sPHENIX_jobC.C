f0f9d7e27fb81edeb0794629445ba1f51d0362a7
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-06 22:48:13 -0400

    use ana.358 for tracking

--- submit/JS_pp200_signal/pass3_jobC_nopileup/rundir/Fun4All_G4_sPHENIX_jobC.C
+++ submit/JS_pp200_signal/pass3_jobC_nopileup/rundir/Fun4All_G4_sPHENIX_jobC.C
@@ -1,12 +1,12 @@
 #include <GlobalVariables.C>
 
 #include <G4_Magnet.C>
-#include <G4_Micromegas.C>
 #include <G4_Production.C>
-#include <G4_Tracking.C>
+#include <Trkr_RecoInit.C>
+#include <Trkr_Reco.C>
 
 #include <ffamodules/FlagHandler.h>
-#include <ffamodules/XploadInterface.h>
+#include <ffamodules/CDBInterface.h>
 
 #include <fun4all/SubsysReco.h>
 #include <fun4all/Fun4AllServer.h>
@@ -24,9 +24,9 @@ R__LOAD_LIBRARY(libfun4all.so)
 int Fun4All_G4_sPHENIX_jobC(
   const int nEvents = 0,
   const int nSkipEvents = 0,
-  const std::string &inputFile1 = "DST_TRACKSEEDS_pythia8_Jet10-0000000062-00000.root",
-  const std::string &inputFile2 = "DST_CALO_CLUSTER_pythia8_Jet10-0000000062-00000.root",
-  const std::string &outputFile = "DST_TRACKS_pythia8_Jet10-0000000062-00000.root",
+  const std::string &inputFile1 = "DST_TRACKSEEDS_pythia8_Jet10-0000000006-00000.root",
+  const std::string &inputFile2 = "DST_CALO_CLUSTER_pythia8_Jet10-0000000006-00000.root",
+  const std::string &outputFile = "DST_TRACKS_pythia8_Jet10-0000000006-00000.root",
   const std::string &outdir = "."
   )
 {
@@ -43,13 +43,11 @@ int Fun4All_G4_sPHENIX_jobC(
   //===============
   // conditions DB flags
   //===============
-  Enable::XPLOAD = true;
+  Enable::CDB = true;
   // tag
-  rc->set_StringFlag("XPLOAD_TAG",XPLOAD::tag);
-  // database config
-  rc->set_StringFlag("XPLOAD_CONFIG",XPLOAD::config);
+  rc->set_StringFlag("CDB_GLOBALTAG",CDB::global_tag);
   // 64 bit timestamp
-  rc->set_uint64Flag("TIMESTAMP",XPLOAD::timestamp);
+  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
 
   // set up production relatedstuff
   Enable::PRODUCTION = true;
@@ -125,7 +123,7 @@ int Fun4All_G4_sPHENIX_jobC(
   se->run(nEvents);
 
   // terminate
-  XploadInterface::instance()->Print(); // print used DB files
+  CDBInterface::instance()->Print(); // print used DB files
   se->End();
   se->PrintTimer();
   std::cout << "All done" << std::endl;
