a136caba298586ce016582cf86154f4f07833373
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-07 11:09:46 -0400

    Add pAu_0_10fm pass1

---
+++ submit/pAu_0_10fm/pass1/condor/run_all.pl
@@ -0,0 +1,143 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Long;
+use DBI;
+
+sub getlastsegment;
+
+my $test;
+my $incremental;
+GetOptions("test"=>\$test, "increment"=>\$incremental);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+
+my $maxsubmit = $ARGV[0];
+my $hijing_runnumber = 6;
+my $hijing_dir = sprintf("/sphenix/sim/sim01/sphnxpro/mdc2/sHijing_HepMC/pAu_0_10fm");
+my $runnumber = 6;
+my $events = 1000;
+my $evtsperfile = 100000;
+my $nmax = $evtsperfile;
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d",$outdir,$runnumber);
+if ($outdir =~ /lustre/)
+{
+    my $storedir = $outdir;
+    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
+    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
+    system($makedircmd);
+}
+else
+{
+  mkpath($outdir);
+}
+my $nsubmit = 0;
+OUTER: for (my $segment=0; $segment<=getlastsegment(); $segment++)
+{
+    my $hijingdatfile = sprintf("%s/pAu_0_10fm-%010d-%05d.dat",$hijing_dir,$hijing_runnumber, $segment);
+    if (! -f $hijingdatfile)
+    {
+	print "could not locate $hijingdatfile\n";
+	next;
+    }
+#    print "hijing: $hijingdatfile\n";
+    my $sequence = $segment*$evtsperfile/$events;
+    for (my $n=0; $n<$nmax; $n+=$events)
+    {
+	my $outfile = sprintf("G4Hits_sHijing_pAu_0_10fm-%010d-%05d.root",$runnumber,$sequence);
+	$chkfile->execute($outfile);
+	if ($chkfile->rows == 0)
+	{
+	    my $tstflag="";
+	    if (defined $test)
+	    {
+		$tstflag="--test";
+	    }
+	    system("perl run_condor.pl $events $hijingdatfile $outdir $outfile $n $runnumber $sequence $tstflag");
+	    my $exit_value  = $? >> 8;
+	    if ($exit_value != 0)
+	    {
+		if (! defined $incremental)
+		{
+		    print "error from run_condor.pl\n";
+		    exit($exit_value);
+		}
+	    }
+	    else
+	    {
+		$nsubmit++;
+	    }
+	    if ($nsubmit >= $maxsubmit || $nsubmit >= 20000)
+	    {
+		print "maximum number of submissions reached, exiting\n";
+		last OUTER;
+	    }
+	}
+        $sequence++;
+    }
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
+
+sub getlastsegment()
+{
+    opendir(DH,$hijing_dir);
+    my @files = sort(readdir(DH));
+    closedir(DH);
+    my $last_segment = -1;
+    if ($files[$#files] =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	$last_segment = int($3);
+	print "last segment: $last_segment\n";
+    }
+    else
+    {
+	print "cannot parse $files[$#files] for segment number\n";
+	exit(1);
+    }
+    return $last_segment;
+}
