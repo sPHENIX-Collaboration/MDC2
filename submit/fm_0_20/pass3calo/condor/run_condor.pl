a596a9b4fda261d51b0f313194b87bce9d275a5e
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-08 09:30:52 -0400

    change run script to run_pass3calo_0_20fm.sh for fm_0_20/pass3calo

--- submit/fm_0_20/pass3calo/condor/run_condor.pl
+++ submit/fm_0_20/pass3calo/condor/run_condor.pl
@@ -19,7 +19,7 @@ my $localdir=`pwd`;
 chomp $localdir;
 my $baseprio = 53;
 my $rundir = sprintf("%s/../rundir",$localdir);
-my $executable = sprintf("%s/run_pass3calo.sh",$rundir);
+my $executable = sprintf("%s/run_pass3calo_0_20fm.sh",$rundir);
 my $nevents = $ARGV[0];
 my $infile0 = $ARGV[1];
 my $infile1 = $ARGV[2];
