60c67859cd9735d0b6879f45958fb07f260467dd
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-08 21:29:42 -0400

    add restart_held.sh for HF_pp200_signal/pass3_job0_nopileup

---
+++ submit/HF_pp200_signal/pass3_job0_nopileup/condor/restart_held.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied"
+  exit 0
+fi
+echo resetting $1
+grepvariable=$1-
+variable=$1
+echo $variable
+[ -f bla ] && rm bla
+condor_q | grep ' H ' | grep run_pass3_job0_nopileup_hf.sh | grep ${grepvariable} > bla
+[ ! -s bla ] && exit 0
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000006-"$1".job"}'`; do echo $i;  condor_submit log/${variable}/condor_${variable}$i; done
+[ -f bla ] && rm bla
