d02503b92f11e8e3e07d87d2389aee1c8a2e7822
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-08 22:18:46 -0400

    add pAu_0_10fm/pass3global

---
+++ submit/pAu_0_10fm/pass3global/rundir/run_pass3global_pau_0_10fm.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.358
+
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    echo $2 > inputfiles.list
+    echo $3 >> inputfiles.list
+    getinputfiles.pl --filelist inputfiles.list
+    if [ $? -ne 0 ]
+    then
+        cat inputfiles.list
+	echo error from getinputfiles.pl  --filelist inputfiles.list, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+    exit -1
+fi
+# arguments 
+# $1: number of events
+# $2: bbc g4hits input file
+# $3: truth g4hits input file
+# $4: output file
+# $5: output dir
+# $6: run number
+# $7: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(bbc g4hits file\): $2
+echo arg3 \(truth g4hits file\): $3
+echo arg4 \(output file\): $4
+echo arg5 \(output dir\): $5
+echo arg6 \(runnumber\): $6
+echo arg7 \(sequence\): $7
+
+runnumber=$(printf "%010d" $6)
+sequence=$(printf "%05d" $7)
+
+echo running root.exe -q -b Fun4All_G4_Global.C\($1,\"$2\",\"$3\",\"$4\",\"$5\"\)
+root.exe -q -b  Fun4All_G4_Global.C\($1,\"$2\",\"$3\",\"$4\",\"$5\"\)
+
+echo "script done"
