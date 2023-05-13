b07930e3f8fb7f8d5bfff1cf77a4c7c8655ad88d
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-13 13:41:29 -0400

    remove minio, fix restart for JS_pp200_signal/pass4_jobA_embed

--- submit/JS_pp200_signal/pass4_jobA_embed/condor/restart_held.sh
+++ submit/JS_pp200_signal/pass4_jobA_embed/condor/restart_held.sh
@@ -1,16 +1,15 @@
 #! /usr/bin/bash
 if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-fi
-echo $1
-variable=$1
-#exit 0
-condor_q | grep ' H ' | grep run_jobA_embed.sh | grep ${variable} | grep sHijing_0_20 > bla
-for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
-if [ -f tmplist ]
 then
-rm tmplist
+  echo "No arguments supplied"
+  exit 0
 fi
-for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000006-"$1".job"}'`; do echo $i >> tmplist ; done
-for i in `cat tmplist`; do condor_submit log/${variable}/condor_${variable}$i; done
+echo resetting $1
+variable=$1
+echo $variable
+[ -f bla ] && rm bla
+condor_q | grep ' H ' | grep run_pass4_jobA_embed_js.sh | grep ${variable} > bla
+[ ! -s bla ] && exit 0
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000006-"$1".job"}'`; do echo log/${variable}/condor_${variable}$i;  condor_submit log/${variable}/condor_${variable}$i; done
+[ -f bla ] && rm bla
