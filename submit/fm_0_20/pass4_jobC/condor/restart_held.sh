68d4b28d01690f353edd95e48cff052603ed41ab
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-20 13:59:59 -0400

    fix restart

--- submit/fm_0_20/pass4_jobC/condor/restart_held.sh
+++ submit/fm_0_20/pass4_jobC/condor/restart_held.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
-condor_q | grep ' H ' | grep run_jobC.sh | grep sHijing_0_20 > bla
+condor_q | grep ' H ' | grep run_pass4_jobC_fm_0_20.sh > bla
 for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
 for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/condor-0000000006-"$1".job"}'`; do condor_submit $i; done
