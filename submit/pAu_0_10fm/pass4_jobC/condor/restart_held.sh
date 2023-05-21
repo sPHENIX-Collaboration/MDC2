1ce0a2b93311e2912112256cba01058e93d1fdbc
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-21 18:31:10 -0400

    add pAu_0_10fm/pass4_jobC

---
+++ submit/pAu_0_10fm/pass4_jobC/condor/restart_held.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+condor_q | grep ' H ' | grep run_pass4_jobC_pau_0_10fm.sh > bla
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/condor-0000000006-"$1".job"}'`; do condor_submit $i; done
