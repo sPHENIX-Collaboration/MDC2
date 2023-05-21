84f94bf58cd71c1dbcd9adb84a4f96617c9ef9c8
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-21 14:39:12 -0400

    remove minio use from G4_Production.C

--- submit/single_particle/upsilon/pass1/rundir/G4_Production.C
+++ submit/single_particle/upsilon/pass1/rundir/G4_Production.C
@@ -20,32 +20,16 @@ namespace PRODUCTION
 void Production_CreateOutputDir()
 {
   PRODUCTION::SaveOutputDir = DstOut::OutputDir;
-  string toreplace("/sphenix/lustre01/sphnxpro");
-  string mkdircmd;
-  size_t strpos = DstOut::OutputDir.find(toreplace);
-  if (strpos == string::npos)
-  {
 // check if directory already exists, mkdirs can hang up the system if we have gazillions of them
-    DIR *dr;
-    struct dirent *en;
-    dr = opendir(DstOut::OutputDir.c_str());
-    if (dr)
-    {
-      closedir(dr);  // output directory exists - close it and do nothing
-      return;
-    }
-    mkdircmd = "mkdir -p " + DstOut::OutputDir;
-  }
-  else
+  DIR *dr;
+  struct dirent *en;
+  dr = opendir(DstOut::OutputDir.c_str());
+  if (dr)
   {
-    DstOut::OutputDir.replace(DstOut::OutputDir.begin(),
-                              DstOut::OutputDir.begin() + toreplace.size(),
-                              "sphenixS3");
-// for mcs3 we add the check in an if statement.
-// TSystem->Exec() returns always -1 when this macro is called from a macro, see
-// https://root.cern/root/html530/src/TSystem.cxx.html#xRCiVD
-    mkdircmd = "if [[ ! `mcs3 stat " + DstOut::OutputDir + "` ]]; then mcs3 mb " + DstOut::OutputDir + "; fi";
+    closedir(dr);  // output directory exists - close it and do nothing
+    return;
   }
+  string mkdircmd = "mkdir -p " + DstOut::OutputDir;
   gSystem->Exec(mkdircmd.c_str());
 }
 
