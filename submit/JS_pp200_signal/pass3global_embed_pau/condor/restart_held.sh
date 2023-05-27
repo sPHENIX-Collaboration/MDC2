41130b3a24577f1872d06fe1c3745d1d91d9efa8
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-27 14:28:21 -0400

    Add JS_pp200_signal/pass3global_embed_pau

---
+++ submit/JS_pp200_signal/pass3global_embed_pau/condor/restart_held.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+fi
+echo $1
+variable=$1
+#exit 0
+[ -f bla ] && rm bla
+condor_q | grep ' H ' | grep run_pass3global_embed_pau_js.sh | grep ${variable} | grep sHijing_0_20 > bla
+[ -f bla ] || exit 0
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+
+[ -f tmplist ] && rm tmplist
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000006-"$1".job"}'`; do echo $i >> tmplist ; done
+for i in `cat tmplist`; do condor_submit log/${variable}/condor_${variable}$i; done
