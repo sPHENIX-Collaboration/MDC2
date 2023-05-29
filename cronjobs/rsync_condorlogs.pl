3f7c8df0e4d1ec427500d6d89f051ea80695ca0a
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-05-28 23:39:43 -0400

    update crons

--- cronjobs/rsync_condorlogs.pl
+++ cronjobs/rsync_condorlogs.pl
@@ -13,6 +13,7 @@ push(@fmrange,"fm_0_488");
 push(@fmrange,"fm_0_20");
 push(@fmrange,"HF_pp200_signal");
 push(@fmrange,"JS_pp200_signal");
+push(@fmrange,"pAu_0_10fm");
 push(@fmrange,"pythia8_pp_mb");
 #push(@fmrange,"FixDST");
 push(@fmrange,"rawdata");
@@ -28,15 +29,18 @@ push(@passes,"pass2_10kHz");
 push(@passes,"pass2_25kHz");
 push(@passes,"pass2_50kHz_0_20fm");
 push(@passes,"pass2_embed");
+push(@passes,"pass2_embed_pau");
 push(@passes,"pass2_pi0_embed");
 push(@passes,"pass2_nopileup");
 push(@passes,"pass3_nopileup");
 push(@passes,"pass3calo");
 push(@passes,"pass3calo_50kHz_0_20fm");
 push(@passes,"pass3calo_embed");
+push(@passes,"pass3calo_embed_pau");
 push(@passes,"pass3distort");
 push(@passes,"pass3global");
 push(@passes,"pass3global_embed");
+push(@passes,"pass3global_embed_pau");
 push(@passes,"pass3jet_nopileup");
 push(@passes,"pass3_job0_nopileup");
 push(@passes,"pass3_jobA_nopileup");
@@ -44,17 +48,22 @@ push(@passes,"pass3_jobC_nopileup");
 push(@passes,"pass3trk");
 push(@passes,"pass3trk_test");
 push(@passes,"pass3trk_embed");
+push(@passes,"pass3trk_embed_pau");
 push(@passes,"pass3trk_10kHz");
 push(@passes,"pass3trk_25kHz");
 push(@passes,"pass3trk_50kHz_0_20fm");
 push(@passes,"pass4_job0");
 push(@passes,"pass4_jobA");
 push(@passes,"pass4_job0_embed");
+push(@passes,"pass4_job0_embed_pau");
 push(@passes,"pass4_jobA_embed");
+push(@passes,"pass4_jobA_embed_pau");
 push(@passes,"pass4_jobC_embed");
+push(@passes,"pass4_jobC_embed_pau");
 push(@passes,"pass4_jobC");
 push(@passes,"pass4jet");
 push(@passes,"pass4jet_embed");
+push(@passes,"pass4jet_embed_pau");
 push(@passes,"pass4jet_nopileup");
 push(@passes,"pass4trk");
 push(@passes,"pass4trk_10kHz");
@@ -81,7 +90,7 @@ foreach my $condorlogdir (sort keys %condorlogs)
     {
 	if (&dir_is_empty($condorlogdir) == 1)
 	{
-	    my $rsynccmd = sprintf("rsync -av %s/* %s",$condorlogdir, $condorlogs{$condorlogdir});
+	    my $rsynccmd = sprintf("rsync -av %s/ %s",$condorlogdir, $condorlogs{$condorlogdir});
 	    print "cmd: $rsynccmd\n";
 	    system($rsynccmd);
 	}
@@ -91,6 +100,7 @@ foreach my $condorlogdir (sort keys %condorlogs)
 	}
     }
 }
+print "all done\n";
 
 sub dir_is_empty
 {
