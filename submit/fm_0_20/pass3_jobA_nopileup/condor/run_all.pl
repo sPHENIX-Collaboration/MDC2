195780820c46fda431ef4d237e97c9cae63564b1
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-06-04 14:28:22 -0400

    remove minio for mkdir

--- submit/fm_0_20/pass3_jobA_nopileup/condor/run_all.pl
+++ submit/fm_0_20/pass3_jobA_nopileup/condor/run_all.pl
@@ -42,17 +42,7 @@ if (! -f "outdir.txt")
 my $outdir = `cat outdir.txt`;
 chomp $outdir;
 $outdir = sprintf("%s/run%04d",$outdir,$outrunnumber);
-if ($outdir =~ /lustre/)
-{
-    my $storedir = $outdir;
-    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
-    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
-    system($makedircmd);
-}
-else
-{
-  mkpath($outdir);
-}
+mkpath($outdir);
 
 my $condorlistfile =  sprintf("condor.list");
 if (-f $condorlistfile)
