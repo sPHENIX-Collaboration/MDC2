6bbc6f073ce65962ec8b2d9613cf9d96b066ea46
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-06-04 14:28:27 -0400

    remove minio for mkdir

--- submit/single_particle/pass2_embed/condor/run_all.pl
+++ submit/single_particle/pass2_embed/condor/run_all.pl
@@ -51,18 +51,7 @@ if (! -f "outdir.txt")
 my $outdir = `cat outdir.txt`;
 chomp $outdir;
 $outdir = sprintf("%s/run%04d/%s",$outdir,$runnumber, lc $particle);
-if ($outdir =~ /lustre/)
-{
-    my $storedir = $outdir;
-    $storedir =~ s/\/sphenix\/lustre01\/sphnxpro/sphenixS3/;
-    my $makedircmd = sprintf("mcs3 mb %s",$storedir);
-    system($makedircmd);
-}
-else
-{
-  mkpath($outdir);
-}
-
+mkpath($outdir);
 
 my %outfiletype = ();
 $outfiletype{"DST_BBC_G4HIT"} = 1;
