588a304fab96649010857f91287adddaf05024c1
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-06-16 15:23:10 -0400

    rename run_pass4jet_embed.sh to run_pass4jet_embed_js.sh for JS_pp200_signal/pass4jet_embed

---
+++ submit/JS_pp200_signal/pass4jet_embed/condor/restart_held.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+echo $1
+variable=$1
+#exit 0
+condor_q | grep ' H ' | grep run_pass4jet_embed.sh | grep ${variable} | grep sHijing_0_20 > bla
+#condor_q | grep ' H ' | grep run_pass4jet_embed_js.sh | grep ${variable} > bla
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+
+if [ -f tmplist ]
+then
+rm tmplist
+fi
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000006-"$1".job"}'`; do echo $i >> tmplist ; done
+
+if [ -f sedlist ]
+then
+rm sedlist
+fi
+for i in `cat tmplist`; do echo log/${variable}/condor_${variable}$i >> sedlist; done
+for i in `cat sedlist`; do  sed -i 's/6144MB/12288MB/' $i; echo $i; done
+for i in `cat sedlist`; do condor_submit $i; done
---
+++ submit/JS_pp200_signal/pass4jet_embed/rundir/run_pass4jet_embed_js.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.349
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    echo begin copy input files
+    date +%s
+    rsync -av $this_dir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+    echo end copy input files
+    date +%s
+else
+    echo condor scratch NOT set
+fi
+
+# arguments 
+# $1: number of events
+# $2: truth input file
+# $3: output file
+# $4: output dir
+# $5: jet trigger
+# $6: runnumber
+# $7: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(truth input file\): $2
+echo arg3 \(output file\): $3
+echo arg4 \(output dir\): $4
+echo arg5 \(jettrigger\): $5
+echo arg6 \(runnumber\): $6
+echo arg7 \(sequence\): $7
+
+echo running root.exe -q -b Fun4All_G4_Jets.C\($1,\"$2\",\"$3\",\"$4\"\)
+root.exe -q -b  Fun4All_G4_Jets.C\($1,\"$2\",\"$3\",\"$4\"\)
+echo "script done"
