092a41458cd75a2cdc7452e5f535a2ebc3e5c955
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-06-16 10:11:11 -0400

    fix restart with new script name for JS_pp200_signal/pass3global_embed

--- submit/JS_pp200_signal/pass3global_embed/condor/restart_held.sh
+++ submit/JS_pp200_signal/pass3global_embed/condor/restart_held.sh
@@ -1,12 +1,14 @@
 #! /usr/bin/bash
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
+  echo "No arguments supplied"
+  exit 0
 fi
 echo $1
 variable=$1
 #exit 0
 condor_q | grep ' H ' | grep run_pass3global_embed.sh | grep ${variable} | grep sHijing_0_20 > bla
+#condor_q | grep ' H ' | grep run_pass3global_embed_js.sh | grep ${variable}  > bla
 for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
 if [ -f tmplist ]
 then
