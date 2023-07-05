4d433a139b4113c09e461ef602ecb50e982e97df
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-07-05 15:00:51 -0400

    cosmetics

--- submit/fm_0_20/cemc_geo_fix/cemc_hits/condor/run_all.pl
+++ submit/fm_0_20/cemc_geo_fix/cemc_hits/condor/run_all.pl
@@ -57,7 +57,7 @@ mkpath($logdir);
 
 my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
 $dbh->{LongReadLen}=2000; # full file paths need to fit in here
-my $getfiles = $dbh->prepare("select filename from datasets where dsttype = 'G4HitsOld' and filename like '%sHijing_0_20fm%' and runnumber = $runnumber  order by segment") || die $DBI::errstr;
+my $getfiles = $dbh->prepare("select filename from datasets where dsttype = 'G4HitsOld' and filename like '%sHijing_0_20fm%' and runnumber = $runnumber order by segment") || die $DBI::errstr;
 my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
 
 my $nsubmit = 0;
