17bf710d8dcf980051e06d7df01288f0503c45e3
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-07-18 20:46:36 -0400

    move JS_pp200_signal/pass3_global_embed to JS_pp200_signal/pass3_bbcepd_embed

---
+++ submit/JS_pp200_signal/pass3_bbcepd_embed/condor/restart_held.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/bash
+if [ $# -eq 0 ]
+  then
+  echo "No arguments supplied"
+  exit 0
+fi
+echo $1
+variable=$1
+#exit 0
+#condor_q | grep ' H ' | grep run_pass3global_embed.sh | grep ${variable} | grep sHijing_0_20 > bla
+condor_q | grep ' H ' | grep run_pass3global_embed_js.sh | grep ${variable}  > bla
+[ -s bla ] || exit 0
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+if [ -f tmplist ]
+then
+rm tmplist
+fi
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000006-"$1".job"}'`; do echo $i >> tmplist ; done
+for i in `cat tmplist`; do condor_submit log/${variable}/condor_${variable}$i; done
