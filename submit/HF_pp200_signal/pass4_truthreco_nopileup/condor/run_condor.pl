8f64de6e7726a5448730139749aca9e5e1ac7411
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-08-05 14:34:12 -0400

    Add HF_pp200_signal/pass4_truthreco_nopileup

---
+++ submit/HF_pp200_signal/pass4_truthreco_nopileup/condor/run_condor.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+my $overwrite;
+GetOptions("test"=>\$test, "overwrite"=>\$overwrite);
+if ($#ARGV < 3)
+{
+    print "usage: run_condor.pl <events> <quarkfilter> <trkr g4hit file> <trkr cluster file> <tracks file> <truth file> <outfile> <outdir> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "--overwrite : overwrite existing jobfiles\n";
+    print "--test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 47;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass4_truthreco_nopileup_hf.sh",$rundir);
+my $nevents = $ARGV[0];
+my $quarkfilter = $ARGV[1];
+my $infile0 = $ARGV[2];
+my $infile1 = $ARGV[3];
+my $infile2 = $ARGV[4];
+my $infile3 = $ARGV[5];
+my $outfilename = $ARGV[6];
+my $dstoutdir = $ARGV[7];
+my $runnumber = $ARGV[8];
+my $sequence = $ARGV[9];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("%s-%010d-%05d",$quarkfilter,$runnumber,$sequence);
+my $logdir = sprintf("%s/log/%s",$localdir,$quarkfilter);
+mkpath($logdir);
+my $condorlogdir = sprintf("/tmp/HF_pp200_signal/pass4_truthreco_nopileup/%s",$quarkfilter);
+mkpath($condorlogdir);
+my $jobfile = sprintf("%s/condor_%s.job",$logdir,$suffix);
+if (-f $jobfile && ! defined $overwrite)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor_%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor_%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor_%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile0 $infile1 $infile2 $infile3 $outfilename $dstoutdir $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+#print F "accounting_group = group_sphenix.prod\n";
+print F "accounting_group = group_sphenix.mdc2\n";
+print F "accounting_group_user = sphnxpro\n";
+print F "Requirements = (CPU_Type == \"mdc2\")\n";
+print F "request_memory = 4096MB\n";
+print F "Priority = $baseprio\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile0,  $infile1, $infile2, $infile3, $outfilename, $dstoutdir, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio\n";
+close(F);
---
+++ submit/HF_pp200_signal/pass4_truthreco_nopileup/rundir/Fun4All_TruthReco.C
@@ -0,0 +1,102 @@
+#ifndef MACRO_FUN4ALLTRUTHRECO_C
+#define MACRO_FUN4ALLTRUTHRECO_C
+
+#include <GlobalVariables.C>
+
+#include <G4_Production.C>
+#include <Trkr_TruthTables.C>
+
+#include <ffamodules/FlagHandler.h>
+#include <ffamodules/CDBInterface.h>
+
+#include <fun4all/SubsysReco.h>
+#include <fun4all/Fun4AllDstInputManager.h>
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libffamodules.so)
+R__LOAD_LIBRARY(libfun4all.so)
+
+void Fun4All_TruthReco(
+  const int nEvents = 0,
+  const std::string &dst_trkr_g4hit = "DST_TRKR_G4HIT_pythia8_Jet10-0000000007-00000.root",
+  const std::string &dst_trkr_cluster = "DST_TRKR_CLUSTER_pythia8_Jet10-0000000007-00000.root",
+  const std::string &dst_tracks = "DST_TRACKS_pythia8_Jet10-0000000007-00000.root",
+  const std::string &dst_truth = "DST_TRUTH_pythia8_Jet10-0000000007-00000.root",
+  const std::string &outputFile = "DST_TRUTH_RECO_pythia8_Jet10-0000000007-00000.root",
+  const std::string &outdir = "."
+)
+{
+  gSystem->Load("libg4dst.so");
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  // make sure to printout random seeds for reproducibility
+  PHRandomSeed::Verbosity(1);
+
+  recoConsts *rc = recoConsts::instance();
+
+  //===============
+  // conditions DB flags
+  //===============
+  Enable::CDB = true;
+  // tag
+  rc->set_StringFlag("CDB_GLOBALTAG",CDB::global_tag);
+  // 64 bit timestamp
+  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
+
+  // set up production relatedstuff
+  Enable::PRODUCTION = true;
+  Enable::DSTOUT = true;
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  FlagHandler *flag = new FlagHandler();
+  se->registerSubsystem(flag);
+
+  build_truthreco_tables();
+
+  auto in = new Fun4AllDstInputManager("DSTin");
+  in->fileopen(dst_truth);
+  se->registerInputManager(in);
+  in = new Fun4AllDstInputManager("DSTinHit1");
+  in->fileopen(dst_trkr_g4hit);
+  se->registerInputManager(in);
+  in = new Fun4AllDstInputManager("DSTin2");
+  in->fileopen(dst_tracks);
+  se->registerInputManager(in);
+  in = new Fun4AllDstInputManager("DSTin3");
+  in->fileopen(dst_trkr_cluster);
+  se->registerInputManager(in);
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+
+  auto out = new Fun4AllDstOutputManager("DSTOUT", outputFile);
+  out->AddNode("Sync");
+  out->AddNode("EventHeader");
+  out->AddNode("PHG4ParticleSvtxMap");
+  out->AddNode("SvtxPHG4ParticleMap");
+  se->registerOutputManager(out);
+
+  se->run(nEvents);
+  // terminate
+  CDBInterface::instance()->Print(); // print used DB files
+  se->End();
+  se->PrintTimer();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+  gSystem->Exit(0);
+}
+
+#endif
