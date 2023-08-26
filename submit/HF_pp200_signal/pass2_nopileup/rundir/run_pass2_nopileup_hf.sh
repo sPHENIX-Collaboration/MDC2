3daf4c4b0aec095860ebd9235068d04b48171247
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-08-26 19:18:34 -0400

    use ana.373 for HF_pp200_signal/pass2_nopileup

--- submit/HF_pp200_signal/pass2_nopileup/rundir/run_pass2_nopileup_hf.sh
+++ submit/HF_pp200_signal/pass2_nopileup/rundir/run_pass2_nopileup_hf.sh
@@ -12,9 +12,9 @@ this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 echo running: $this_script $*
 
-ana_calo=ana.372
-ana_global=ana.372
-ana_pass3trk=ana.372
+ana_calo=ana.373
+ana_global=ana.373
+ana_pass3trk=ana.373
 
 source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n
 
