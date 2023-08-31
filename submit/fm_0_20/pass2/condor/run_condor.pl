57a7bafb82d981a7e3f3ffd52989433b31c1c743
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-08-31 09:50:31 -0400

    reduce memory to 7160MB for pass2/condor

--- submit/fm_0_20/pass2/condor/run_condor.pl
+++ submit/fm_0_20/pass2/condor/run_condor.pl
@@ -67,7 +67,7 @@ print F "accounting_group = group_sphenix.mdc2\n";
 print F "accounting_group_user = sphnxpro\n";
 print F "Requirements = (CPU_Type == \"mdc2\")\n";
 #print F "Requirements = (CPU_Type == \"mdc2_minio\")\n";
-print F "request_memory = 6136MB\n";
+print F "request_memory = 7160MB\n";
 print F "Priority = $baseprio\n";
 #print F "concurrency_limits = PHENIX_1000\n";
 print F "job_lease_duration = 3600\n";
