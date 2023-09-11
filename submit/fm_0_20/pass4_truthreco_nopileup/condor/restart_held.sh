e00a455a672ebb8eb31c56554ca555aa2a15f997
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-11 07:19:46 -0400

    run 7 for restart_held.sh

--- submit/fm_0_20/pass4_truthreco_nopileup/condor/restart_held.sh
+++ submit/fm_0_20/pass4_truthreco_nopileup/condor/restart_held.sh
@@ -1,28 +1,4 @@
 #! /usr/bin/bash
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-    exit 1
-fi
-echo $1
-variable=$1
-#exit 0
-condor_q | grep ' H ' | grep run_pass5_truthreco_js.sh | grep ${variable} > bla
-
-[ -s bla ] ||  exit 1
-
+condor_q | grep ' H ' | grep run_pass4_truthreco_nopileup_fm_0_20.sh > bla
 for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
-
-if [ -f tmplist ]
-then
-rm tmplist
-fi
-for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000007-"$1".job"}'`; do echo $i >> tmplist ; done
-
-if [ -f sedlist ]
-then
-rm sedlist
-fi
-for i in `cat tmplist`; do echo log/${variable}/condor_${variable}$i >> sedlist; done
-for i in `cat sedlist`; do  sed -i 's/4096MB/12288MB/' $i; echo $i; done
-for i in `cat sedlist`; do condor_submit $i; done
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/condor-0000000007-"$1".job"}'`; do  echo submitting $i; condor_submit $i; done
