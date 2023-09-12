43e0dd2fc74925f3641f98347f2da21d6de5265f
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-12 16:40:06 -0400

    Add JS_pp200_signal/pass3_bbcepd

---
+++ submit/JS_pp200_signal/pass3_bbcepd/condor/restart_held.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied"
+  exit 1
+fi
+echo $1
+variable=$1
+#exit 0
+condor_q | grep ' H ' | grep run_pass3_bbcepd_js.sh | grep ${variable}  > bla
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+if [ -f tmplist ]
+then
+rm tmplist
+fi
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "-0000000008-"$1".job"}'`; do echo $i >> tmplist ; done
+for i in `cat tmplist`; do condor_submit log/${variable}/condor_${variable}$i; done
