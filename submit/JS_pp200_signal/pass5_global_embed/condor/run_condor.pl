d97c8cd494c65aff71f508c89cc11b9c6109d8e8
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-16 14:22:48 -0400

    ana.376, new mbd output

--- submit/JS_pp200_signal/pass5_global_embed/condor/run_condor.pl
+++ submit/JS_pp200_signal/pass5_global_embed/condor/run_condor.pl
@@ -38,7 +38,7 @@ my $condorlistfile = sprintf("condor.list");
 my $suffix = sprintf("%s-%010d-%05d",$jettrigger,$runnumber,$sequence);
 my $logdir = sprintf("%s/log/%s",$localdir,$jettrigger);
 mkpath($logdir);
-my $condorlogdir = sprintf("/tmp/JS_pp200_signal/pass5_global_embed_js/%s",$jettrigger);
+my $condorlogdir = sprintf("/tmp/JS_pp200_signal/pass5_global_embed/%s",$jettrigger);
 mkpath($condorlogdir);
 my $jobfile = sprintf("%s/condor_%s.job",$logdir,$suffix);
 if (-f $jobfile && ! defined $overwrite)
