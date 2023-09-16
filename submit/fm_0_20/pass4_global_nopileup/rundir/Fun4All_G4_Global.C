c816b008ffe759b6440fafd3a3abe674265c09b5
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-16 14:51:58 -0400

    ana.376, new mbd output for fm_0_20/pass4_global_nopileup

--- submit/fm_0_20/pass4_global_nopileup/rundir/Fun4All_G4_Global.C
+++ submit/fm_0_20/pass4_global_nopileup/rundir/Fun4All_G4_Global.C
@@ -3,29 +3,27 @@
 #include <G4_Global.C>
 #include <G4_Production.C>
 
-#include <ffamodules/FlagHandler.h>
 #include <ffamodules/CDBInterface.h>
+#include <ffamodules/FlagHandler.h>
 
-#include <fun4all/SubsysReco.h>
-#include <fun4all/Fun4AllServer.h>
 #include <fun4all/Fun4AllDstInputManager.h>
 #include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/SubsysReco.h>
 
 #include <phool/PHRandomSeed.h>
 #include <phool/recoConsts.h>
-
 
 R__LOAD_LIBRARY(libffamodules.so)
 R__LOAD_LIBRARY(libfun4all.so)
 
 //________________________________________________________________________________________________
 int Fun4All_G4_Global(
-  const int nEvents = 0,
-  const std::string &inputFile1 = "DST_TRACKS_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000007-00001.root",
-  const std::string &inputFile2 = "DST_BBC_EPD_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000007-00001.root",
-  const std::string &outputFile = "DST_GLOBAL_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000007-00001.root",
-  const std::string &outdir = "."
-  )
+    const int nEvents = 0,
+    const std::string &inputFile1 = "DST_TRACKS_sHijing_0_20fm-0000000007-00000.root",
+    const std::string &inputFile2 = "DST_BBC_EPD_sHijing_0_20fm-0000000007-00000.root",
+    const std::string &outputFile = "DST_GLOBAL_sHijing_0_20fm-0000000007-00000.root",
+    const std::string &outdir = ".")
 {
   gSystem->Load("libg4dst.so");
   recoConsts *rc = recoConsts::instance();
@@ -35,9 +33,9 @@ int Fun4All_G4_Global(
   //===============
   Enable::CDB = true;
   // tag
-  rc->set_StringFlag("CDB_GLOBALTAG",CDB::global_tag);
+  rc->set_StringFlag("CDB_GLOBALTAG", CDB::global_tag);
   // 64 bit timestamp
-  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
+  rc->set_uint64Flag("TIMESTAMP", CDB::timestamp);
 
   // set up production relatedstuff
   Enable::PRODUCTION = true;
@@ -76,7 +74,7 @@ int Fun4All_G4_Global(
   auto out = new Fun4AllDstOutputManager("DSTOUT", outputFile);
   out->AddNode("Sync");
   out->AddNode("EventHeader");
-  out->AddNode("BbcPmtContainer");
+  out->AddNode("BbcPmtInfoContainer");
   out->AddNode("TOWERINFO_SIM_EPD");
   out->AddNode("TOWERINFO_CALIB_EPD");
   out->AddNode("GlobalVertexMap");
@@ -86,7 +84,7 @@ int Fun4All_G4_Global(
   se->run(nEvents);
 
   // terminate
-  CDBInterface::instance()->Print(); // print used DB files
+  CDBInterface::instance()->Print();  // print used DB files
   se->End();
   se->PrintTimer();
   std::cout << "All done" << std::endl;
