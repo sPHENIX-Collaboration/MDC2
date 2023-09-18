99aba75097a360aa87445ddea27a768e56a2ebfb
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-17 21:00:33 -0400

    run 7 without pp_mode for HF_pp200_signal/pass3_jobA_nopileup

--- submit/HF_pp200_signal/pass3_jobA_nopileup/rundir/Fun4All_G4_sPHENIX_jobA.C
+++ submit/HF_pp200_signal/pass3_jobA_nopileup/rundir/Fun4All_G4_sPHENIX_jobA.C
@@ -52,7 +52,7 @@ int Fun4All_G4_sPHENIX_jobA(
   DstOut::OutputFile = outputFile;
 
   // set pp tracking mode
-  TRACKING::pp_mode = true;
+//  TRACKING::pp_mode = true;
 
   // central tracking
   Enable::MVTX = true;
