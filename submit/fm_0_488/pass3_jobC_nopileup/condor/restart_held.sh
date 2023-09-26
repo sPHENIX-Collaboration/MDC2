40ae858319bd9bb67b8792cb2d5891802b562b4d
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-25 20:43:03 -0400

    add fm_0_488/pass3_jobC_nopileup

---
+++ submit/fm_0_488/pass3_jobC_nopileup/condor/restart_held.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+condor_q | grep ' H ' | grep run_pass3_jobC_nopileup_fm_0_488.sh  > bla
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/run7/condor-0000000007-"$1".job"}'`; do condor_submit $i; done
