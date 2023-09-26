eb18109b7e9b019f7216dc918ce53d5e7b057889
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-25 21:00:30 -0400

    add fm_0_488/pass4_truthreco_nopileup

---
+++ submit/fm_0_488/pass4_truthreco_nopileup/condor/restart_held.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+condor_q | grep ' H ' | grep run_pass4_truthreco_nopileup_fm_0_488.sh > bla
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/run7/condor-0000000007-"$1".job"}'`; do  echo submitting $i; condor_submit $i; done
