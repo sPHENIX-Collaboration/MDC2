4d0ba79fe329abbb8f45b681a5b0e835018fa8c7
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-09-28 16:58:18 -0400

    add HF_pp200_signal/pass5_global

---
+++ submit/HF_pp200_signal/pass5_global/condor/restart_held.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+condor_q | grep ' H ' | grep run_pass5_global_hf.sh > bla
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/condor-0000000008-"$1".job"}'`; do condor_submit $i; done
