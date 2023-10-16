c560e9f82056549c3c0798acfe2777219ac44c12
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-10-16 08:16:33 -0400

    Add HF_pp200_signal/pass5_truthreco_embed

---
+++ submit/HF_pp200_signal/pass5_truthreco_embed/rundir/run_pass5_truthreco_embed_hf.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.376
+
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    echo $2 > inputfiles.list
+    echo $3 >> inputfiles.list
+    echo $4 >> inputfiles.list
+    echo $5 >> inputfiles.list
+    getinputfiles.pl --filelist inputfiles.list
+    if [ $? -ne 0 ]
+    then
+        cat inputfiles.list
+	echo error from getinputfiles.pl --filelist inputfiles.list, exiting
+	exit -1
+    fi
+else
+ echo condor scratch NOT set
+ exit 1
+fi
+
+# arguments 
+# $1: number of output events
+# $2: dst_trkr_g4hit
+# $3: dst_trkr_cluster
+# $4: dst_tracks
+# $5: dst_truth
+# $6: output file
+# $7: output directory
+# $8: run number
+# $9: sequence
+# $10" quarkfilter
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(output events\) : $1
+echo arg2 \(dst_trkr_g4hit\): $2
+echo arg3 \(dst_trkr_cluster\): $3
+echo arg4 \(dst_tracks\): $4
+echo arg5 \(dst_truth\): $5
+echo arg6 \(output file\): $6
+echo arg7 \(output dir\): $7
+echo arg8 \(runnumber\): $8
+echo arg9 \(sequence\): $9
+echo arg10 \(quarkfilter\): ${10}
+
+runnumber=$(printf "%010d" $8)
+sequence=$(printf "%05d" $9)
+
+echo running root.exe -q -b Fun4All_TruthReco.C\($1,\"$2\",\"$3\",\"$4\",\"$5\",\"$6\",\"$7\"\)
+root.exe -q -b  Fun4All_TruthReco.C\($1,\"$2\",\"$3\",\"$4\",\"$5\",\"$6\",\"$7\"\)
+
+echo "script done"
