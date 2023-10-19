aa6ba10601fab54ea65b2f831f140109d4baf537
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-10-18 20:56:01 -0400

    add pythia8_pp_mb/pass4_truthreco_nopileup

---
+++ submit/pythia8_pp_mb/pass4_truthreco_nopileup/condor/run_condor.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+my $overwrite;
+GetOptions("test"=>\$test, "overwrite"=>\$overwrite);
+if ($#ARGV < 8)
+{
+    print "usage: run_condor.pl <events> <trkr g4hit file> <trkr cluster file> <tracks file> <truth file> <outfile> <outdir> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "--overwrite : overwrite existing jobfiles\n";
+    print "--test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 47;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass4_truthreco_nopileup_pp_mb.sh",$rundir);
+my $nevents = $ARGV[0];
+my $infile0 = $ARGV[1];
+my $infile1 = $ARGV[2];
+my $infile2 = $ARGV[3];
+my $infile3 = $ARGV[4];
+my $outfilename = $ARGV[5];
+my $dstoutdir = $ARGV[6];
+my $runnumber = $ARGV[7];
+my $sequence = $ARGV[8];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("-%010d-%05d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log/run%d",$localdir,$runnumber);
+mkpath($logdir);
+my $condorlogdir = sprintf("/tmp/pythia8_pp_mb/pass4_truthreco_nopileup/run%d",$runnumber);
+mkpath($condorlogdir);
+my $jobfile = sprintf("%s/condor%s.job",$logdir,$suffix);
+if (-f $jobfile && ! defined $overwrite)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile0 $infile1 $infile2 $infile3 $outfilename $dstoutdir $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+#print F "accounting_group = group_sphenix.prod\n";
+print F "accounting_group = group_sphenix.mdc2\n";
+print F "accounting_group_user = sphnxpro\n";
+print F "Requirements = (CPU_Type == \"mdc2\")\n";
+print F "request_memory = 2048MB\n";
+print F "Priority = $baseprio\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile0,  $infile1, $infile2, $infile3, $outfilename, $dstoutdir, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio\n";
+close(F);
