52fda7db32d3060631a3219566d0f6b14e5a86d3
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-10-20 09:22:42 -0400

    add restart.sh to fm_0_488/pass4_job0

---
+++ submit/fm_0_488/pass4_job0/condor/restart_held.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/bash
+
+run=7
+
+condor_q | grep ' H ' | grep run_pass4_job0_fm_0_488.sh  > bla
+
+[ -s bla ] ||  exit 1
+
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+
+[ -f tmplist ] && rm tmplist
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. -v run=${run} '{print "000000000"run"-"$1".job"}'`; do echo $i >> tmplist ; done
+
+[ -f sedlist ] && rm sedlist
+for i in `cat tmplist`; do echo log/run${run}/condor-$i >> sedlist; done
+for i in `cat sedlist`; do condor_submit $i; done
