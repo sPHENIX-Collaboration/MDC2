266f80caa60355b4f04cc85d94acc9f4342e4aa8
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-10-19 23:24:43 -0400

    add restart_held.sh to fm_0_488/pass5_global

--- submit/fm_0_488/pass5_global/condor/restart_held.sh
+++ submit/fm_0_488/pass5_global/condor/restart_held.sh
@@ -1,4 +1,16 @@
 #! /usr/bin/bash
-condor_q | grep ' H ' | grep run_pass5_global_fm_0_488.sh > bla
+
+run=7
+
+condor_q | grep ' H ' | grep run_pass5_global_fm_0_488.sh  > bla
+
+[ -s bla ] ||  exit 1
+
 for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
-for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/condor-0000000007-"$1".job"}'`; do condor_submit $i; done
+
+[ -f tmplist ] && rm tmplist
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. -v run=${run} '{print "000000000"run"-"$1".job"}'`; do echo $i >> tmplist ; done
+
+[ -f sedlist ] && rm sedlist
+for i in `cat tmplist`; do echo log/run${run}/condor-$i >> sedlist; done
+for i in `cat sedlist`; do condor_submit $i; done
