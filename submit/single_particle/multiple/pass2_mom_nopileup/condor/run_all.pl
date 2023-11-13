ccc0c1dc075c2d46d84be76205fb933a03e33ddb
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-11-13 08:54:34 -0500

    add single_particle/multiple/pass2_mom_nopileup

---
+++ submit/single_particle/multiple/pass2_mom_nopileup/condor/run_all.pl
@@ -0,0 +1,160 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber = 7;
+my $test;
+my $incremental;
+my $shared;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "shared" => \$shared);
+if ($#ARGV < 3)
+{
+    print "usage: run_all.pl <number of jobs> <particle> <pmin> <pmax>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+my $maxsubmit = $ARGV[0];
+my $particle = lc $ARGV[1];
+my $pmin = $ARGV[2];
+my $pmax = $ARGV[3];
+my $filetype="single";
+my $partprop = sprintf("%s_p_%d_%d",$particle,$pmin,$pmax);
+$filetype=sprintf("%s_%sMeV",$filetype,$partprop);
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my @outdir = ();
+open(F,"outdir.txt");
+while (my $line = <F>)
+{
+    chomp $line;
+    $line = sprintf("%s/run%04d/%s",$line,$runnumber,$particle);
+    mkpath($line);
+    push(@outdir,$line);
+}
+close(F);
+
+
+my %outfiletype = ();
+$outfiletype{"DST_CALO_CLUSTER"} = $outdir[0];
+foreach my $type (sort keys %outfiletype)
+{
+    print "type $type, dir: $outfiletype{$type}\n";
+} 
+#die;
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename from datasets where dsttype = 'G4Hits' and filename like '%$filetype%' and runnumber = $runnumber order by filename") || die $DBI::errstr;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::errstr;
+while (my @res = $getfiles->fetchrow_array())
+{
+    my $lfn = $res[0];
+#    print "found $lfn\n";
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+        my $foundall = 1;
+	foreach my $type (sort keys %outfiletype)
+	{
+            my $lfn =  sprintf("%s_%s-%010d-%05d.root",$type,$filetype,$runnumber,$segment);
+#            print "checking for $lfn\n";
+	    $chkfile->execute($lfn);
+	    if ($chkfile->rows > 0)
+	    {
+		next;
+	    }
+	    else
+	    {
+		$foundall = 0;
+		last;
+	    }
+	}
+	if ($foundall == 1)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $calooutfilename = sprintf("DST_CALO_CLUSTER_%s-%010d-%05d.root",$filetype,$runnumber,$segment);
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %s %d %d %s", $outevents, $particle, $filetype, $lfn, $calooutfilename, $outdir[0],$runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if ($maxsubmit != 0 && $nsubmit >= $maxsubmit)
+	{
+	    print "maximum number of submissions reached, exiting\n";
+	    last;
+	}
+    }
+}
+
+$getfiles->finish();
+$chkfile->finish();
+$dbh->disconnect;
+
+my $jobfile = sprintf("condor.job");
+if (defined $shared)
+{
+ $jobfile = sprintf("condor.job.shared");
+}
+if (! -f $jobfile)
+{
+    print "could not find $jobfile\n";
+    exit(1);
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit $jobfile\n";
+    }
+    else
+    {
+	system("condor_submit $jobfile");
+    }
+}
---
+++ submit/single_particle/multiple/pass2_mom_nopileup/condor/run_condor.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+
+my $test;
+GetOptions("test"=>\$test);
+if ($#ARGV < 3)
+{
+    print "usage: run_condor.pl <events> <filetype> <infile> <calo outfile>  <calo outdir> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 72;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass2_mom_nopileup.sh",$rundir);
+my $nevents = $ARGV[0];
+my $particle = $ARGV[1];
+my $filetype = $ARGV[2];
+my $infile = $ARGV[3];
+my $calooutfile = $ARGV[4];
+my $calodstoutdir = $ARGV[5];
+my $runnumber = $ARGV[6];
+my $sequence = $ARGV[7];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("%s-%010d-%05d",$filetype,$runnumber,$sequence);
+my $logdir = sprintf("%s/log/run%d/%s",$localdir,$runnumber,$particle);
+if (! -d $logdir)
+{
+  mkpath($logdir);
+}
+my $condorlogdir = sprintf("/tmp/single_particle/multiple/pass2_mom_nopileup/run%d/%s",$runnumber,$particle);
+if (! -d $condorlogdir)
+{
+  mkpath($condorlogdir);
+}
+my $jobfile = sprintf("%s/condor_%s.job",$logdir,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor_%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor_%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor_%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile $calooutfile $calodstoutdir $runnumber $sequence $filetype\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+#print F "accounting_group = group_sphenix.prod\n";
+print F "accounting_group = group_sphenix.mdc2\n";
+print F "accounting_group_user = sphnxpro\n";
+print F "Requirements = (CPU_Type == \"mdc2\")\n";
+print F "request_memory = 2048MB\n";
+print F "Priority = $baseprio\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile, $calooutfile, $calodstoutdir, $runnumber, $sequence, $filetype, $outfile, $errfile, $condorlogfile, $rundir, $baseprio\n";
+close(F);
