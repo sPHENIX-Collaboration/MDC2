cd3f291a2192f7c5171369662b7f3e172512c82a
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-12-01 13:12:30 -0500

    add cosmic/pass1

---
+++ submit/cosmic/pass1/condor/run_all.pl
@@ -0,0 +1,118 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Long;
+
+my $test;
+my $incremental;
+my $killexist;
+my $runnumber = 11;
+my $events = 1000;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "killexist" => \$killexist);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--killexist : delete output file if it already exists (but no jobfile)\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $filetype = sprintf("cosmic");
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d/%s",$outdir,$runnumber);
+mkpath($outdir);
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $logdir = sprintf("%s/log/run%d",$localdir,$runnumber);
+my $nsubmit = 0;
+my $njob = 0;
+OUTER: for (my $isub = 0; $isub < $maxsubmit; $isub++)
+{
+    my $jobfile = sprintf("%s/condor-%010d-%05d.job",$logdir,$runnumber,$njob);
+    while (-f $jobfile)
+    {
+	$njob++;
+	$jobfile = sprintf("%s/condor-%010d-%05d.job",$logdir,$runnumber,$njob);
+    }
+    print "using jobfile $jobfile\n";
+    my $outfile = sprintf("G4Hits_%s-%010d-%05d.root",$filetype, $runnumber,$njob);
+    my $fulloutfile = sprintf("%s/%s",$outdir,$outfile);
+    print "out: $fulloutfile\n";
+    if (defined $killexist)
+    {
+	if (-f $fulloutfile)
+	{
+	    unlink  $fulloutfile;
+	}
+    }
+    if (! -f $fulloutfile)
+    {
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	system("perl run_condor.pl $events $outdir $outfile $runnumber $njob $tstflag");
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($nsubmit >= $maxsubmit) || $nsubmit >= 20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, exiting\n";
+	    last OUTER;
+	}
+    }
+    else
+    {
+	print "output file already exists\n";
+	$njob++;
+    }
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
