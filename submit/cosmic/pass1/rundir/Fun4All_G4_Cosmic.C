2ad4093ff222f48345b776a565bcb4d4e1256f9c
sPHENIX Production Account <pinkenburg@bnl.gov>
2023-12-01 13:11:44 -0500

    add cosmic/pass1

---
+++ submit/cosmic/pass1/rundir/Fun4All_G4_Cosmic.C
@@ -0,0 +1,232 @@
+#ifndef MACRO_FUN4ALLG4COSMIC_C
+#define MACRO_FUN4ALLG4COSMIC_C
+
+#include <GlobalVariables.C>
+
+#include <G4Setup_sPHENIX.C>
+#include <G4_Bbc.C>
+#include <G4_Global.C>
+#include <G4_Input.C>
+#include <G4_Production.C>
+#include <G4_TrkrSimulation.C>
+
+#include <phpythia8/PHPy8JetTrigger.h>
+
+#include <ffamodules/FlagHandler.h>
+#include <ffamodules/HeadReco.h>
+#include <ffamodules/SyncReco.h>
+#include <ffamodules/CDBInterface.h>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllSyncManager.h>
+#include <fun4all/Fun4AllUtils.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libffamodules.so)
+
+int Fun4All_G4_Cosmic(
+  const int nEvents = 1,
+  const string &outputFile = "G4Hits_cosmic-0000011-00000.root",
+  const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  PHRandomSeed::Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You can either set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  //  rc->set_IntFlag("RANDOMSEED", 12345);
+  //int seedValue = 491258969;
+  //rc->set_IntFlag("RANDOMSEED", seedValue);
+
+  //===============
+  // conditions DB flags
+  //===============
+  Enable::CDB = true;
+  // global tag
+  rc->set_StringFlag("CDB_GLOBALTAG",CDB::global_tag);
+  // 64 bit timestamp
+  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
+
+  pair<int, int> runseg = Fun4AllUtils::GetRunSegment(outputFile);
+  int runnumber=runseg.first;
+  int segment=runseg.second;
+  if (runnumber != 0)
+  {
+    rc->set_IntFlag("RUNNUMBER",runnumber);
+    Fun4AllSyncManager *syncman = se->getSyncManager();
+    syncman->SegmentNumber(segment);
+  }
+
+  //===============
+  // Input options
+  //===============
+  // verbosity setting (applies to all input managers)
+  Input::VERBOSITY = 0;
+
+  Input::COSMIC = true;
+
+  InputInit();
+
+  // register all input generators with Fun4All
+  InputRegister();
+
+  SyncReco *sync = new SyncReco();
+  se->registerSubsystem(sync);
+
+  HeadReco *head = new HeadReco();
+  se->registerSubsystem(head);
+
+  FlagHandler *flag = new FlagHandler();
+  se->registerSubsystem(flag);
+
+  // set up production relatedstuff
+  Enable::PRODUCTION = true;
+
+  //======================
+  // Write the DST
+  //======================
+
+  Enable::DSTOUT = true;
+  Enable::DSTOUT_COMPRESS = false;
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  //Option to convert DST to human command readable TTree for quick poke around the outputs
+  //  Enable::DSTREADER = true;
+
+  //======================
+  // What to run
+  //======================
+
+  // Global options (enabled for all enables subsystems - if implemented)
+  //  Enable::ABSORBER = true;
+  //  Enable::OVERLAPCHECK = true;
+  //  Enable::VERBOSITY = 1;
+
+  Enable::BBC = true;
+//  Enable::BBCFAKE = true;  // Smeared vtx and t0, use if you don't want real BBC in simulation
+
+  Enable::PIPE = true;
+//  Enable::PIPE_ABSORBER = true;
+
+  // central tracking
+  Enable::MVTX = true;
+
+  Enable::INTT = true;
+
+  Enable::TPC = true;
+
+  Enable::MICROMEGAS = true;
+
+  //  cemc electronics + thin layer of W-epoxy to get albedo from cemc
+  //  into the tracking, cannot run together with CEMC
+  //  Enable::CEMCALBEDO = true;
+
+  Enable::CEMC = true;
+
+  Enable::HCALIN = true;
+
+  Enable::MAGNET = true;
+//  Enable::MAGNET_ABSORBER = false;
+
+  Enable::HCALOUT = true;
+
+  Enable::EPD = true;
+
+  //! forward flux return plug door. Out of acceptance and off by default.
+//  Enable::PLUGDOOR = true;
+  Enable::PLUGDOOR_BLACKHOLE = true;
+//  Enable::PLUGDOOR_ABSORBER = true;
+
+  // new settings using Enable namespace in GlobalVariables.C
+  Enable::BLACKHOLE = true;
+  Enable::BLACKHOLE_FORWARD_SAVEHITS = false; // disable forward/backward hits
+  //Enable::BLACKHOLE_SAVEHITS = false; // turn off saving of bh hits
+  //BlackHoleGeometry::visible = true;
+
+
+  //---------------
+  // Magnet Settings
+  //---------------
+
+  //  const string magfield = "1.5"; // alternatively to specify a constant magnetic field, give a float number, which will be translated to solenoidal field in T, if string use as fieldmap name (including path)
+  //  G4MAGNET::magfield = string(getenv("CALIBRATIONROOT")) + string("/Field/Map/sPHENIX.2d.root");  // default map from the calibration database
+//  G4MAGNET::magfield_rescale = -1.4 / 1.5;  // make consistent with expected Babar field strength of 1.4T
+
+
+  // Initialize the selected subsystems
+  G4Init();
+
+  //---------------------
+  // GEANT4 Detector description
+  //---------------------
+    G4Setup();
+
+  //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+  if (Enable::DSTOUT)
+  {
+    string FullOutFile = DstOut::OutputFile;
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+    se->registerOutputManager(out);
+  }
+  //-----------------
+  // Event processing
+  //-----------------
+  // if we use a negative number of events we go back to the command line here
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  // if we run the particle generator and use 0 it'll run forever
+  if (nEvents == 0 && !Input::HEPMC && !Input::READHITS)
+  {
+    cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+    cout << "it will run forever, so I just return without running anything" << endl;
+    return 0;
+  }
+
+  se->skip(skip);
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  CDBInterface::instance()->Print(); // print used DB files
+  se->End();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+
+  gSystem->Exit(0);
+  return 0;
+}
+#endif
---
+++ submit/cosmic/pass1/rundir/run_pass1_cosmic.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n ana.391
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+else
+    echo condor scratch NOT set
+    exit 1
+fi
+
+# arguments 
+# $1: number of events
+# $2: output file
+# $3: output dir
+# $4: runnumber
+# $5: sequence
+
+echo 'here comes your environment'
+
+printenv
+
+echo arg1 \(events\) : $1
+echo arg2 \(output file\): $2
+echo arg3 \(output dir\): $3
+echo arg4 \(runnumber\): $4
+echo arg5 \(sequence\): $5
+
+runnumber=$(printf "%010d" $4)
+sequence=$(printf "%05d" $5)
+
+echo running root.exe -q -b Fun4All_G4_Cosmic.C\($1,\"$2\",\"$3\"\)
+root.exe -q -b Fun4All_G4_Cosmic.C\($1,\"$2\",\"$3\"\)
+
+echo "script done"
