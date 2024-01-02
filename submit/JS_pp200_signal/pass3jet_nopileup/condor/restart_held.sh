cef9a9ef2ab6ad5402d95504e63d1dacd19ecd73
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-01-02 06:38:21 -0500

    add restart_held.sh to JS_pp200_signal/pass3jet_nopileup

---
+++ submit/JS_pp200_signal/pass3jet_nopileup/condor/restart_held.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+echo $1
+variable=$1
+run=11
+#exit 0
+condor_q | grep ' H ' | grep run_pass3jet_nopileup_js.sh | grep ${variable} > bla
+
+[ -s bla ] ||  exit 1
+
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+
+[ -f tmplist ] && rm tmplist
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. -v run=${run} '{print "-00000000"run"-"$1".job"}'`; do echo $i >> tmplist ; done
+
+[ -f sedlist ] && rm sedlist
+for i in `cat tmplist`; do echo log/run${run}/${variable}/condor_${variable}$i >> sedlist; done
+#for i in `cat sedlist`; do  sed -i 's/6144MB/12288MB/' $i; echo $i; done
+for i in `cat sedlist`; do condor_submit $i; done
+
