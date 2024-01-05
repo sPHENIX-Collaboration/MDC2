5c824de804b791c63d7efc13dd35008ea3f0f799
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-01-05 15:12:34 -0500

    change bash script name to run_pass2_0_20fm.sh for fm_0_20/pass2

--- submit/fm_0_20/pass2/condor/run_condor.pl
+++ submit/fm_0_20/pass2/condor/run_condor.pl
@@ -19,7 +19,7 @@ my $localdir=`pwd`;
 chomp $localdir;
 my $baseprio = 52;
 my $rundir = sprintf("%s/../rundir",$localdir);
-my $executable = sprintf("%s/run_pileup_0_20fm.sh",$rundir);
+my $executable = sprintf("%s/run_pass2_0_20fm.sh",$rundir);
 my $nevents = $ARGV[0];
 my $infile = $ARGV[1];
 my $backgroundlist = $ARGV[2];
