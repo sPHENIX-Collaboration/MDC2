0b6ebb2d18313f5a0816b3a8522bd2de2e56d1f3
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-01-24 13:15:14 -0500

    Add EPOS

---
+++ submit/EPOS/pass1/condor/run_all.pl
@@ -0,0 +1,148 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Long;
+use DBI;
+
+sub getlastsegment;
+
+my $test;
+my $incremental;
+GetOptions("test"=>\$test, "increment"=>\$incremental);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+
+my $maxsubmit = $ARGV[0];
+my $epos_runnumber = 1;
+my $epos_dir = sprintf("/sphenix/sim/sim01/sphnxpro/mdc2/EPOS/AuAu");
+my $runnumber = 10;
+my $events = 400;
+$events = 200;
+#$events = 100; # for ftfp_bert_hp
+my $evtsperfile = 400;
+my $nmax = $evtsperfile;
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d",$outdir,$runnumber);
+mkpath($outdir);
+
+my $nsubmit = 0;
+my $lastsegment=getlastsegment();
+OUTER: for (my $segment=0; $segment<=$lastsegment; $segment++)
+{
+    my $eposdatfile = sprintf("%s/EPOS_AuAu-%010d-%05d.dat",$epos_dir,$epos_runnumber, $segment);
+    if (! -f $eposdatfile)
+    {
+	print "could not locate $eposdatfile\n";
+	next;
+    }
+#    print "epos: $eposdatfile\n";
+    my $sequence = $segment*$evtsperfile/$events;
+    for (my $n=0; $n<$nmax; $n+=$events)
+    {
+	my $outfile = sprintf("G4Hits_EPOS-%010d-%06d.root",$runnumber,$sequence);
+	$chkfile->execute($outfile);
+	if ($chkfile->rows == 0)
+	{
+	    my $tstflag="";
+	    if (defined $test)
+	    {
+		$tstflag="--test";
+	    }
+	    system("perl run_condor.pl $events $eposdatfile $outdir $outfile $n $runnumber $sequence $tstflag");
+	    my $exit_value  = $? >> 8;
+	    if ($exit_value != 0)
+	    {
+		if (! defined $incremental)
+		{
+		    print "error from run_condor.pl\n";
+		    exit($exit_value);
+		}
+	    }
+	    else
+	    {
+		$nsubmit++;
+	    }
+	    if ($nsubmit >= $maxsubmit || $nsubmit >= 20000)
+	    {
+		print "maximum number of submissions reached, exiting\n";
+		last OUTER;
+	    }
+	}
+#	else
+#	{
+#	    print "$outfile exists\n";
+#	}
+        $sequence++;
+    }
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
+
+sub getlastsegment()
+{
+    opendir(DH,$epos_dir);
+    my @tmpfiles = sort(readdir(DH));
+    closedir(DH);
+    my @files = ();
+    foreach my $f (@tmpfiles)
+    {
+	if ($f =~ /0_20fm/)
+	{
+	    push(@files,$f);
+	}
+    }
+    my $last_segment = -1;
+    if ($files[$#files] =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	$last_segment = int($3);
+    }
+    else
+    {
+	print "cannot parse $files[$#files] for segment number\n";
+	exit(1);
+    }
+    return $last_segment;
+}
---
+++ submit/EPOS/pass1/rundir/Fun4All_G4_Pass1.C
@@ -0,0 +1,237 @@
+#ifndef MACRO_FUN4ALLG4SPHENIX_C
+#define MACRO_FUN4ALLG4SPHENIX_C
+
+#include <GlobalVariables.C>
+
+#include <G4Setup_sPHENIX.C>
+#include <G4_Mbd.C>
+#include <G4_Input.C>
+#include <G4_Production.C>
+#include <G4_TrkrSimulation.C>
+
+#include <ffamodules/FlagHandler.h>
+#include <ffamodules/HeadReco.h>
+#include <ffamodules/SyncReco.h>
+#include <ffamodules/CDBInterface.h>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+#include <fun4all/Fun4AllSyncManager.h>
+#include <fun4all/Fun4AllUtils.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libffamodules.so)
+
+int Fun4All_G4_Pass1(
+    const int nEvents = 1,
+    const string &inputFile = "/sphenix/sim/sim01/sphnxpro/MDC1/sHijing_HepMC/data/sHijing_0_20fm-0000000001-00000.dat",
+    const string &outputFile = "G4Hits_sHijing_0_20fm-0000000050-00000.root",
+    const int skip = 0,
+    const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(0);
+
+  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  PHRandomSeed::Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You can either set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  //  rc->set_IntFlag("RANDOMSEED", 12345);
+
+  //===============
+  // conditions DB flags
+  //===============
+  Enable::CDB = true;
+  // global tag
+  rc->set_StringFlag("CDB_GLOBALTAG",CDB::global_tag);
+  // 64 bit timestamp
+  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
+
+// this extracts the runnumber and segment from the output filename
+// and sets this so the server can pick it up
+  pair<int, int> runseg = Fun4AllUtils::GetRunSegment(outputFile);
+  int runnumber=runseg.first;
+  int segment=runseg.second;
+  if (runnumber != 0)
+  {
+    rc->set_IntFlag("RUNNUMBER",runnumber);
+    Fun4AllSyncManager *syncman = se->getSyncManager();
+    syncman->SegmentNumber(segment);
+  }
+
+  //===============
+  // Input options
+  //===============
+  // verbosity setting (applies to all input managers)
+  Input::VERBOSITY = 1; // so we get prinouts of the event number
+  Input::HEPMC = true;
+  
+  INPUTHEPMC::filename = inputFile;
+  INPUTHEPMC::REACTIONPLANERAND = true;
+  // Event pile up simulation with collision rate in Hz MB collisions.
+  //Input::PILEUPRATE = 100e3;
+  // Enable this is emulating the nominal pp/pA/AA collision vertex distribution
+  Input::BEAM_CONFIGURATION = Input::AA_COLLISION; // for 2023 sims we want the AA geometry for no pileup sims
+
+  //-----------------
+  // Initialize the selected Input/Event generation
+  //-----------------
+  // This creates the input generator(s)
+  InputInit();
+
+  //--------------
+  // Set Input Manager specific options
+  //--------------
+  // can only be set after InputInit() is called
+
+  if (Input::HEPMC)
+  {
+    //! apply sPHENIX nominal beam parameter with 2mrad crossing as defined in sPH-TRG-2020-001
+    Input::ApplysPHENIXBeamParameter(INPUTMANAGER::HepMCInputManager);
+    if (Input::PILEUPRATE > 0)
+    {
+      // Copy vertex settings from foreground hepmc input
+      INPUTMANAGER::HepMCPileupInputManager->CopyHelperSettings(INPUTMANAGER::HepMCInputManager);
+      // and then modify the ones you want to be different
+      // INPUTMANAGER::HepMCPileupInputManager->set_vertex_distribution_width(100e-4,100e-4,8,0);
+    }
+  }
+  // register all input generators with Fun4All
+  InputRegister();
+
+  SyncReco *sync = new SyncReco();
+  se->registerSubsystem(sync);
+
+  FlagHandler *flag = new FlagHandler();
+  se->registerSubsystem(flag);
+
+  HeadReco *head = new HeadReco();
+  se->registerSubsystem(head);
+
+  // set up production relatedstuff
+    Enable::PRODUCTION = true;
+
+  //======================
+  // Write the DST
+  //======================
+
+  Enable::DSTOUT = true;
+
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  //======================
+  // What to run
+  //======================
+  // Global options (enabled for all enables subsystems - if implemented)
+  //  Enable::ABSORBER = true;
+  //  Enable::OVERLAPCHECK = true;
+  //  Enable::VERBOSITY = 1;
+
+  Enable::MBD = true;
+
+  Enable::PIPE = true;
+
+  // central tracking
+  Enable::MVTX = true;
+
+  Enable::INTT = true;
+
+  Enable::TPC = true;
+
+  Enable::MICROMEGAS = true;
+
+  Enable::CEMC = true;
+
+  Enable::HCALIN = true;
+
+  Enable::MAGNET = true;
+
+  Enable::HCALOUT = true;
+
+  Enable::EPD = true;
+
+  //! forward flux return plug door. Out of acceptance and off by default.
+  Enable::PLUGDOOR = true;
+  //Enable::PLUGDOOR_BLACKHOLE = true;
+
+  // new settings using Enable namespace in GlobalVariables.C
+  Enable::BLACKHOLE = true;
+  Enable::BLACKHOLE_FORWARD_SAVEHITS = false; // disable forward/backward hits
+  //Enable::BLACKHOLE_SAVEHITS = false; // turn off saving of bh hits
+  //BlackHoleGeometry::visible = true;
+
+  // Initialize the selected subsystems
+  G4Init();
+
+  G4Setup();
+
+  //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::PRODUCTION)
+  {
+     Production_CreateOutputDir();
+  }
+
+  if (Enable::DSTOUT)
+  {
+    string FullOutFile = DstOut::OutputFile;
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+    if (Enable::DSTOUT_COMPRESS) DstCompress(out);
+    se->registerOutputManager(out);
+  }
+  //-----------------
+  // Event processing
+  //-----------------
+
+  // if we use a negative number of events we go back to the command line here
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  // if we run the particle generator and use 0 it'll run forever
+  if (nEvents == 0 && !Input::HEPMC && !Input::READHITS)
+  {
+    cout << "using 0 for number of events is a bad idea when using particle generators" << endl;
+    cout << "it will run forever, so I just return without running anything" << endl;
+    return 0;
+  }
+
+  se->skip(skip);
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  CDBInterface::instance()->Print(); // print used DB files
+  se->End();
+  se->PrintTimer();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+
+  gSystem->Exit(0);
+  return 0;
+}
+#endif
