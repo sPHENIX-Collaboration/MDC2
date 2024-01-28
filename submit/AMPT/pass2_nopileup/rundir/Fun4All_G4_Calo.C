f35b1c52b8f9c6c5cd760a5199e49bf64152e24b
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-01-28 11:49:10 -0500

    add AMPT/pass2_nopileup

---
+++ submit/AMPT/pass2_nopileup/rundir/Fun4All_G4_Calo.C
@@ -0,0 +1,220 @@
+#ifndef MACRO_FUN4ALLG4CALO_C
+#define MACRO_FUN4ALLG4CALO_C
+
+#include <GlobalVariables.C>
+
+#include <G4_CEmc_Spacal.C>
+#include <G4_HcalIn_ref.C>
+#include <G4_HcalOut_ref.C>
+#include <G4_Input.C>
+#include <G4_Production.C>
+#include <G4_TopoClusterReco.C>
+
+#include <ffamodules/FlagHandler.h>
+#include <ffamodules/CDBInterface.h>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+#include <phool/PHRandomSeed.h>
+#include <phool/recoConsts.h>
+
+R__LOAD_LIBRARY(libffamodules.so)
+R__LOAD_LIBRARY(libfun4all.so)
+
+int Fun4All_G4_Calo(
+    const int nEvents = 1,
+    const string &inputFile0 = "G4Hits_AMPT-0000000010-00000.root",
+    const string &outputFile = "DST_CALO_CLUSTER_AMPT-0000000010-00000.root",
+    const string &outdir = ".")
+{
+  Fun4AllServer *se = Fun4AllServer::instance();
+  se->Verbosity(1);
+
+  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  PHRandomSeed::Verbosity(1);
+
+  // just if we set some flags somewhere in this macro
+  recoConsts *rc = recoConsts::instance();
+  // By default every random number generator uses
+  // PHRandomSeed() which reads /dev/urandom to get its seed
+  // if the RANDOMSEED flag is set its value is taken as seed
+  // You can either set this to a random value using PHRandomSeed()
+  // which will make all seeds identical (not sure what the point of
+  // this would be:
+  //  rc->set_IntFlag("RANDOMSEED",PHRandomSeed());
+  // or set it to a fixed value so you can debug your code
+  //  rc->set_IntFlag("RANDOMSEED", 12345);
+  Enable::CDB = true;
+  rc->set_StringFlag("CDB_GLOBALTAG",CDB::global_tag);
+  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
+
+  //===============
+  // Input options
+  //===============
+  // verbosity setting (applies to all input managers)
+  Input::VERBOSITY = 1;
+  // First enable the input generators
+  // Either:
+  // read previously generated g4-hits files, in this case it opens a DST and skips
+  // the simulations step completely. The G4Setup macro is only loaded to get information
+  // about the number of layers used for the cell reco code
+  Input::READHITS = true;
+  INPUTREADHITS::filename[0] = inputFile0;
+
+  //-----------------
+  // Initialize the selected Input/Event generation
+  //-----------------
+  // This creates the input generator(s)
+  InputInit();
+
+  // register all input generators with Fun4All
+  InputRegister();
+
+// register the flag handling
+  FlagHandler *flag = new FlagHandler();
+  se->registerSubsystem(flag);
+
+  // set up production relatedstuff
+   Enable::PRODUCTION = true;
+
+  //======================
+  // Write the DST
+  //======================
+
+  Enable::DSTOUT = true;
+  Enable::DSTOUT_COMPRESS = false;
+  DstOut::OutputDir = outdir;
+  DstOut::OutputFile = outputFile;
+
+  //======================
+  // What to run
+  //======================
+  // Global options (enabled for all enables subsystems - if implemented)
+  //  Enable::VERBOSITY = 1;
+
+  Enable::CEMC = true;
+  Enable::CEMC_CELL = Enable::CEMC && true;
+  Enable::CEMC_TOWER = Enable::CEMC_CELL && true;
+  Enable::CEMC_CLUSTER = Enable::CEMC_TOWER && true;
+
+  Enable::HCALIN = true;
+  Enable::HCALIN_CELL = Enable::HCALIN && true;
+  Enable::HCALIN_TOWER = Enable::HCALIN_CELL && true;
+  Enable::HCALIN_CLUSTER = Enable::HCALIN_TOWER && true;
+
+  Enable::HCALOUT = true;
+  Enable::HCALOUT_CELL = Enable::HCALOUT && true;
+  Enable::HCALOUT_TOWER = Enable::HCALOUT_CELL && true;
+  Enable::HCALOUT_CLUSTER = Enable::HCALOUT_TOWER && true;
+
+  //------------------
+  // Detector Reconstruction
+  //------------------
+
+  if (Enable::CEMC_CELL) CEMC_Cells();
+
+  if (Enable::HCALIN_CELL) HCALInner_Cells();
+
+  if (Enable::HCALOUT_CELL) HCALOuter_Cells();
+
+  //-----------------------------
+  // CEMC towering and clustering
+  //-----------------------------
+
+  if (Enable::CEMC_TOWER) CEMC_Towers();
+  if (Enable::CEMC_CLUSTER) CEMC_Clusters();
+
+  //-----------------------------
+  // HCAL towering and clustering
+  //-----------------------------
+
+  if (Enable::HCALIN_TOWER) HCALInner_Towers();
+  if (Enable::HCALIN_CLUSTER) HCALInner_Clusters();
+
+  if (Enable::HCALOUT_TOWER) HCALOuter_Towers();
+  if (Enable::HCALOUT_CLUSTER) HCALOuter_Clusters();
+
+  // if enabled, do topoClustering early, upstream of any possible jet reconstruction
+  if (Enable::TOPOCLUSTER) TopoClusterReco();
+
+  //--------------
+  // Set up Input Managers
+  //--------------
+
+  InputManagers();
+
+  if (Enable::PRODUCTION)
+  {
+    Production_CreateOutputDir();
+  }
+
+  if (Enable::DSTOUT)
+  {
+    string FullOutFile = DstOut::OutputFile;
+    Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
+    out->AddNode("Sync");
+    out->AddNode("EventHeader");
+// Inner Hcal
+    out->AddNode("TOWER_SIM_HCALIN");
+    out->AddNode("TOWER_RAW_HCALIN");
+    out->AddNode("TOWER_CALIB_HCALIN");
+    out->AddNode("TOWERINFO_RAW_HCALIN");
+    out->AddNode("TOWERINFO_SIM_HCALIN");
+    out->AddNode("TOWERINFO_CALIB_HCALIN");
+    out->AddNode("CLUSTER_HCALIN");
+
+// Outer Hcal
+    out->AddNode("TOWER_SIM_HCALOUT");
+    out->AddNode("TOWER_RAW_HCALOUT");
+    out->AddNode("TOWER_CALIB_HCALOUT");
+    out->AddNode("TOWERINFO_RAW_HCALOUT");
+    out->AddNode("TOWERINFO_SIM_HCALOUT");
+    out->AddNode("TOWERINFO_CALIB_HCALOUT");
+    out->AddNode("CLUSTER_HCALOUT");
+
+// CEmc
+    out->AddNode("TOWER_SIM_CEMC");
+    out->AddNode("TOWER_RAW_CEMC");
+    out->AddNode("TOWER_CALIB_CEMC");
+    out->AddNode("TOWERINFO_RAW_CEMC");
+    out->AddNode("TOWERINFO_SIM_CEMC");
+    out->AddNode("TOWERINFO_CALIB_CEMC");
+    out->AddNode("CLUSTER_CEMC");
+    out->AddNode("CLUSTER_POS_COR_CEMC");
+
+// leave the topo cluster here in case we run this during pass3
+    out->AddNode("TOPOCLUSTER_ALLCALO");
+    out->AddNode("TOPOCLUSTER_EMCAL");
+    out->AddNode("TOPOCLUSTER_HCAL");
+    se->registerOutputManager(out);
+  }
+  //-----------------
+  // Event processing
+  //-----------------
+  // if we use a negative number of events we go back to the command line here
+  if (nEvents < 0)
+  {
+    return 0;
+  }
+  se->run(nEvents);
+
+  //-----
+  // Exit
+  //-----
+
+  CDBInterface::instance()->Print(); // print used DB files
+  se->End();
+  se->PrintTimer();
+  std::cout << "All done" << std::endl;
+  delete se;
+  if (Enable::PRODUCTION)
+  {
+    Production_MoveOutput();
+  }
+
+  gSystem->Exit(0);
+  return 0;
+}
+#endif
---
+++ submit/AMPT/pass2_nopileup/rundir/run_pass2_nopileup_ampt.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/bash
+
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+ana_calo=ana.399
+ana_mbdepd=ana.399
+ana_pass3trk=ana.399
+
+run_calo=1
+run_trk=1
+run_mbdepd=1
+
+# just to get a working environment, the specific ana builds for each reconstruction are set later
+source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n
+
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    getinputfiles.pl $2
+    if [ $? -ne 0 ]
+    then
+	echo error from getinputfiles.pl $2, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+    exit 1
+fi
+
+# arguments 
+# $1: number of events
+# $2: g4hits input file
+# $3: calo output file
+# $4: calo output dir
+# $5: mbdepd output file
+# $6: mbdepd output dir
+# $7: track output dir
+# $8: runnumber
+# $9: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(g4hits file\): $2
+echo arg3 \(calo output file\): $3
+echo arg4 \(calo output dir\): $4
+echo arg5 \(mbdepd output file\): $5
+echo arg6 \(mbdepd output dir\): $6
+echo arg7 \(trk output dir\): $7
+echo arg8 \(runnumber\): $8
+echo arg9 \(sequence\): $9
+
+
+#---------------------------------------------------------------
+# Calorimeter Reconstruction
+
+if [ ${run_calo} -gt 0 ]
+then
+    source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n $ana_calo
+    echo 'here comes your environment for Fun4All_G4_Calo.C'
+    printenv
+    echo running calo root.exe -q -b Fun4All_G4_Calo.C\($1,\"$2\",\"$3\",\"$4\"\)
+    root.exe -q -b  Fun4All_G4_Calo.C\($1,\"$2\",\"$3\",\"$4\"\)
+fi
+
+#---------------------------------------------------------------
+# Mbd Epd Reconstruction
+
+if [ ${run_mbdepd} -gt 0 ]
+then
+    source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n $ana_mbdepd
+    echo 'here comes your environment for Fun4All_G4_MBD_EPD.C'
+    printenv
+
+    echo root.exe -q -b Fun4All_G4_MBD_EPD.C\($1,\"$2\",\"$5\",\"$6\"\)
+    root.exe -q -b  Fun4All_G4_MBD_EPD.C\($1,\"$2\",\"$5\",\"$6\"\)
+fi
+
+#---------------------------------------------------------------
+# pass3 tracking
+
+if [ ${run_trk} -gt 0 ]
+then
+    source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n $ana_pass3trk
+    echo 'here comes your environment for Fun4All_G4_Pass3Trk.C'
+    printenv
+
+    echo running root.exe -q -b  Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$7\"\)
+    root.exe -q -b  Fun4All_G4_Pass3Trk.C\($1,\"$2\",\"$7\"\)
+fi
+
+echo "script done"
