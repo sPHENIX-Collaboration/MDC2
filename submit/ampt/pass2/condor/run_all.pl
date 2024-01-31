754e39622c8bf3925ccd75496a0b7545e52fcc6c
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-01-31 11:13:05 -0500

    add ampt/pass2

---
+++ submit/ampt/pass2/condor/run_all.pl
@@ -0,0 +1,207 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber = 10;
+my $test;
+my $incremental;
+my $verbosity;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "verbose"=>\$verbosity);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    print "--verbose : turn on blabbering\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d",$outdir,$runnumber);
+mkpath($outdir);
+
+my %outfiletype = ();
+$outfiletype{"DST_BBC_G4HIT"} = 1;
+$outfiletype{"DST_CALO_G4HIT"} = 1;
+$outfiletype{"DST_TRKR_G4HIT"} = 1;
+$outfiletype{"DST_TRUTH_G4HIT"} = "DST_TRUTH";
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $logdir = sprintf("%s/log/run%d",$localdir,$runnumber);
+if (! -d $logdir)
+{
+  mkpath($logdir);
+}
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename from datasets where dsttype = 'G4Hits' and filename like '%ampt_0_20fm%' and runnumber = $runnumber order by filename") || die $DBI::errstr;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+
+my $getbkglastsegment = $dbh->prepare("select max(segment) from datasets where dsttype = 'G4Hits' and filename like '%ampt_0_20fm%' and runnumber = $runnumber");
+$getbkglastsegment->execute();
+my @res1 = $getbkglastsegment->fetchrow_array();
+my $lastsegment = $res1[0];
+$getbkglastsegment->finish();
+
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::errstr;
+while (my @res = $getfiles->fetchrow_array())
+{
+    my $lfn = $res[0];
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+        my $prefix=$1;
+	my $runnumber = int($2);
+	my $segment = int($3);
+	my $foundall = 1;
+	foreach my $type (sort keys %outfiletype)
+	{
+            my $lfn =  sprintf("%s_ampt_0_20fm_50kHz_bkg_0_20fm-%010d-%06d.root",$type,$runnumber,$segment);
+	    if ($segment < 100000)
+	    {
+		$lfn =  sprintf("%s_ampt_0_20fm_50kHz_bkg_0_20fm-%010d-%05d.root",$type,$runnumber,$segment);
+	    }
+	    $chkfile->execute($lfn);
+	    if ($chkfile->rows > 0)
+	    {
+		next;
+	    }
+	    else
+	    {
+		my $newlfn = $outfiletype{$type};
+		if ($newlfn ne "1")
+		{
+		    $lfn =~ s/$type/$outfiletype{$type}/;
+		    $chkfile->execute($lfn);
+		    if ($chkfile->rows > 0)
+		    {
+			next;
+		    }
+		}
+		$foundall = 0;
+		last;
+	    }
+	}
+	if ($foundall == 1)
+	{
+	    next;
+	}
+# output file does not exist yet, check for 2 MB background files (n to n+1)
+	$foundall = 1;
+	my @bkgfiles = ();
+
+	for (my $cnt = $segment+1; $cnt <=$segment+3; $cnt++)
+	{
+	    my $bkgseg = $cnt;
+	    while ($bkgseg > $lastsegment)
+	    {
+		$bkgseg = $bkgseg - $lastsegment -1; # make sure it starts at segment zero, not 1
+	    }
+	    my $bckfile = sprintf("%s-%010d-%06d.root",$prefix,$runnumber,$bkgseg);
+	    if ($bkgseg < 100000)
+	    {
+		$bckfile = sprintf("%s-%010d-%05d.root",$prefix,$runnumber,$bkgseg);
+	    }
+	    $chkfile->execute($bckfile);
+	    if ($chkfile->rows == 0)
+	    {
+		if (defined $verbosity)
+		{
+		    print "$bckfile missing\n";
+		}
+		$foundall = 0;
+		last;
+	    }
+	    push(@bkgfiles,$bckfile);
+	}
+	if ($foundall == 0)
+	{
+	    next;
+	}
+	my $bkglistfile = sprintf("%s/condor-%010d-%06d.bkglist",$logdir,$runnumber,$segment);
+	if ($segment < 100000)
+	{
+	    $bkglistfile = sprintf("%s/condor-%010d-%05d.bkglist",$logdir,$runnumber,$segment);
+	}
+	open(F1,">$bkglistfile");
+	foreach my $bf (@bkgfiles)
+	{
+	    print F1 "$bf\n";
+	}
+	close(F1);
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %d %d %s", $outevents, $lfn, $bkglistfile, $outdir, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit >= 20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, exiting\n";
+	    last;
+	}
+    }
+}
+
+$getfiles->finish();
+$chkfile->finish();
+$dbh->disconnect;
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
