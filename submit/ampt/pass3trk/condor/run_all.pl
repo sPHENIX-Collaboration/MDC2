2130200ded7469e43bfd811b7cad96d01007ca76
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-02-01 11:31:27 -0500

    add ampt/pass3trk

---
+++ submit/ampt/pass3trk/condor/run_all.pl
@@ -0,0 +1,185 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $outevents = 0;
+my $runnumber = 10;
+my $test;
+my $incremental;
+my $shared;
+GetOptions("test"=>\$test, "increment"=>\$incremental, "shared" => \$shared);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--shared : submit jobs to shared pool\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub01 or phnxsub02\n";
+    exit(1);
+}
+my $maxsubmit = $ARGV[0];
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    exit(1);
+}
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d",$outdir,$runnumber);
+mkpath($outdir);
+
+my %outfiletype = ();
+$outfiletype{"DST_TRKR_HIT"} = 1;
+$outfiletype{"DST_TRUTH"} = 1;
+
+my %trkhash = ();
+my %truthhash = ();
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_G4HIT' and filename like 'DST_TRKR_G4HIT_ampt_0_20fm_50kHz_bkg_0_20fm%' and runnumber = $runnumber order by filename") || die $DBI::errstr;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like 'DST_TRUTH_G4HIT_ampt_0_20fm_50kHz_bkg_0_20fm%' and runnumber = $runnumber");
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::errstr;
+my $ncal = $getfiles->rows;
+while (my @res = $getfiles->fetchrow_array())
+{
+    if ($res[1] < 100000)
+    {
+	$trkhash{sprintf("%05d",$res[1])} = $res[0];
+    }
+    else
+    {
+	$trkhash{sprintf("%06d",$res[1])} = $res[0];
+    }
+
+}
+$getfiles->finish();
+$gettruthfiles->execute() || die $DBI::errstr;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    if ($res[1] < 100000)
+    {
+	$truthhash{sprintf("%05d",$res[1])} = $res[0];
+    }
+    else
+    {
+	$truthhash{sprintf("%06d",$res[1])} = $res[0];
+    }
+}
+$gettruthfiles->finish();
+#print "input files: $ncal, truth: $ntruth\n";
+foreach my $segment (sort keys %trkhash)
+{
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $trkhash{$segment};
+#    print "found $lfn\n";
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+        my $foundall = 1;
+	foreach my $type (sort keys %outfiletype)
+	{
+            my $lfn =  sprintf("%s_ampt_0_20fm_50kHz_bkg_0_20fm-%010d-%06d.root",$type,$runnumber,$segment);
+	    if ($segment < 100000)
+	    {
+		$lfn =  sprintf("%s_ampt_0_20fm_50kHz_bkg_0_20fm-%010d-%05d.root",$type,$runnumber,$segment);
+
+	    }
+	    $chkfile->execute($lfn);
+	    if ($chkfile->rows > 0)
+	    {
+		next;
+	    }
+	    else
+	    {
+		$foundall = 0;
+		last;
+	    }
+	}
+	if ($foundall == 1)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	my $truthfile = $truthhash{sprintf("%06d",$segment)};
+	if ($segment < 100000)
+	{
+	    $truthfile = $truthhash{sprintf("%05d",$segment)};
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %d %d %s", $outevents, $lfn, $truthfile, $outdir, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit >= 20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, exiting\n";
+	    last;
+	}
+    }
+}
+
+$chkfile->finish();
+$dbh->disconnect;
+
+my $jobfile = sprintf("condor.job");
+if (defined $shared)
+{
+# $jobfile = sprintf("condor.job.shared");
+    $jobfile = sprintf("condor.job");
+}
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit $jobfile\n";
+    }
+    else
+    {
+	system("condor_submit $jobfile");
+    }
+}
---
+++ submit/ampt/pass3trk/rundir/G4_OutputManager_Pass3Trk.C
@@ -0,0 +1,89 @@
+#ifndef MACRO_G4OUTPUTMANAGER_C
+#define MACRO_G4OUTPUTMANAGER_C
+
+#include <GlobalVariables.C>
+
+#include <G4_Production.C>
+
+#include <fun4all/Fun4AllDstOutputManager.h>
+#include <fun4all/Fun4AllOutputManager.h>
+#include <fun4all/Fun4AllServer.h>
+
+namespace OUTPUTMANAGER
+{
+  set<string> outfiles;
+}
+
+void AddCommonNodes(Fun4AllOutputManager *out);
+
+void CreateDstOutput(int runnumber, int segment)
+{
+  auto se = Fun4AllServer::instance();
+
+  char segrun[100];
+  if (segment < 100000)
+  {
+    snprintf(segrun,100,"%010d-%05d",runnumber,segment);
+  }
+  else
+  {
+    snprintf(segrun,100,"%010d-%06d",runnumber,segment);
+  }
+
+  string FullOutFile = "DST_TRUTH_ampt_0_20fm_50kHz_bkg_0_20fm-" + string(segrun) + ".root";;
+  Fun4AllOutputManager *out = new Fun4AllDstOutputManager("TRUTHOUT", FullOutFile);
+  AddCommonNodes(out);
+  out->AddNode("PHHepMCGenEventMap");
+  out->AddNode("G4HIT_BH_1");
+  out->AddNode("G4TruthInfo");
+  out->AddNode("TRKR_HITTRUTHASSOC");
+  se->registerOutputManager(out);
+  OUTPUTMANAGER::outfiles.insert(FullOutFile);
+
+  FullOutFile = "DST_TRKR_HIT_ampt_0_20fm_50kHz_bkg_0_20fm-" + string(segrun) + ".root";;
+  out = new Fun4AllDstOutputManager("TRKROUT", FullOutFile);
+  AddCommonNodes(out);
+  out->AddNode("TRKR_HITSET");
+  se->registerOutputManager(out);
+  OUTPUTMANAGER::outfiles.insert(FullOutFile);
+}
+
+void AddCommonNodes(Fun4AllOutputManager *out)
+{
+  out->AddNode("Sync");
+  out->AddNode("EventHeader");
+  return;
+}
+
+void DstOutput_move()
+{
+  string copyscript = "copyscript.pl";
+  ifstream f(copyscript);
+  bool scriptexists = f.good();
+  f.close();
+  if (Enable::DSTOUT)
+  {
+    // if run locally it will try to copy output file to itself wiping it out
+    if (PRODUCTION::SaveOutputDir == ".")
+    {
+      return;
+    }
+    for (auto iter = OUTPUTMANAGER::outfiles.begin(); iter != OUTPUTMANAGER::outfiles.end(); ++iter)
+    {
+//   string mvcmd = "mv " + *iter + " " + PRODUCTION::SaveOutputDir;
+      string mvcmd;
+      if (scriptexists)
+      {
+//        mvcmd = copyscript + " -outdir " + PRODUCTION::SaveOutputDir + " " + *iter + " --test";
+        mvcmd = copyscript + " -outdir " + PRODUCTION::SaveOutputDir + " " + *iter;
+      }
+      else
+      {
+	mvcmd = "cp " + *iter + " " + PRODUCTION::SaveOutputDir;
+      }
+      gSystem->Exec(mvcmd.c_str());
+    }
+  }
+}
+
+#endif
