d59e022bb93479b40880b9148a5ec02472418310
Chris Pinkenburg <pinkenburg@bnl.gov>
2024-09-25 13:09:16 -0400

    new pass3calo pass with waveform saving

--- submit/JS_pp200_signal/pass3calo_embed/rundir/Fun4All_G4_Calo.C
+++ submit/JS_pp200_signal/pass3calo_embed/rundir/Fun4All_G4_Calo.C
@@ -10,8 +10,18 @@
 #include <G4_Production.C>
 #include <G4_TopoClusterReco.C>
 
-#include <ffamodules/FlagHandler.h>
 #include <ffamodules/CDBInterface.h>
+#include <ffamodules/FlagHandler.h>
+
+#include <fun4allutils/TimerStats.h>
+
+#include <caloreco/CaloGeomMapping.h>
+#include <caloreco/CaloTowerBuilder.h>
+#include <caloreco/CaloTowerCalib.h>
+#include <caloreco/CaloWaveformProcessing.h>
+#include <caloreco/CaloTowerStatus.h>
+
+#include <calowaveformsim/CaloWaveformSim.h>
 
 #include <fun4all/Fun4AllDstOutputManager.h>
 #include <fun4all/Fun4AllOutputManager.h>
@@ -22,18 +32,22 @@
 
 R__LOAD_LIBRARY(libffamodules.so)
 R__LOAD_LIBRARY(libfun4all.so)
+R__LOAD_LIBRARY(libfun4allutils.so)
+R__LOAD_LIBRARY(libCaloWaveformSim.so)
+R__LOAD_LIBRARY(libcalo_reco.so)
 
 int Fun4All_G4_Calo(
     const int nEvents = 1,
-    const string &inputFile0 = "DST_CALO_G4HIT_pythia8_Jet30_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000006-00000.root",
-    const string &outputFile = "DST_CALO_CLUSTER_pythia8_Jet30_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000006-00000.root",
+    const string &inputFile0 = "DST_CALO_G4HIT_pythia8_Jet30_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000019-000000.root",
+    const string &inputFile1 = "pedestal-00046796.root",
+    const string &outputFile = "DST_CALO_CLUSTER_pythia8_Jet30_sHijing_0_20fm_50kHz_bkg_0_20fm-0000000019-000000.root",
     const string &outdir = ".",
-    const string &cdbtag = "MDC2_ana.398")
+    const string &cdbtag = "MDC2_ana.438")
 {
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(1);
 
-  //Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
+  // Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
   PHRandomSeed::Verbosity(1);
 
   // just if we set some flags somewhere in this macro
@@ -48,10 +62,9 @@ int Fun4All_G4_Calo(
   // or set it to a fixed value so you can debug your code
   //  rc->set_IntFlag("RANDOMSEED", 12345);
   Enable::CDB = true;
-  rc->set_StringFlag("CDB_GLOBALTAG",cdbtag);
+  rc->set_StringFlag("CDB_GLOBALTAG", cdbtag);
+  rc->set_uint64Flag("TIMESTAMP", CDB::timestamp);
   CDBInterface::instance()->Verbosity(1);
-  rc->set_uint64Flag("TIMESTAMP",CDB::timestamp);
-
   //===============
   // Input options
   //===============
@@ -74,12 +87,12 @@ int Fun4All_G4_Calo(
   // register all input generators with Fun4All
   InputRegister();
 
-// register the flag handling
+  // register the flag handling
   FlagHandler *flag = new FlagHandler();
   se->registerSubsystem(flag);
 
   // set up production relatedstuff
-   Enable::PRODUCTION = true;
+  Enable::PRODUCTION = true;
 
   //======================
   // Write the DST
@@ -105,11 +118,13 @@ int Fun4All_G4_Calo(
   Enable::HCALIN_CELL = Enable::HCALIN && true;
   Enable::HCALIN_TOWER = Enable::HCALIN_CELL && true;
   Enable::HCALIN_CLUSTER = Enable::HCALIN_TOWER && true;
+  G4HCALIN::tower_emin = 0.;
 
   Enable::HCALOUT = true;
   Enable::HCALOUT_CELL = Enable::HCALOUT && true;
   Enable::HCALOUT_TOWER = Enable::HCALOUT_CELL && true;
   Enable::HCALOUT_CLUSTER = Enable::HCALOUT_TOWER && true;
+  G4HCALOUT::tower_emin = 0.;
 
   //------------------
   // Detector Reconstruction
@@ -141,11 +156,138 @@ int Fun4All_G4_Calo(
   // if enabled, do topoClustering early, upstream of any possible jet reconstruction
   if (Enable::TOPOCLUSTER) TopoClusterReco();
 
+  // put waveform stuff here
+    CaloWaveformSim *caloWaveformSim = new CaloWaveformSim();
+    caloWaveformSim->set_detector_type(CaloTowerDefs::HCALOUT);
+    caloWaveformSim->set_detector("HCALOUT");
+    caloWaveformSim->set_nsamples(12);
+    caloWaveformSim->set_pedestalsamples(12);
+    caloWaveformSim->set_timewidth(0.2);
+    caloWaveformSim->set_peakpos(6);
+    // caloWaveformSim->Verbosity(2);
+    // caloWaveformSim->set_noise_type(CaloWaveformSim::NOISE_NONE);
+    se->registerSubsystem(caloWaveformSim);
+
+    caloWaveformSim = new CaloWaveformSim();
+    caloWaveformSim->set_detector_type(CaloTowerDefs::HCALIN);
+    caloWaveformSim->set_detector("HCALIN");
+    caloWaveformSim->set_nsamples(12);
+    caloWaveformSim->set_pedestalsamples(12);
+    caloWaveformSim->set_timewidth(0.2);
+    caloWaveformSim->set_peakpos(6);
+    //  caloWaveformSim->set_noise_type(CaloWaveformSim::NOISE_NONE);
+    se->registerSubsystem(caloWaveformSim);
+
+    caloWaveformSim = new CaloWaveformSim();
+    caloWaveformSim->set_detector_type(CaloTowerDefs::CEMC);
+    caloWaveformSim->set_detector("CEMC");
+    caloWaveformSim->set_nsamples(12);
+    caloWaveformSim->set_pedestalsamples(12);
+    caloWaveformSim->set_timewidth(0.2);
+    caloWaveformSim->set_peakpos(6);
+    caloWaveformSim->set_calibName("cemc_pi0_twrSlope_v1_default");
+
+    //  caloWaveformSim->set_noise_type(CaloWaveformSim::NOISE_NONE);
+    /*
+    caloWaveformSim->get_light_collection_model().load_data_file(
+        string(getenv("CALIBRATIONROOT")) +
+            string("/CEMC/LightCollection/Prototype3Module.xml"),
+        "data_grid_light_guide_efficiency", "data_grid_fiber_trans");
+    */
+    se->registerSubsystem(caloWaveformSim);
+
+    CaloTowerBuilder *ca2 = new CaloTowerBuilder();
+    ca2->set_detector_type(CaloTowerDefs::HCALOUT);
+    ca2->set_nsamples(12);
+    ca2->set_dataflag(false);
+    ca2->set_processing_type(CaloWaveformProcessing::TEMPLATE);
+    ca2->set_builder_type(CaloTowerDefs::kWaveformTowerv2);
+    // match our current ZS threshold ~7ADC for hcal
+    ca2->set_softwarezerosuppression(true, 7);
+    se->registerSubsystem(ca2);
+
+    ca2 = new CaloTowerBuilder();
+    ca2->set_detector_type(CaloTowerDefs::HCALIN);
+    ca2->set_nsamples(12);
+    ca2->set_dataflag(false);
+    ca2->set_processing_type(CaloWaveformProcessing::TEMPLATE);
+    ca2->set_builder_type(CaloTowerDefs::kWaveformTowerv2);
+    ca2->set_softwarezerosuppression(true, 7);
+    se->registerSubsystem(ca2);
+
+    ca2 = new CaloTowerBuilder();
+    ca2->set_detector_type(CaloTowerDefs::CEMC);
+    ca2->set_nsamples(12);
+    ca2->set_dataflag(false);
+    ca2->set_processing_type(CaloWaveformProcessing::TEMPLATE);
+    ca2->set_builder_type(CaloTowerDefs::kWaveformTowerv2);
+    // match our current ZS threshold ~14ADC for emcal
+    ca2->set_softwarezerosuppression(true, 14);
+    se->registerSubsystem(ca2);
+
+    /////////////////////////////////////////////////////
+    // Set status of towers, Calibrate towers,  Cluster
+    /////////////////////////////////////////////////////
+    CaloTowerStatus *statusEMC = new CaloTowerStatus("CEMCSTATUS");
+    statusEMC->set_detector_type(CaloTowerDefs::CEMC);
+    statusEMC->set_time_cut(1);
+    se->registerSubsystem(statusEMC);
+
+    CaloTowerStatus *statusHCalIn = new CaloTowerStatus("HCALINSTATUS");
+    statusHCalIn->set_detector_type(CaloTowerDefs::HCALIN);
+    statusHCalIn->set_time_cut(2);
+    se->registerSubsystem(statusHCalIn);
+
+    CaloTowerStatus *statusHCALOUT = new CaloTowerStatus("HCALOUTSTATUS");
+    statusHCALOUT->set_detector_type(CaloTowerDefs::HCALOUT);
+    statusHCALOUT->set_time_cut(2);
+    se->registerSubsystem(statusHCALOUT);
+
+    ////////////////////
+    // Calibrate towers
+    CaloTowerCalib *calibEMC = new CaloTowerCalib("CEMCCALIB");
+    calibEMC->set_detector_type(CaloTowerDefs::CEMC);
+    calibEMC->set_outputNodePrefix("TOWERINFO_CALIB_");
+    se->registerSubsystem(calibEMC);
+
+    CaloTowerCalib *calibOHCal = new CaloTowerCalib("HCALOUTCALIB");
+    calibOHCal->set_detector_type(CaloTowerDefs::HCALOUT);
+    calibOHCal->set_outputNodePrefix("TOWERINFO_CALIB_");
+    se->registerSubsystem(calibOHCal);
+
+    CaloTowerCalib *calibIHCal = new CaloTowerCalib("HCALINCALIB");
+    calibIHCal->set_detector_type(CaloTowerDefs::HCALIN);
+    calibIHCal->set_outputNodePrefix("TOWERINFO_CALIB_");
+    se->registerSubsystem(calibIHCal);
+    //////////////////
+    // Clusters
+    RawClusterBuilderTemplate *ClusterBuilder = new RawClusterBuilderTemplate("EmcRawClusterBuilderTemplate");
+    ClusterBuilder->Detector("CEMC");
+    ClusterBuilder->set_threshold_energy(0.030);  // for when using basic calibration
+    std::string emc_prof = getenv("CALIBRATIONROOT");
+    emc_prof += "/EmcProfile/CEMCprof_Thresh30MeV.root";
+    ClusterBuilder->LoadProfile(emc_prof);
+    ClusterBuilder->set_UseTowerInfo(1);  // to use towerinfo objects rather than old RawTower
+    se->registerSubsystem(ClusterBuilder);
+  
+
+  //--------------
+  // Timing module is last to register
+  //--------------
+  TimerStats *ts = new TimerStats();
+  ts->OutFileName("jobtime.root");
+  se->registerSubsystem(ts);
+
   //--------------
   // Set up Input Managers
   //--------------
 
   InputManagers();
+
+  Fun4AllInputManager *hitsin = new Fun4AllNoSyncDstInputManager("DST2");
+  hitsin->AddFile(inputFile1);
+  hitsin->Repeat();
+  se->registerInputManager(hitsin);
 
   if (Enable::PRODUCTION)
   {
@@ -158,35 +300,46 @@ int Fun4All_G4_Calo(
     Fun4AllDstOutputManager *out = new Fun4AllDstOutputManager("DSTOUT", FullOutFile);
     out->AddNode("Sync");
     out->AddNode("EventHeader");
-// Inner Hcal
-    out->AddNode("TOWER_SIM_HCALIN");
-    out->AddNode("TOWER_RAW_HCALIN");
-    out->AddNode("TOWER_CALIB_HCALIN");
-    out->AddNode("TOWERINFO_RAW_HCALIN");
-    out->AddNode("TOWERINFO_SIM_HCALIN");
-    out->AddNode("TOWERINFO_CALIB_HCALIN");
+    // Inner Hcal
+    out->AddNode("TOWER_SIM_NZ_HCALIN");
+    out->AddNode("TOWER_RAW_NZ_HCALIN");
+    out->AddNode("TOWER_CALIB_NZ_HCALIN");
+    out->AddNode("TOWERINFO_RAW_NZ_HCALIN");
+    out->AddNode("TOWERINFO_SIM_NZ_HCALIN");
+    out->AddNode("TOWERINFO_CALIB_NZ_HCALIN");
     out->AddNode("CLUSTER_HCALIN");
+    out->AddNode("CLUSTERINFO_HCALIN");
+    out->AddNode("TOWERINFO_CALIB_HCALIN");
+    out->AddNode("WAVEFORM_HCALIN");
+    out->AddNode("TOWERS_HCALIN");
 
-// Outer Hcal
-    out->AddNode("TOWER_SIM_HCALOUT");
-    out->AddNode("TOWER_RAW_HCALOUT");
-    out->AddNode("TOWER_CALIB_HCALOUT");
-    out->AddNode("TOWERINFO_RAW_HCALOUT");
-    out->AddNode("TOWERINFO_SIM_HCALOUT");
-    out->AddNode("TOWERINFO_CALIB_HCALOUT");
+    // Outer Hcal
+    out->AddNode("TOWER_SIM_NZ_HCALOUT");
+    out->AddNode("TOWER_RAW_NZ_HCALOUT");
+    out->AddNode("TOWER_CALIB_NZ_HCALOUT");
+    out->AddNode("TOWERINFO_RAW_NZ_HCALOUT");
+    out->AddNode("TOWERINFO_SIM_NZ_HCALOUT");
+    out->AddNode("TOWERINFO_CALIB_NZ_HCALOUT");
     out->AddNode("CLUSTER_HCALOUT");
+    out->AddNode("CLUSTERINFO_HCALOUT");
+    out->AddNode("TOWERINFO_CALIB_HCALOUT");
+    out->AddNode("WAVEFORM_HCALOUT");
+    out->AddNode("TOWERS_HCALOUT");
 
-// CEmc
-    out->AddNode("TOWER_SIM_CEMC");
-    out->AddNode("TOWER_RAW_CEMC");
-    out->AddNode("TOWER_CALIB_CEMC");
-    out->AddNode("TOWERINFO_RAW_CEMC");
-    out->AddNode("TOWERINFO_SIM_CEMC");
-    out->AddNode("TOWERINFO_CALIB_CEMC");
+    // CEmc
+    out->AddNode("TOWER_SIM_NZ_CEMC");
+    out->AddNode("TOWER_RAW_NZ_CEMC");
+    out->AddNode("TOWER_CALIB_NZ_CEMC");
+    out->AddNode("TOWERINFO_RAW_NZ_CEMC");
+    out->AddNode("TOWERINFO_SIM_NZ_CEMC");
+    out->AddNode("TOWERINFO_CALIB_NZ_CEMC");
     out->AddNode("CLUSTER_CEMC");
-    out->AddNode("CLUSTER_POS_COR_CEMC");
+    out->AddNode("CLUSTERINFO_CEMC");
+    out->AddNode("TOWERINFO_CALIB_CEMC");
+    out->AddNode("WAVEFORM_CEMC");
+    out->AddNode("TOWERS_CEMC");
 
-// leave the topo cluster here in case we run this during pass3
+    // leave the topo cluster here in case we run this during pass3
     out->AddNode("TOPOCLUSTER_ALLCALO");
     out->AddNode("TOPOCLUSTER_EMCAL");
     out->AddNode("TOPOCLUSTER_HCAL");
@@ -206,7 +359,7 @@ int Fun4All_G4_Calo(
   // Exit
   //-----
 
-  CDBInterface::instance()->Print(); // print used DB files
+  CDBInterface::instance()->Print();  // print used DB files
   se->End();
   se->PrintTimer();
   std::cout << "All done" << std::endl;
