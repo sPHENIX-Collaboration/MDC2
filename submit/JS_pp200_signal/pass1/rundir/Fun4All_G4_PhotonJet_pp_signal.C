dd226f1025a055b0db7f21158b7783125802d62f
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-10-24 13:51:12 -0400

    fix up photon jets

--- submit/JS_pp200_signal/pass1/rundir/Fun4All_G4_PhotonJet_pp_signal.C
+++ submit/JS_pp200_signal/pass1/rundir/Fun4All_G4_PhotonJet_pp_signal.C
@@ -44,6 +44,8 @@ int Fun4All_G4_PhotonJet_pp_signal(
   Fun4AllServer *se = Fun4AllServer::instance();
   se->Verbosity(1);
 
+  CDBInterface::instance()->Verbosity(1);
+
   // Opt to print all random seed used for debugging reproducibility. Comment out to reduce stdout prints.
   PHRandomSeed::Verbosity(1);
 
@@ -60,6 +62,9 @@ int Fun4All_G4_PhotonJet_pp_signal(
   //  rc->set_IntFlag("RANDOMSEED", 12345);
   // int seedValue = 491258969;
   // rc->set_IntFlag("RANDOMSEED", seedValue);
+
+  TRACKING::pp_mode = true;
+  TRACKING::pp_extended_readout_time = 90000;
 
   //===============
   // conditions DB flags
@@ -104,6 +109,14 @@ int Fun4All_G4_PhotonJet_pp_signal(
   case 12:
     Input::BEAM_CONFIGURATION = Input::pA_COLLISION;  // for 2023 sims we want the AA geometry for no pileup sims
     cout << "using Input::pA_COLLISION" << endl;
+    break;
+  case 21: // zero beam xing angle, mvtx rotated
+    Input::BEAM_CONFIGURATION = Input::pp_ZEROANGLE;
+    Enable::MVTX_APPLYMISALIGNMENT = true;
+    break;
+  case 22: // zero beam xing angle, mvtx rotated
+    Input::BEAM_CONFIGURATION = Input::pp_COLLISION;
+    Enable::MVTX_APPLYMISALIGNMENT = true;
     break;
   default:
     cout << "runnnumber " << runnumber << " not implemented" << endl;
@@ -235,21 +248,8 @@ int Fun4All_G4_PhotonJet_pp_signal(
   Enable::EPD = true;
 
   //! forward flux return plug door. Out of acceptance and off by default.
-  //  Enable::PLUGDOOR = true;
-  Enable::PLUGDOOR_BLACKHOLE = true;
-  //  Enable::PLUGDOOR_ABSORBER = true;
-
-  //  Enable::BEAMLINE = true;
-  G4BEAMLINE::skin_thickness = 0.5;
-  //  Enable::BEAMLINE_ABSORBER = true;  // makes the beam line magnets sensitive volumes
-  //  Enable::BEAMLINE_BLACKHOLE = true; // turns the beamline magnets into black holes
-  //  Enable::ZDC = true;
-  //  Enable::ZDC_ABSORBER = true;
-  //  Enable::ZDC_SUPPORT = true;
-  //  Enable::ZDC_TOWER = Enable::ZDC && true;
-  Enable::ZDC_EVAL = Enable::ZDC_TOWER && true;
-  //  Enable::GLOBAL_RECO = true;
-  // Enable::GLOBAL_FASTSIM = true;
+  Enable::PLUGDOOR = true;
+  // Enable::PLUGDOOR_BLACKHOLE = true;
 
   // new settings using Enable namespace in GlobalVariables.C
   Enable::BLACKHOLE = true;
