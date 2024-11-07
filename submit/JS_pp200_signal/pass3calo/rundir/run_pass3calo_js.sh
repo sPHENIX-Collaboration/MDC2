f8edf5c93a44a10eb32d7997d55986cb84b9c892
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-11-07 16:28:22 -0500

    new calo output for JS_pp200_signal/pass3calo

--- submit/JS_pp200_signal/pass3calo/rundir/run_pass3calo_js.sh
+++ submit/JS_pp200_signal/pass3calo/rundir/run_pass3calo_js.sh
@@ -11,60 +11,17 @@ this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 echo running: $this_script $*
 
-anabuild=ana.418
-
-source /cvmfs/sphenix.sdcc.bnl.gov/gcc-12.1.0/opt/sphenix/core/bin/sphenix_setup.sh -n $anabuild
-
-cdbtag=MDC2_$anabuild
-
 if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
 then
     cd $_CONDOR_SCRATCH_DIR
     rsync -av $this_dir/* .
-    getinputfiles.pl $2
-    if [ $? -ne 0 ]
-    then
-        echo error from getinputfiles.pl $2, exiting
-        exit -1
-    fi
 else
     echo condor scratch NOT set
     hostname
     exit -1
 fi
-# arguments 
-# $1: number of events
-# $2: calo g4hits input file
-# $3: output file
-# $4: output dir
-# $5: run number
-# $6: sequence
 
-echo 'here comes your environment'
-printenv
-echo arg1 \(events\) : $1
-echo arg2 \(calo g4hits file\): $2
-echo arg3 \(output file\): $3
-echo arg4 \(output dir\): $4
-echo arg5 \(jet trigger\): $5
-echo arg6 \(runnumber\): $6
-echo arg7 \(sequence\): $7
-echo cdbtag: $cdbtag
+container_script=container_`basename $this_script`
+singularity exec -B /home -B /direct/sphenix+u -B /gpfs02 -B /sphenix/u -B /sphenix/lustre01 -B /sphenix/user  -B /sphenix/sim /cvmfs/sphenix.sdcc.bnl.gov/singularity/rhic_sl7.sif ./$container_script $*
 
-runnumber=$(printf "%010d" $6)
-sequence=$(printf "%06d" $7)
-
-filename=timing
-
-echo running running root.exe -q -b Fun4All_G4_Calo.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\"\)
-root.exe -q -b  Fun4All_G4_Calo.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\"\)
-
-timedirname=/sphenix/sim/sim01/sphnxpro/mdc2/logs/js_pp200_signal/pass3calo/timing.run${6}/${5}
-
-[ ! -d $timedirname ] &&  mkdir -p $timedirname
-
-rootfilename=${timedirname}/${filename}-${runnumber}-${sequence}.root
-
-[ -f jobtime.root ] && cp -v jobtime.root $rootfilename
-
-echo "script done"
+echo "wrapper script done"
