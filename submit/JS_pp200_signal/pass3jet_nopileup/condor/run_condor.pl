f5cf9dab11634225a94053d044dfb925ab6ee0ac
sPHENIX Production Account <pinkenburg@bnl.gov>
2024-11-11 09:50:55 -0500

    run JS_pp200_signal/pass3jet_nopileup in container

--- submit/JS_pp200_signal/pass3jet_nopileup/condor/run_condor.pl
+++ submit/JS_pp200_signal/pass3jet_nopileup/condor/run_condor.pl
@@ -4,13 +4,16 @@ use strict;
 use warnings;
 use Getopt::Long;
 use File::Path;
+use File::Basename;
 
 my $test;
-GetOptions("test"=>\$test);
-if ($#ARGV < 5)
+my $memory = sprintf("2048MB");
+GetOptions("memory:s"=>\$memory, "test"=>\$test);
+if ($#ARGV < 7)
 {
-    print "usage: run_condor.pl <events> <jettrigger> <truth infile> <outfile> <outdir> <runnumber> <sequence>\n";
+    print "usage: run_condor.pl <events> <jettrigger> <truth infile> <outfile> <outdir> <build> <runnumber> <sequence>\n";
     print "options:\n";
+    print "-memory: memory requirement\n";
     print "-test: testmode - no condor submission\n";
     exit(-2);
 }
@@ -25,12 +28,14 @@ my $jettrigger = $ARGV[1];
 my $infile = $ARGV[2];
 my $dstoutfile = $ARGV[3];
 my $dstoutdir = $ARGV[4];
-my $runnumber = $ARGV[5];
-my $sequence = $ARGV[6];
+my $build = $ARGV[5];
+my $runnumber = $ARGV[6];
+my $sequence = $ARGV[7];
 if ($sequence < 100)
 {
     $baseprio = 90;
 }
+my $batchname = sprintf("%s %s",basename($executable),$jettrigger);
 my $condorlistfile = sprintf("condor.list");
 my $suffix = sprintf("_%s-%010d-%06d",$jettrigger,$runnumber,$sequence);
 my $logdir = sprintf("%s/log/run%d/%s",$localdir,$runnumber,$jettrigger);
@@ -60,7 +65,7 @@ print "job: $jobfile\n";
 open(F,">$jobfile");
 print F "Universe 	= vanilla\n";
 print F "Executable 	= $executable\n";
-print F "Arguments       = \"$nevents $infile $dstoutfile $dstoutdir $runnumber $sequence\"\n";
+print F "Arguments       = \"$nevents $infile $dstoutfile $dstoutdir $build $runnumber $sequence\"\n";
 print F "Output  	= $outfile\n";
 print F "Error 		= $errfile\n";
 print F "Log  		= $condorlogfile\n";
@@ -70,7 +75,8 @@ print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
 print F "accounting_group = group_sphenix.mdc2\n";
 print F "accounting_group_user = sphnxpro\n";
 print F "Requirements = (CPU_Type == \"mdc2\")\n";
-print F "request_memory = 2048MB\n";
+print F "request_memory = $memory\n";
+print F "batch_name = \"$batchname\"\n";
 print F "Priority = $baseprio\n";
 #print F "concurrency_limits = PHENIX_2000\n";
 print F "job_lease_duration = 3600\n";
@@ -86,5 +92,5 @@ close(F);
 #}
 
 open(F,">>$condorlistfile");
-print F "$executable, $nevents, $infile, $dstoutfile, $dstoutdir, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio\n";
+print F "$executable, $nevents, $infile, $dstoutfile, $dstoutdir, $build, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio, $memory, $batchname\n";
 close(F);
