cf513032a8102e1ee17474ae071f7108616d5689
sPHENIX Production Account <pinkenburg@bnl.gov>
2025-01-09 17:22:25 -0500

    add herwig

---
+++ submit/herwig/pass1/condor/run_all.pl
@@ -0,0 +1,189 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use File::Path;
+use Getopt::Long;
+use DBI;
+
+sub getlastsegment;
+
+my $build;
+my $incremental;
+my $runnumber;
+my $test;
+my $type = "MB";
+GetOptions("build:s" => \$build, "increment"=>\$incremental, "run:i" =>\$runnumber, "test"=>\$test);
+if ($#ARGV < 1)
+{
+    print "usage: run_all.pl <number of jobs> <\"Herwig_Jet10\", \"Herwig_Jet30\", \"Herwig_MB\" production>\n";
+    print "parameters:\n";
+    print "--build: <ana build>\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--run: <runnumber>\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+my $isbad = 0;
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /phnxsub/)
+{
+    print "submit only from phnxsub hosts\n";
+    $isbad = 1;
+}
+if (! defined $runnumber)
+{
+    print "need runnumber with --run <runnumber>\n";
+    $isbad = 1;
+}
+
+if (! defined $build)
+{
+    print "need build with --build <ana build>\n";
+    $isbad = 1;
+}
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    $isbad = 1;
+}
+
+if ($isbad > 0)
+{
+    exit(1);
+}
+
+
+my $maxsubmit = $ARGV[0];
+my $jettrigger = $ARGV[1];
+if ($jettrigger  ne "Herwig_Jet10" &&
+    $jettrigger  ne "Herwig_Jet30" &&
+    $jettrigger  ne "Herwig_MB")
+{
+    print "second argument has to be Herwig_Jet10, Herwig_Jet30 or Herwig_MB\n";
+    exit(1);
+}
+if ($maxsubmit > 50000)
+{
+    print "beware of gpfs overload, you sure you want to run $maxsubmit jobs?\n";
+    exit(0);
+}
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+
+my $herwig_runnumber = 1;
+my $herwig_dir = sprintf("/sphenix/sim/sim01/sphnxpro/mdc2/herwig");
+my $events = 1000; # for running with plugdoor
+#$events = 200;
+#$events = 100; # for ftfp_bert_hp
+my $evtsperfile = 10000;
+my $nmax = $evtsperfile;
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d/%s",$outdir,$runnumber,lc $jettrigger);
+if (! -d $outdir)
+{
+    mkpath($outdir);
+}
+my $nsubmit = 0;
+my $lastsegment=getlastsegment();
+OUTER: for (my $segment=0; $segment<=$lastsegment; $segment++)
+{
+    my $herwigdatfile = sprintf("%s/%s/%s-%06d.hepmc",$herwig_dir,$jettrigger,$jettrigger,$segment);
+    if (! -f $herwigdatfile)
+    {
+	print "could not locate $herwigdatfile\n";
+	next;
+    }
+#    print "herwig: $herwigdatfile\n";
+    my $sequence = $segment*$evtsperfile/$events;
+    for (my $n=0; $n<$nmax; $n+=$events)
+    {
+	my $outfile = sprintf("G4Hits_%s-%010d-%06d.root",$jettrigger,$runnumber,$sequence);
+	$chkfile->execute($outfile);
+	if ($chkfile->rows == 0)
+	{
+	    my $tstflag="";
+	    if (defined $test)
+	    {
+		$tstflag="--test";
+	    }
+            my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %d %s %s %d %d %s", $events, $herwigdatfile, $outdir, $outfile, $n, $build, $jettrigger, $runnumber, $sequence, $tstflag);
+	    print "cmd: $subcmd\n";
+	    system($subcmd);
+	    my $exit_value  = $? >> 8;
+	    if ($exit_value != 0)
+	    {
+		if (! defined $incremental)
+		{
+		    print "error from run_condor.pl\n";
+		    exit($exit_value);
+		}
+	    }
+	    else
+	    {
+		$nsubmit++;
+	    }
+	    if ($nsubmit >= $maxsubmit || $nsubmit >= 20000)
+	    {
+		print "maximum number of submissions reached, exiting\n";
+		last OUTER;
+	    }
+	}
+#	else
+#	{
+#	    print "$outfile exists\n";
+#	}
+        $sequence++;
+    }
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit condor.job\n";
+    }
+    else
+    {
+	system("condor_submit condor.job");
+    }
+}
+
+sub getlastsegment()
+{
+    my $herwigsubdir = sprintf("%s/%s",$herwig_dir,$jettrigger);
+    opendir(DH,$herwigsubdir);
+    my @tmpfiles = sort(readdir(DH));
+    closedir(DH);
+    my @files = ();
+    foreach my $f (@tmpfiles)
+    {
+	if ($f =~ /$jettrigger/)
+	{
+	    push(@files,$f);
+	}
+    }
+    my $last_segment = -1;
+    if ($files[$#files] =~ /(\S+)-(\d+).*\..*/ )
+    {
+	$last_segment = int($2);
+    }
+    else
+    {
+	print "cannot parse $files[$#files] for segment number\n";
+	exit(1);
+    }
+    return $last_segment;
+}
---
+++ submit/herwig/pass1/condor/run_condor.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+use File::Basename;
+
+my $test;
+my $memory = sprintf("5500MB");
+GetOptions("memory:s" => \$memory, "test"=>\$test);
+if ($#ARGV < 8)
+{
+    print "usage: run_condor.pl <events> <infile> <outdir> <outfile> <skip> <build> <jettrigger> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-memory: memory requirement\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+else
+{
+    print "running condor\n";
+}
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 51;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass1_herwig.sh",$rundir);
+my $nevents = $ARGV[0];
+my $infile = $ARGV[1];
+my $dstoutdir = $ARGV[2];
+my $dstoutfile = $ARGV[3];
+my $skip = $ARGV[4];
+my $build = $ARGV[5];
+my $jettrigger = $ARGV[6];
+my $runnumber = $ARGV[7];
+my $sequence = $ARGV[8];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $batchname = sprintf("%s %s",basename($executable),$jettrigger);
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("%010d-%06d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log/run%d/%s",$localdir,$runnumber,$jettrigger);
+if (! -d $logdir)
+{
+  mkpath($logdir);
+}
+my $condorlogdir = sprintf("/tmp/herwig/pass1/run%d/%s",$runnumber,$jettrigger);
+if (! -d $condorlogdir)
+{
+  mkpath($condorlogdir);
+}
+my $jobfile = sprintf("%s/condor-%s.job",$logdir,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor-%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor-%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor-%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile $dstoutfile $skip $dstoutdir $build $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+print F "accounting_group = group_sphenix.mdc2\n";
+print F "accounting_group_user = sphnxpro\n";
+print F "Requirements = (CPU_Type == \"mdc2\")\n";
+# FTFP_BERT
+print F "request_memory = $memory\n";
+print F "Priority = $baseprio\n";
+print F "batch_name = \"$batchname\"\n";
+#FTFP_BERT_HP
+#print F "request_memory = 12288MB\n";
+#print F "Priority 	= 30\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile, $dstoutfile, $skip, $dstoutdir, $build, $jettrigger, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio, $memory $batchname\n";
+close(F);
