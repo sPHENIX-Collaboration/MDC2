d2f6c72ec3aaf613074587f7dabff8499c870eb5
Kolja Kauder <kkauder@gmail.com>
2025-04-29 18:40:39 -0400

    commit recent changes

--- submit/JS_pp200_signal/pass1/rundir/run_pass1_js.sh
+++ submit/JS_pp200_signal/pass1/rundir/run_pass1_js.sh
@@ -21,6 +21,6 @@ else
 fi
 
 container_script=container_`basename $this_script`
-singularity exec -B /home -B /direct/sphenix+u -B /gpfs02 -B /sphenix/u -B /sphenix/lustre01 -B /sphenix/user  -B /sphenix/sim /cvmfs/sphenix.sdcc.bnl.gov/singularity/rhic_sl7.sif ./$container_script $*
+singularity exec -B /home -B /direct/sphenix+u -B /gpfs02 -B /sphenix/u -B /sphenix/lustre01 -B /sphenix/user  -B /sphenix/sim /cvmfs/sphenix.sdcc.bnl.gov/singularity/sdcc_a9.sif ./$container_script $*
 
 echo "wrapper script done"
