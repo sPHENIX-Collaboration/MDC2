782efbc11b5e1ceb7efbdf57a0975213277092a8
Kolja Kauder <kkauder@gmail.com>
2025-04-29 20:40:44 -0400

    fix Herwig pass1

--- submit/Herwig/pass1/condor/run_condor.pl
+++ submit/Herwig/pass1/condor/run_condor.pl
@@ -47,7 +47,7 @@ if (! -d $logdir)
 {
   mkpath($logdir);
 }
-my $condorlogdir = sprintf("/tmp/herwig/pass1/run%d/%s",$runnumber,$jettrigger);
+my $condorlogdir = sprintf("/tmp/Herwig/pass1/run%d/%s",$runnumber,$jettrigger);
 if (! -d $condorlogdir)
 {
   mkpath($condorlogdir);
