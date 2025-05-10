89d3bc9ae2760f58f16a3ea868157785394d9b4b
Kolja Kauder <kkauder@gmail.com>
2025-05-10 16:07:44 -0400

    pass4jet -> pass3jet for JS_pp200_signal/pass3jet_embed

--- submit/JS_pp200_signal/pass3jet_embed/condor/run_condor.pl
+++ submit/JS_pp200_signal/pass3jet_embed/condor/run_condor.pl
@@ -7,7 +7,7 @@ use File::Path;
 use File::Basename;
 
 my $test;
-my $memory = sprintf("4000MB");
+my $memory = sprintf("500MB");
 GetOptions("memory:s"=>\$memory, "test"=>\$test);
 if ($#ARGV < 8)
 {
