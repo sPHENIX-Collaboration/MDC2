ec8e0bc735f0d68267c1c3d3edd796f4279ffb6d
Kolja Kauder <kkauder@gmail.com>
2025-07-06 13:23:57 -0400

    Add rawdata/run2auauwaveformfitting

---
+++ rawdata/run2auauwaveformfitting/condor/submitcron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+[[ -e submitrunning ]] && exit 0
+echo $$ > submitrunning
+source  /opt/sphenix/core/bin/sphenix_setup.sh -n
+echo running on `hostname` > run_runrange.log
+perl run_runrange.pl 53381 54974 --inc &>> run_runrange.log
+rm submitrunning
