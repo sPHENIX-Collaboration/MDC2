b994792a23c928147d71e9d84749963bc5654042
Kolja Kauder <kkauder@gmail.com>
2025-07-14 08:02:42 -0400

    update from run 30 for fm_0_20/pass2

--- submit/fm_0_20/pass2/condor/restart_held.sh
+++ submit/fm_0_20/pass2/condor/restart_held.sh
@@ -1,9 +1,24 @@
 #! /usr/bin/bash
+if [ $# -eq 0 ]
+then
+  echo "No arguments supplied"
+  exit 1
+fi
+run=$1
+runnumber=$(printf "%010d" $run)
+echo restarting run $1
+
 [ -f bla ] && rm bla
-condor_q | grep ' H ' | grep run_pileup_0_20fm.sh | grep sHijing_0_20 > bla
+condor_q | grep ' H ' | grep run_pass2_0_20fm.sh | grep sHijing_0_20 | grep ${runnumber} > bla
 #condor_q | grep ' H ' | grep run_pileup.sh | grep sHijing_0_20 > bla
-[ ! -s bla ] && exit 0
+[ ! -s bla ] && exit 1
 for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
-#for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/condor-0000000006-"$1".job"}'`; do sed -i 's/run_pileup.sh/run_pileup_0_20fm.sh/' $i; echo $i; condor_submit $i; done
-for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. '{print "log/condor-0000000006-"$1".job"}'`; do echo $i; condor_submit $i; done
-[ -f bla ] && rm bla
+
+[ -f tmplist ] && rm tmplist
+
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. -v runnumber=${runnumber} '{print ""runnumber"-"$1".job"}'`; do echo $i >> tmplist ; done
+
+[ -f sedlist ] && rm sedlist
+for i in `cat tmplist`; do echo log/run${run}/condor-$i >> sedlist; done
+for i in `cat sedlist`; do  sed -i 's/7000MB/12000MB/' $i; echo $i; done
+for i in `cat sedlist`; do condor_submit $i; done
--- submit/fm_0_20/pass2/rundir/run_pass2_0_20fm.sh
+++ submit/fm_0_20/pass2/rundir/run_pass2_0_20fm.sh
@@ -11,6 +11,13 @@ this_dir=`dirname $this_script`
 echo rsyncing from $this_dir
 echo running: $this_script $*
 
+# check the current tag (if exist), if no tag, save commit id
+this_gitcommitid=`git describe --exact-match --tags 2> /dev/null`
+if [ $? != 0 ]
+then
+ this_gitcommitid=`git show HEAD | sed -n 1p | cut -d " " -f 2`
+fi
+
 anabuild=${5}
 
 source /opt/sphenix/core/bin/sphenix_setup.sh -n $anabuild
@@ -59,7 +66,7 @@ echo cdbtag: $cdbtag
 runnumber=$(printf "%010d" $6)
 sequence=$(printf "%06d" $7)
 
-echo running root.exe -q -b Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\"\)
-root.exe -q -b  Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\"\)
+echo running root.exe -q -b Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\",\"$this_gitcommitid\"\)
+root.exe -q -b  Fun4All_G4_Pileup.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\",\"$this_gitcommitid\"\)
 
 echo "script done"
