b664f6f4bb250087bbb3f0ff466325dd8897eb58
sPHENIX Production Account <sphnxpro@sphnxprod01.sdcc.bnl.gov>
2025-07-29 09:03:18 -0400

    add run3auaucalocalib

---
+++ rawdata/run3auaucalocalib/condor/submitcron.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+[[ -e submitrunning ]] && exit 0
+echo $$ > submitrunning
+source  /opt/sphenix/core/bin/sphenix_setup.sh -n
+echo running on `hostname` > run_runrange.log
+perl run_runrange.pl 66457 80000 --inc &>> run_runrange.log
+rm submitrunning
