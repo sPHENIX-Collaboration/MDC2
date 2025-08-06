f4ac9fe15e1e178c9c62fc9f07002562769862ce
sPHENIX Production Account <sphnxpro@sphnxprod01.sdcc.bnl.gov>
2025-08-05 21:07:36 -0400

    Add run3auaueventcombine

---
+++ rawdata/run3auaueventcombine/condor/restart_held.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+echo need to implement grep
+exit 1
+condor_q | grep ' H ' | grep cosmics_eventcombine.sh | grep production2 > bla
+cat bla | awk '{printf("log/condor-%s_%010d.job\n",$12,$11)}' > f.l
+for i in `cat f.l`; do sed -i 's/2048MB/4000MB/' $i; done
+for i in `cat bla | awk '{print $1}'`; do condor_rm $i; done
+for i in `cat f.l`; do condor_submit $i; done
