2f31b8d001f3f7ac0e67ee7b4a62b9cda4402559
sPHENIX Production Account <sphnxpro@sphnxprod04.sdcc.bnl.gov>
2025-08-08 14:41:12 -0400

    add fm_0_20/pass3_global_nopileup

---
+++ submit/fm_0_20/pass3_global_nopileup/condor/run_condor.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+use File::Basename;
+
+my $memory = sprintf("500MB");
+my $test;
+GetOptions("memory:s"=>\$memory, "test"=>\$test);
+if ($#ARGV < 6)
+{
+    print "usage: run_condor.pl <events> <bbcepd infile> <outfile> <outdir> <build> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "-memory: memory requirement\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 57;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass3_global_nopileup_fm_0_20.sh",$rundir);
+my $nevents = $ARGV[0];
+my $infile = $ARGV[1];
+my $dstoutfile = $ARGV[2];
+my $dstoutdir = $ARGV[3];
+my $build = $ARGV[4];
+my $runnumber = $ARGV[5];
+my $sequence = $ARGV[6];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $batchname = sprintf("%s %s",basename($executable),$jettrigger);
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("%010d-%06d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log/run%d",$localdir,$runnumber);
+if (! -d $logdir)
+{
+  mkpath($logdir);
+}
+my $condorlogdir = sprintf("/tmp/fm_0_20/pass3_global_nopileup/run%d",$runnumber);
+if (! -d $condorlogdir)
+{
+  mkpath($condorlogdir);
+}
+my $jobfile = sprintf("%s/condor-%s.job",$logdir,$suffix);
+if (-f $jobfile)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor-%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor-%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor-%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile $dstoutfile $dstoutdir $build $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+print F "request_memory = $memory\n";
+print F "batch_name = \"$batchname\"\n";
+print F "Priority = $baseprio\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile, $dstoutfile, $dstoutdir, $build, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio, $memory, $batchname\n";
+close(F);
---
+++ submit/fm_0_20/pass3_global_nopileup/rundir/run_pass3_global_nopileup_fm_0_20.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+this_gitcommitid=`git show HEAD | sed -n 1p | cut -d " " -f 2`
+
+anabuild=${6}
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n $anabuild
+
+cdbtag=MDC2_$anabuild
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    echo $2 > inputfiles.list
+    getinputfiles.pl --filelist inputfiles.list
+    if [ $? -ne 0 ]
+    then
+        cat inputfiles.list
+	echo error from getinputfiles.pl --filelist inputfiles.list, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+    hostname
+    exit -1
+fi
+
+# arguments 
+# $1: number of events
+# $2: bbc_epd input file
+# $3: output file
+# $4: output dir
+# $5: build
+# $6: run number
+# $7: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(bbc/epd file\): $2
+echo arg3 \(output file\): $3
+echo arg4 \(output dir\): $4
+echo arg5 \(build\): $5
+echo arg6 \(runnumber\): $6
+echo arg7 \(sequence\): $7
+echo cdbtag: $cdbtag
+echo git commit id: $this_gitcommitid
+
+runnumber=$(printf "%010d" $6)
+sequence=$(printf "%06d" $7)
+
+filename=timing
+
+echo running root.exe -q -b Fun4All_G4_Global.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\",\"$this_gitcommitid\"\)
+root.exe -q -b  Fun4All_G4_Global.C\($1,\"$2\",\"$3\",\"$4\",\"$cdbtag\",\"$this_gitcommitid\"\)
+
+timedirname=/sphenix/sim/sim01/sphnxpro/mdc2/logs/shijing_hepmc/fm_0_20/pass3_global_nopileup/timing.run${6}
+
+[ ! -d $timedirname ] &&  mkdir -p $timedirname
+
+rootfilename=${timedirname}/${filename}-${runnumber}-${sequence}.root
+
+[ -f jobtime.root ] && cp -v jobtime.root $rootfilename
+
+echo "script done"
