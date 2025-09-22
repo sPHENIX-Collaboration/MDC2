6e75fbe197d400fd9198db5ab3010423c8430b18
sPHENIX Production Account <pinkenburg@bnl.gov>
2025-09-22 14:49:44 -0400

    add rawdata/run2ppjetskimmer

---
+++ rawdata/run2ppjetskimmer/condor/run_condor.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+use File::Basename;
+
+my $overwrite;
+my $test;
+GetOptions("overwrite" => \$overwrite, "test"=>\$test);
+if ($#ARGV < 8)
+{
+    print "not enough arguments: $#ARGV, need 9, here is the list\n";
+    print "usage: run_condor.pl <events> <runnumber> <segment> <infile> <outfile1> <outfile2> <outdir> <qafile> <qadir>\n";
+    print "options:\n";
+    print "--overwrite: overwrite existing job files\n";
+    print "--test: testmode - no condor submission\n";
+    exit(-2);
+}
+else
+{
+    print "running condor\n";
+}
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 91;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run2pp_jetskimmer.sh",$rundir);
+my $nevents = $ARGV[0];
+my $runnumber = $ARGV[1];
+my $segment = $ARGV[2];
+my $infile = $ARGV[3];
+my $dstoutfile1 = $ARGV[4];
+my $dstoutfile2 = $ARGV[5];
+my $dstoutdir = $ARGV[6];
+my $qafile = $ARGV[7];
+my $qadir = $ARGV[8];
+my $batchname = sprintf("%s",basename($executable));
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("%08d-%05d",$runnumber,$segment);
+my $logdir = sprintf("%s/log",$localdir);
+if (! -d $logdir)
+{
+    mkpath($logdir);
+}
+my $condorlogdir = sprintf("/tmp/rawdata/run2ppjetskimmer");
+if (! -d $condorlogdir)
+{
+    mkpath($condorlogdir);
+}
+my $jobfile = sprintf("%s/condor-%s.job",$logdir,$suffix);
+if (-f $jobfile && !defined $overwrite)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor-%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor-%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor-%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $runnumber $segment $infile $dstoutfile1 $dstoutfile2 $dstoutdir $qafile $qadir\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1)\n";
+print F "request_memory = 4000MB\n";
+print F "Priority = $baseprio\n";
+print F "Rank = -SlotID\n";
+print F "batch_name =  \"$batchname\"\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $runnumber, $segment, $infile, $dstoutfile1, $dstoutfile2, $dstoutdir, $qafile, $qadir, $errfile, $outfile, $condorlogfile, $rundir, $baseprio, $batchname\n";
+close(F);
---
+++ rawdata/run2ppjetskimmer/rundir/run2pp_jetskimmer.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n ana.509
+
+hostname
+
+echo running: run_jetskimmer.sh $*
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+else
+    echo condor scratch NOT set
+    exit 1
+fi
+
+# arguments 
+# $1: number of events
+# $2: dst outfile1
+# $3: dst outfile2
+# $4: dst outdir
+# $5: qa outfile
+# $6: qa outdir
+
+
+echo 'here comes your environment'
+
+printenv
+
+echo arg1 \(events\) : $1
+echo arg2 \(run number\) : $2
+echo arg3 \(segment\) : $3
+echo arg4 \(input file\): $4
+echo arg5 \(dst outfile1\): $5
+echo arg6 \(dst outfile2\): $6
+echo arg7 \(dst outdir\): $7
+echo arg8 \(qa outfile\): $8
+echo arg9 \(qa outdir\): $9
+
+runnumber=$(printf "%010d" $2)
+getinputfiles.pl -dd $4
+if [ $? -ne 0 ]
+then
+    echo error from getinputfiles.pl -dd $4, exiting
+    exit -1
+fi
+ls -l
+echo running root.exe -q -b Fun4All_JetSkimmedProductionYear2.CC\($1,\"$4\",\"$5\",\"$6\",\"$8\"\)
+root.exe -q -b Fun4All_JetSkimmedProductionYear2.C\($1,\"$4\",\"$5\",\"$6\",\"$8\"\)
+ls -l
+if [ -f $5 ]
+then
+    copyscript.pl $5 -dd -mv -outdir $7
+else
+    echo could not find $5
+fi
+if [ -f $6 ]
+then
+    copyscript.pl $6 -dd -mv -outdir $7
+else
+    echo could not find $6
+fi
+if [ -f $8 ]
+then
+    copyscript.pl $8 -dd -mv -outdir $9
+else
+    echo could not find $9
+fi
+
+echo "script done"
