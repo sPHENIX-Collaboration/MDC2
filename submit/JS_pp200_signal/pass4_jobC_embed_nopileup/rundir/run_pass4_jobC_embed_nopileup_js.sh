cba184d2e2ad21e7e3dbb0dfdd883dcadba33845
sPHENIX Production Account <pinkenburg@bnl.gov>
2025-10-08 11:01:12 -0400

    add JS_pp200_signal/pass4_jobC_embed_nopileup

---
+++ submit/JS_pp200_signal/pass4_jobC_embed_nopileup/rundir/run_pass4_jobC_embed_nopileup_js.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+
+anabuild=${7}
+
+source /opt/sphenix/core/bin/sphenix_setup.sh -n $anabuild
+
+
+cdbtag=MDC2_$anabuild
+
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+    echo $2 > inputfiles.list
+    echo $3 >> inputfiles.list
+    getinputfiles.pl --filelist inputfiles.list
+    if [ $? -ne 0 ]
+    then
+        cat inputfiles.list
+	echo error from getinputfiles.pl --filelist inputfiles.list, exiting
+	exit -1
+    fi
+else
+    echo condor scratch NOT set
+    hostname
+    exit -1
+fi
+
+# arguments 
+# $1: number of events
+# $2: trkr seed input file
+# $3: cluster input file
+# $4: output file
+# $5: output dir
+# $6: jet trigger
+# $7: build
+# $8: run number
+# $9: sequence
+
+echo 'here comes your environment'
+printenv
+echo arg1 \(events\) : $1
+echo arg2 \(trkr seed file\): $2
+echo arg3 \(cluster file\): $3
+echo arg4 \(output file\): $4
+echo arg5 \(output dir\): $5
+echo arg6 \(jet trigger\): $6
+echo arg7 \(build\): $7
+echo arg8 \(runnumber\): $8
+echo arg9 \(sequence\): $9
+echo cdbtag: $cdbtag
+
+runnumber=$(printf "%010d" $8)
+sequence=$(printf "%06d" $9)
+
+echo running root.exe -q -b Fun4All_G4_sPHENIX_jobC.C\($1,0,\"$2\",\"$3\",\"$4\",\"$5\",\"$cdbtag\"\)
+root.exe -q -b  Fun4All_G4_sPHENIX_jobC.C\($1,0,\"$2\",\"$3\",\"$4\",\"$5\",\"$cdbtag\"\)
+
+echo "script done"
