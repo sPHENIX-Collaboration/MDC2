5006116a316cbc8be1c8f0545ab4759ccb7627b6
Chris Pinkenburg <pinkenburg@bnl.gov>
2025-10-17 09:37:50 -0400

    fix filenames

--- submit/JS_pp200_signal/pass3_jobA_nopileup/rundir/Fun4All_G4_sPHENIX_jobA.C
+++ submit/JS_pp200_signal/pass3_jobA_nopileup/rundir/Fun4All_G4_sPHENIX_jobA.C
@@ -28,8 +28,8 @@ R__LOAD_LIBRARY(libfun4allutils.so)
 int Fun4All_G4_sPHENIX_jobA(
   const int nEvents = 0,
   const int nSkipEvents = 0,
-  const string &inputFile = "DST_TRKR_CLUSTER_pythia8_Jet10-0000000028-00000.root",
-  const string &outputFile = "DST_TRACKSEEDS_pythia8_Jet10-0000000028-00000.root",
+  const string &inputFile = "DST_TRKR_CLUSTER_pythia8_Jet30-0000000028-000000.root",
+  const string &outputFile = "DST_TRACKSEEDS_pythia8_Jet30-0000000028-000000.root",
   const string &outdir = ".",
   const string &cdbtag = "MDC2")
 {
