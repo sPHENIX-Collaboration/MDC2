581210e1460da6800d3177d4be846874e241a9db
sPHENIX Production Account <pinkenburg@bnl.gov>
2025-12-06 10:07:13 -0500

    add restart_held.sh

---
+++ submit/JS_pp200_signal/pass3trk/condor/restart_held.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/bash
+if [ $# -lt 2 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+echo system: $1
+echo pileup: $2
+variable=$1_$2
+
+run=28
+runnumber=$(printf "%010d" $run)
+
+#exit 0
+condor_q | grep ' H ' | grep run_pass3trk_js.sh | grep ${variable} > bla
+
+[ -s bla ] ||  exit 1
+
+for i in `cat bla| awk '{print $1}'`; do condor_rm $i; done
+
+[ -f tmplist ] && rm tmplist
+for i in `cat bla | awk '{print $12}' | awk -F- '{print $3}' | awk -F. -v runnumber=${runnumber} '{print ""runnumber"-"$1".job"}'`; do echo $i >> tmplist ; done
+
+[ -f sedlist ] && rm sedlist
+for i in `cat tmplist`; do echo log/run${run}/${variable}/condor_${variable}-$i >> sedlist; done
+for i in `cat sedlist`; do  sed -i 's/6000MB/8000MB/' $i; echo $i; done
+for i in `cat sedlist`; do  sed -i 's/4000MB/6000MB/' $i; echo $i; done
+#for i in `cat sedlist`; do  sed -i 's/Priority = 42/Priority = 52/' $i; echo $i; done
+for i in `cat sedlist`; do [ -f $i ] && condor_submit $i; done
+#/direct/sphenix+u/sphnxpro/MDC2/submit/JS_pp200_signal/pass3_job0_nopileup/condor/log/run7/PhotonJet/condor_PhotonJet-0000000007-15130.out
