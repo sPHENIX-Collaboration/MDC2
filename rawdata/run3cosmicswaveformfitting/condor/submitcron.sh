e3c6671346ba1edb3700a7df463a3b6001a5028d
sPHENIX Production Account <pinkenburg@bnl.gov>
2026-02-13 16:39:27 -0500

    update run3cosmicswaveformfitting

--- rawdata/run3cosmicswaveformfitting/condor/submitcron.sh
+++ rawdata/run3cosmicswaveformfitting/condor/submitcron.sh
@@ -3,5 +3,6 @@
 echo $$ > submitrunning
 source  /opt/sphenix/core/bin/sphenix_setup.sh -n
 echo running on `hostname` > run_runrange.log
-perl run_runrange.pl 66456 80000 --inc &>> run_runrange.log
+perl run_runrange.pl 66390 90000 --inc &>> run_runrange.log
+#perl run_runrange.pl 79000 90000 --inc &>> run_runrange.log
 rm submitrunning
