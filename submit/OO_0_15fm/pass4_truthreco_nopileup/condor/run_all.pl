db8a1f48dafd2f9ceb6b4842c4e1a0270b43b06f
sPHENIX Production Account <pinkenburg@bnl.gov>
2026-02-21 13:47:49 -0500

    add OO_0_15fm/pass4_truthreco_nopileup

---
+++ submit/OO_0_15fm/pass4_truthreco_nopileup/condor/run_all.pl
@@ -0,0 +1,224 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $build;
+my $incremental;
+my $memory;
+my $outevents = 0;
+my $overwrite;
+my $runnumber;
+my $shared;
+my $test;
+my $verbosity = 0;
+GetOptions("build:s" => \$build, "increment"=>\$incremental, "memory:s"=>\$memory, "overwrite"=>\$overwrite, "run:i" =>\$runnumber, "shared" => \$shared, "test"=>\$test, "verbosity:i" => \$verbosity);
+if ($#ARGV < 0)
+{
+    print "usage: run_all.pl <number of jobs>\n";
+    print "parameters:\n";
+    print "--build: <ana build>\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--memory : memory requirement with unit (MB)\n";
+    print "--overwrite : overwrite eisting output\n";
+    print "--run: <runnumber>\n";
+    print "--shared : submit jobs to shared pool\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+
+my $isbad = 0;
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /sphnxprod/)
+{
+    print "submit only from sphnxprod hosts\n";
+    $isbad = 1;
+}
+if (! defined $runnumber)
+{
+    print "need runnumber with --run <runnumber>\n";
+    $isbad = 1;
+}
+
+if (! defined $build)
+{
+    print "need build with --build <ana build>\n";
+    $isbad = 1;
+}
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    $isbad = 1;
+}
+
+if ($isbad > 0)
+{
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+
+my $outfilelike = sprintf("sHijing_OO_0_15fm-");
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/run%04d",$outdir,$runnumber);
+if (! -d $outdir)
+{
+  mkpath($outdir);
+}
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'G4Hits' and filename like 'G4Hits_$outfilelike%' and runnumber = $runnumber") || die $DBI::errstr;
+my $getclusterfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like 'DST_TRKR_CLUSTER_$outfilelike%' and runnumber = $runnumber") || die $DBI::errstr;
+my $gettrackfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRACKS' and filename like 'DST_TRACKS_$outfilelike%' and runnumber = $runnumber") || die $DBI::errstr;
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH' and filename like 'DST_TRUTH_$outfilelike%' and runnumber = $runnumber") || die $DBI::errstr;
+
+if ($verbosity > 0)
+{
+    print "select filename,segment from datasets where dsttype = 'G4Hits' and filename like 'G4Hits_$outfilelike%' and runnumber = $runnumber\n";
+    print "select filename,segment from datasets where dsttype = 'DST_TRKR_CLUSTER' and filename like 'DST_TRKR_CLUSTER_$outfilelike%' and runnumber = $runnumber\n";
+    print "select filename,segment from datasets where dsttype = 'DST_TRACKS' and filename like 'DST_TRACKS_$outfilelike%' and runnumber = $runnumber\n";
+    print "select filename,segment from datasets where dsttype = 'DST_TRUTH' and filename like 'DST_TRUTH_$outfilelike%' and runnumber = $runnumber\n";
+}
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+
+
+my %g4hithash = ();
+$getfiles->execute() || die $DBI::errstr;
+my $ng4hit = $getfiles->rows;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $g4hithash{sprintf("%06d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+
+my %clusterhash = ();
+$getclusterfiles->execute() || die $DBI::errstr;
+my $ncluster = $getclusterfiles->rows;
+while (my @res = $getclusterfiles->fetchrow_array())
+{
+    $clusterhash{sprintf("%06d",$res[1])} = $res[0];
+}
+$getclusterfiles->finish();
+
+my %trackhash = ();
+$gettrackfiles->execute() || die $DBI::errstr;
+my $ntrack = $gettrackfiles->rows;
+while (my @res = $gettrackfiles->fetchrow_array())
+{
+    $trackhash{sprintf("%06d",$res[1])} = $res[0];
+}
+$gettrackfiles->finish();
+
+my %truthhash = ();
+$gettruthfiles->execute() || die $DBI::errstr;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    $truthhash{sprintf("%06d",$res[1])} = $res[0];
+}
+$gettruthfiles->finish();
+
+
+print "input files g4hit: $ng4hit, cluster: $ncluster, track: $ntrack, truth: $ntruth\n";
+
+my $nsubmit = 0;
+
+foreach my $segment (sort keys %trackhash)
+{
+    if (! exists $g4hithash{$segment})
+    {
+	next;
+    }
+    if (! exists $clusterhash{$segment})
+    {
+	next;
+    }
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $trackhash{$segment};
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+        my $outfilename =  sprintf("DST_TRUTH_RECO_%s%010d-%06d.root",$outfilelike,$runnumber,$segment);
+	$chkfile->execute($outfilename);
+	if ($chkfile->rows > 0)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	if (defined $memory)
+	{
+	    $tstflag = sprintf("%s %s",$tstflag,$memory);
+	}
+        if (defined $overwrite)
+	{
+	    $tstflag = sprintf("%s --overwrite",$tstflag);
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %s %s %s %d %d %s", $outevents, $g4hithash{sprintf("%06d",$segment)}, $clusterhash{sprintf("%06d",$segment)}, $trackhash{sprintf("%06d",$segment)}, $truthhash{sprintf("%06d",$segment)}, $outfilename, $outdir, $build, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit >=20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, exiting\n";
+	    last;
+	}
+    }
+}
+
+$chkfile->finish();
+$dbh->disconnect;
+
+my $jobfile = sprintf("condor.job");
+if (defined $shared)
+{
+ $jobfile = sprintf("condor.job.shared");
+}
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit $jobfile\n";
+    }
+    else
+    {
+	system("condor_submit $jobfile");
+    }
+}
---
+++ submit/OO_0_15fm/pass4_truthreco_nopileup/condor/run_condor.pl
@@ -0,0 +1,99 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+use File::Basename;
+
+my $memory = sprintf("3000MB");
+my $overwrite;
+my $test;
+
+GetOptions("memory:s"=>\$memory, "overwrite"=>\$overwrite, "test"=>\$test);
+
+if ($#ARGV < 9)
+{
+    print "usage: run_condor.pl <events> <trkr g4hit file> <trkr cluster file> <tracks file> <truth file> <outfile> <outdir> <build> <runnumber> <sequence>\n";
+    print "options:\n";
+    print "--memory: memory requirement\n";
+    print "--overwrite : overwrite existing jobfiles\n";
+    print "--test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 57;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass4_truthreco_nopileup_oo_0_15fm.sh",$rundir);
+my $nevents = $ARGV[0];
+my $infile0 = $ARGV[1];
+my $infile1 = $ARGV[2];
+my $infile2 = $ARGV[3];
+my $infile3 = $ARGV[4];
+my $outfilename = $ARGV[5];
+my $dstoutdir = $ARGV[6];
+my $build = $ARGV[7];
+my $runnumber = $ARGV[8];
+my $sequence = $ARGV[9];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $batchname = sprintf("%s",basename($executable));
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("-%010d-%06d",$runnumber,$sequence);
+my $logdir = sprintf("%s/log/run%d",$localdir,$runnumber);
+if (! -d $logdir)
+{
+  mkpath($logdir);
+}
+my $condorlogdir = sprintf("/tmp/OO_0_15fm/pass4_truthreco_nopileup/run%d",$runnumber);
+if (! -d $condorlogdir)
+{
+  mkpath($condorlogdir);
+}
+my $jobfile = sprintf("%s/condor%s.job",$logdir,$suffix);
+if (-f $jobfile && ! defined $overwrite)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile0 $infile1 $infile2 $infile3 $outfilename $dstoutdir $build $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold    = (NumJobStarts>=1 && JobStatus == 1 && !(ON_EVICT_CHECK_RequestMemory_REQUIREMENTS))\n";
+print F "request_memory = $memory\n";
+print F "retry_request_memory_increase = RequestMemory + 1000\n";
+print F "retry_request_memory_max = 10000MB\n";
+print F "batch_name = \"$batchname\"\n";
+print F "Priority = $baseprio\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile0,  $infile1, $infile2, $infile3, $outfilename, $dstoutdir, $build, $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio, $memory, $batchname\n";
+close(F);
