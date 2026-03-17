cd7f0a44b49f21e53445ab11888e929ba741b7a5
sPHENIX Production Account <pinkenburg@bnl.gov>
2026-03-17 16:31:57 -0400

    add JS_pp200_signal/pass3_global_dual_nopileup

---
+++ submit/JS_pp200_signal/pass3_global_dual_nopileup/rundir/run_pass3_global_dual_nopileup_js.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+export USER="$(id -u -n)"
+export LOGNAME=${USER}
+export HOME=/sphenix/u/${USER}
+
+hostname
+
+this_script=$BASH_SOURCE
+this_script=`readlink -f $this_script`
+this_dir=`dirname $this_script`
+echo rsyncing from $this_dir
+echo running: $this_script $*
+this_gitcommitid=`git show HEAD | sed -n 1p | cut -d " " -f 2`
+if [[ ! -z "$_CONDOR_SCRATCH_DIR" && -d $_CONDOR_SCRATCH_DIR ]]
+then
+    cd $_CONDOR_SCRATCH_DIR
+    rsync -av $this_dir/* .
+else
+    echo condor scratch NOT set
+    hostname
+    exit -1
+fi
+
+container_script=container_`basename $this_script`
+#singularity exec -B /home -B /direct/sphenix+u -B /gpfs02 -B /sphenix/u -B /sphenix/lustre01 -B /sphenix/user  -B /sphenix/sim /cvmfs/sphenix.sdcc.bnl.gov/singularity/rhic_sl7.sif ./$container_script $* $this_gitcommitid
+
+./$container_script $* $this_gitcommitid
+
+echo "wrapper script done"
