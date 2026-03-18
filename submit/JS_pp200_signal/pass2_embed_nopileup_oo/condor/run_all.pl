1bafdeb4fe286707560c93a32f9b94a046466661
sPHENIX Production Account <pinkenburg@bnl.gov>
2026-03-18 17:23:53 -0400

    add 12GeV jets

--- submit/JS_pp200_signal/pass2_embed_nopileup_oo/condor/run_all.pl
+++ submit/JS_pp200_signal/pass2_embed_nopileup_oo/condor/run_all.pl
@@ -19,7 +19,7 @@ my $test;
 GetOptions("build:s" => \$build, "events:i"=> \$outevents, "fm:s" =>\$fm, "increment"=>\$incremental, "overwrite"=> \$overwrite, "run:i" =>\$runnumber, "startsegment:i" => \$startsegment, "test"=>\$test);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Jet5\">, <\"Jet10\">, <\"Jet15\">, <\"Jet20\">, <\"Jet30\">, <\"Jet40\">, <\"Jet50\">, <\"Jet60\">, <\"Jet70\">, <\"PhotonJet\">, <\"PhotonJet5\">, <\"PhotonJet10\">, <\"PhotonJet20\">, <\"Detroit\"> production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Jet5\">, <\"Jet10\">, <\"Jet12\">, <\"Jet15\">, <\"Jet20\">, <\"Jet30\">, <\"Jet40\">, <\"Jet50\">, <\"Jet60\">, <\"Jet70\">, <\"PhotonJet\">, <\"PhotonJet5\">, <\"PhotonJet10\">, <\"PhotonJet20\">, <\"Detroit\"> production>\n";
     print "parameters:\n";
     print "--build: <ana build>\n";
     print "--fm : fermi range for embedding\n";
@@ -65,6 +65,7 @@ my $maxsubmit = $ARGV[0];
 my $jettrigger = $ARGV[1];
 if ($jettrigger  ne "Jet5" &&
     $jettrigger  ne "Jet10" &&
+    $jettrigger  ne "Jet12" &&
     $jettrigger  ne "Jet20" &&
     $jettrigger  ne "Jet15" &&
     $jettrigger  ne "Jet30" &&
