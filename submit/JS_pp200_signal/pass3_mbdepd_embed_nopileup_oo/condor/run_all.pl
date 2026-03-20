42d50bc93c630e0534efcdcda8c2450346ed754a
sPHENIX Production Account <pinkenburg@bnl.gov>
2026-03-20 14:25:44 -0400

    add JS_pp200_signal/pass3_mbdepd_embed_nopileup_oo

---
+++ submit/JS_pp200_signal/pass3_mbdepd_embed_nopileup_oo/condor/run_all.pl
@@ -0,0 +1,200 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $build;
+my $incremental;
+my $fm = "0_15fm";
+my $outevents = 0;
+my $overwrite;
+my $runnumber;
+my $shared;
+my $test;
+GetOptions("build:s" => \$build, "increment"=>\$incremental, "fm:s" =>\$fm, "overwrite"=> \$overwrite, "run:i" =>\$runnumber, "shared" => \$shared, "test"=>\$test);
+if ($#ARGV < 1)
+{
+    print "usage: run_all.pl <number of jobs> <\"Jet5\">, <\"Jet10\">, <\"Jet12\">, <\"Jet15\">, <\"Jet20\">, <\"Jet30\">, <\"Jet40\">, <\"Jet50\">, <\"Jet60\">, <\"Jet70\">, <\"PhotonJet\">, <\"PhotonJet5\">, <\"PhotonJet10\">, <\"PhotonJet20\">, <\"Detroit\"> production>\n";
+    print "parameters:\n";
+    print "--build: <ana build>\n";
+    print "--fm : fermi range for embedding\n";
+    print "--overwrite : overwrite exiting jobfiles\n";
+    print "--run: <runnumber>\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--test : dryrun - create jobfiles\n";
+    exit(1);
+}
+my $isbad = 0;
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /sphnxprod/)
+{
+    print "submit only from sphnxprod hosts\n";
+    $isbad = 1;
+}
+if (! defined $runnumber)
+{
+    print "need runnumber with --run <runnumber>\n";
+    $isbad = 1;
+}
+
+if (! defined $build)
+{
+    print "need build with --build <ana build>\n";
+    $isbad = 1;
+}
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    $isbad = 1;
+}
+
+if ($isbad > 0)
+{
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $jettrigger = $ARGV[1];
+if ($jettrigger  ne "Jet5" &&
+    $jettrigger  ne "Jet10" &&
+    $jettrigger  ne "Jet12" &&
+    $jettrigger  ne "Jet20" &&
+    $jettrigger  ne "Jet15" &&
+    $jettrigger  ne "Jet30" &&
+    $jettrigger  ne "Jet40" &&
+    $jettrigger  ne "Jet50" &&
+    $jettrigger  ne "Jet60" &&
+    $jettrigger  ne "Jet70" &&
+    $jettrigger  ne "PhotonJet" &&
+    $jettrigger  ne "PhotonJet5" &&
+    $jettrigger  ne "PhotonJet10" &&
+    $jettrigger  ne "PhotonJet20" &&
+    $jettrigger  ne "Detroit")
+{
+    print "second argument has to be Jet5, Jet10, Jet15, Jet20, Jet30, Jet40, Jet60, Jet70, PhotonJet, PhotonJet5, PhotonJet10, PhotonJet20 or Detroit\n";
+    exit(1);
+}
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+my $outfilelike = sprintf("pythia8_%s_sHijing_OO_%s",$jettrigger,$fm);
+
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$outdir = sprintf("%s/%s/run%04d/%s",$outdir,$fm,$runnumber,lc $jettrigger);
+if (! -d $outdir)
+{
+  mkpath($outdir);
+}
+
+my %mbdhash = ();
+my %truthhash = ();
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_BBC_G4HIT' and filename like '%$outfilelike-%' and runnumber = $runnumber order by filename") || die $DBI::errstr;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like '%$outfilelike-%' and runnumber = $runnumber");
+
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::errstr;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $mbdhash{sprintf("%06d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+
+$gettruthfiles->execute() || die $DBI::errstr;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    $truthhash{sprintf("%06d",$res[1])} = $res[0];
+}
+$gettruthfiles->finish();
+
+foreach my $segment (sort keys %mbdhash)
+{
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $mbdhash{$segment};
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+	my $outfilename = sprintf("DST_MBD_EPD_%s-%010d-%06d.root",$outfilelike,$runnumber,$segment);
+	$chkfile->execute($outfilename);
+	if ($chkfile->rows > 0)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+        elsif (defined $overwrite)
+	{
+	    $tstflag="--overwrite";
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %s %s %d %d %s %s", $outevents, $jettrigger, $lfn, $truthhash{sprintf("%06d",$segment)}, $outfilename, $outdir, $build, $runnumber, $segment,  $fm, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit >= 20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, exiting\n";
+	    last;
+	}
+    }
+}
+$chkfile->finish();
+$dbh->disconnect;
+
+my $jobfile = sprintf("condor.job");
+if (defined $shared)
+{
+ $jobfile = sprintf("condor.job.shared");
+}
+if (! -f $jobfile)
+{
+    print "could not find $jobfile\n";
+    exit(1);
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit $jobfile\n";
+    }
+    else
+    {
+	system("condor_submit $jobfile");
+    }
+}
---
+++ submit/JS_pp200_signal/pass3_mbdepd_embed_nopileup_oo/condor/run_condor.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use File::Path;
+use File::Basename;
+
+my $test;
+my $memory = sprintf("2500MB");
+my $overwrite;
+
+GetOptions("memory:s" => \$memory, "overwrite"=>\$overwrite, "test"=>\$test);
+if ($#ARGV < 8)
+{
+    print "usage: run_condor.pl <events> <jettrigger> <mbd infile> <truth infile> <outfile> <outdir> <build> <runnumber> <sequence> <fm range>\n";
+    print "options:\n";
+    print "--memory: memory requirement\n";
+    print "--overwrite : overwrite existing jobfiles\n";
+    print "-test: testmode - no condor submission\n";
+    exit(-2);
+}
+
+my $localdir=`pwd`;
+chomp $localdir;
+my $baseprio = 53;
+my $rundir = sprintf("%s/../rundir",$localdir);
+my $executable = sprintf("%s/run_pass3_mbdepd_embed_nopileup_oo_js.sh",$rundir);
+my $nevents = $ARGV[0];
+my $jettrigger = $ARGV[1];
+my $infile0 = $ARGV[2];
+my $infile1 = $ARGV[3];
+my $dstoutfile = $ARGV[4];
+my $dstoutdir = $ARGV[5];
+my $build = $ARGV[6];
+my $runnumber = $ARGV[7];
+my $sequence = $ARGV[8];
+my $fm = $ARGV[9];
+if ($sequence < 100)
+{
+    $baseprio = 90;
+}
+my $batchname = sprintf("%s %s",basename($executable),$jettrigger);
+my $condorlistfile = sprintf("condor.list");
+my $suffix = sprintf("%s-%010d-%06d",$jettrigger,$runnumber,$sequence);
+my $logdir = sprintf("%s/log/%s/run%d/%s",$localdir,$fm,$runnumber,$jettrigger);
+if (! -d $logdir)
+{
+  mkpath($logdir);
+}
+my $condorlogdir = sprintf("/tmp/JS_pp200_signal/pass3_mbdepd_embed_nopileup_oo/%s/run%d/%s",$fm,$runnumber,$jettrigger);
+if (! -d $condorlogdir)
+{
+  mkpath($condorlogdir);
+}
+my $jobfile = sprintf("%s/condor_%s.job",$logdir,$suffix);
+if (-f $jobfile && ! defined $overwrite)
+{
+    print "jobfile $jobfile exists, possible overlapping names\n";
+    exit(1);
+}
+my $condorlogfile = sprintf("%s/condor_%s.log",$condorlogdir,$suffix);
+if (-f $condorlogfile)
+{
+    unlink $condorlogfile;
+}
+my $errfile = sprintf("%s/condor_%s.err",$logdir,$suffix);
+my $outfile = sprintf("%s/condor_%s.out",$logdir,$suffix);
+print "job: $jobfile\n";
+open(F,">$jobfile");
+print F "Universe 	= vanilla\n";
+print F "Executable 	= $executable\n";
+print F "Arguments       = \"$nevents $infile0 $infile1 $dstoutfile $dstoutdir $jettrigger $build $runnumber $sequence\"\n";
+print F "Output  	= $outfile\n";
+print F "Error 		= $errfile\n";
+print F "Log  		= $condorlogfile\n";
+print F "Initialdir  	= $rundir\n";
+print F "PeriodicHold 	= (NumJobStarts>=1 && JobStatus == 1 && !(ON_EVICT_CHECK_RequestMemory_REQUIREMENTS))\n";
+print F "request_memory = $memory\n";
+print F "retry_request_memory_increase = RequestMemory + 2000\n";
+print F "retry_request_memory_max = 10000MB\n";
+print F "Priority = $baseprio\n";
+print F "batch_name = \"$batchname\"\n";
+print F "job_lease_duration = 3600\n";
+print F "Queue 1\n";
+close(F);
+#if (defined $test)
+#{
+#    print "would submit $jobfile\n";
+#}
+#else
+#{
+#    system("condor_submit $jobfile");
+#}
+
+open(F,">>$condorlistfile");
+print F "$executable, $nevents, $infile0 $infile1, $dstoutfile, $dstoutdir, $jettrigger,$build,  $runnumber, $sequence, $outfile, $errfile, $condorlogfile, $rundir, $baseprio, $memory, $batchname\n";
+close(F);
