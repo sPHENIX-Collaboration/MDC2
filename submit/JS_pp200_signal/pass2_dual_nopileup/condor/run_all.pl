29b8c2b24d7c640736ac99cccbe0371a4def9610
sPHENIX Production Account <pinkenburg@bnl.gov>
2026-04-07 17:43:17 -0400

    add 8 GeV jets to JS_pp200_signal dual

--- submit/JS_pp200_signal/pass2_dual_nopileup/condor/run_all.pl
+++ submit/JS_pp200_signal/pass2_dual_nopileup/condor/run_all.pl
@@ -25,7 +25,7 @@ my $test;
 GetOptions("build:s" => \$build, "disable_calo" => \$disable_calo, "disable_mbd" => \$disable_mbd, "disable_trk" => \$disable_trk, "increment"=>\$incremental, "overwrite" => \$overwrite, "run:i" =>\$runnumber, "shared" => \$shared, "test"=>\$test);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Jet5\", <\"Jet10\" <\"Jet12\", \"Jet15\", \"Jet20\", \"Jet30\", \"Jet40\", \"Jet50\", \"Jet60\", \"Jet70\", \"PhotonJet\", \"PhotonJet5\", \"PhotonJet10\", \"PhotonJet20\" or \"Detroit\" production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Jet5\", \"Jet8\", <\"Jet10\" <\"Jet12\", \"Jet15\", \"Jet20\", \"Jet30\", \"Jet40\", \"Jet50\", \"Jet60\", \"Jet70\", \"PhotonJet\", \"PhotonJet5\", \"PhotonJet10\", \"PhotonJet20\" or \"Detroit\" production>\n";
     print "parameters:\n";
     print "--build: <ana build>\n";
     print "--disable_calo: disable cal reconstruction\n";
@@ -74,6 +74,7 @@ if ($isbad > 0)
 my $maxsubmit = $ARGV[0];
 my $jettrigger = $ARGV[1];
 if ($jettrigger  ne "Jet5" &&
+    $jettrigger  ne "Jet8" &&
     $jettrigger  ne "Jet10" &&
     $jettrigger  ne "Jet12" &&
     $jettrigger  ne "Jet15" &&
@@ -89,7 +90,7 @@ if ($jettrigger  ne "Jet5" &&
     $jettrigger  ne "PhotonJet20" &&
     $jettrigger  ne "Detroit")
 {
-    print "second argument has to be Jet5, Jet10, Jet15, Jet20, Jet30, Jet40, Jet50, Jet60, Jet70, PhotonJet, PhotonJet5, PhotonJet10, PhotonJet20 or Detroit\n";
+    print "second argument has to be Jet5, Jet8, Jet10, Jet15, Jet20, Jet30, Jet40, Jet50, Jet60, Jet70, PhotonJet, PhotonJet5, PhotonJet10, PhotonJet20 or Detroit\n";
     exit(1);
 }
 
--- submit/JS_pp200_signal/pass2jet_dual_nopileup/condor/run_all.pl
+++ submit/JS_pp200_signal/pass2jet_dual_nopileup/condor/run_all.pl
@@ -20,7 +20,7 @@ my $verbosity = 0;
 GetOptions("build:s" => \$build, "increment"=>\$incremental, "memory:s"=>\$memory, "overwrite" => \$overwrite, "run:i" =>\$runnumber, "shared" => \$shared, "test"=>\$test, "verbosity:i" => \$verbosity);
 if ($#ARGV < 1)
 {
-    print "usage: run_all.pl <number of jobs> <\"Jet5\">, \"Jet10\">, \"Jet12\">, <\"Jet15\">, <\"Jet30\">, <\"Jet40\">, <\"Jet50\">, <\"Jet60\">, <\"Jet70\">, \"PhotonJet\">, \"PhotonJet5\">, \"PhotonJet10\">, \"PhotonJet20\">, \"Detroit\">  production>\n";
+    print "usage: run_all.pl <number of jobs> <\"Jet5\">, \"Jet8\", \"Jet10\">, \"Jet12\">, <\"Jet15\">, <\"Jet30\">, <\"Jet40\">, <\"Jet50\">, <\"Jet60\">, <\"Jet70\">, \"PhotonJet\">, \"PhotonJet5\">, \"PhotonJet10\">, \"PhotonJet20\">, \"Detroit\">  production>\n";
     print "parameters:\n";
     print "--build: <ana build>\n";
     print "--increment : submit jobs while processing running\n";
@@ -67,6 +67,7 @@ if ($isbad > 0)
 my $maxsubmit = $ARGV[0];
 my $jettrigger = $ARGV[1];
 if ($jettrigger  ne "Jet5" &&
+    $jettrigger  ne "Jet8" &&
     $jettrigger  ne "Jet10" &&
     $jettrigger  ne "Jet12" &&
     $jettrigger  ne "Jet15" &&
@@ -82,7 +83,7 @@ if ($jettrigger  ne "Jet5" &&
     $jettrigger  ne "PhotonJet20" &&
     $jettrigger  ne "Detroit")
 {
-    print "second argument has to be Jet5, Jet10, Jet15, Jet20, Jet30, Jet40, Jet50, Jet60, Jet70, PhotonJet, PhotonJet5, PhotonJet10, PhotonJet20 or Detroit\n";
+    print "second argument has to be Jet5, Jet8, Jet10, Jet15, Jet20, Jet30, Jet40, Jet50, Jet60, Jet70, PhotonJet, PhotonJet5, PhotonJet10, PhotonJet20 or Detroit\n";
      exit(1);
 }
 
