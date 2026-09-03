1ae15f686e3041254b1edc487913720907929939
sphnxpro <none>
2026-09-03 09:00:41 -0400

    add Herwig/pass3trk

---
+++ submit/Herwig/pass3trk/condor/run_all.pl
@@ -0,0 +1,242 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use File::Path;
+use File::Basename;
+use Getopt::Long;
+use DBI;
+
+
+my $build;
+my $incremental;
+my $memory;
+my $outevents = 0;
+my $overwrite;
+my $runnumber;
+my $shared;
+my $test;
+my $pileup;
+my $verbosity = 0;
+GetOptions("build:s" => \$build, "events:i"=>\$outevents, "increment"=>\$incremental, "memory:s"=>\$memory, "overwrite"=> \$overwrite, "pileup:s" => \$pileup, "run:i" =>\$runnumber, "shared" => \$shared, "test"=>\$test, "verbosity:i" => \$verbosity);
+if ($#ARGV < 1)
+{
+    print "usage: run_all.pl <number of jobs> <\"Jet5\", \"Jet12\", \"Jet20\", \"Jet30\", \"Jet40\", \"Jet50\", \"MB\", \"PhotonJet5\", \"PhotonJet10\", \"PhotonJet20\" production>\n";
+    print "parameters:\n";
+    print "--build: <ana build>\n";
+    print "--events: number of events\n";
+    print "--increment : submit jobs while processing running\n";
+    print "--overwrite : overwrite exiting jobfiles\n";
+    print "--pileup : collision rate (with unit, kHz, MHz)\n";
+    print "--shared : submit jobs to shared pool\n";
+    print "--test : dryrun - create jobfiles\n";
+    print "--verbosity <level>: verbosity level\n";
+    exit(1);
+}
+
+my $isbad = 0;
+
+my $hostname = `hostname`;
+chomp $hostname;
+if ($hostname !~ /sphnxprod/)
+{
+    print "submit only from sphnxprod hosts\n";
+    $isbad = 1;
+}
+if (! defined $pileup)
+{
+    print "need pileup with --pileup <rate with unit> (kHz, MHz)\n";
+    $isbad = 1;
+}
+
+if (! defined $runnumber)
+{
+    print "need runnumber with --run <runnumber>\n";
+    $isbad = 1;
+}
+
+if (! defined $build)
+{
+    print "need build with --build <ana build>\n";
+    $isbad = 1;
+}
+if (! -f "outdir.txt")
+{
+    print "could not find outdir.txt\n";
+    $isbad = 1;
+}
+
+if ($isbad > 0)
+{
+    exit(1);
+}
+
+my $maxsubmit = $ARGV[0];
+my $jettrigger = $ARGV[1];
+if ($jettrigger  ne "Jet5" &&
+    $jettrigger  ne "Jet12" &&
+    $jettrigger  ne "Jet20" &&
+    $jettrigger  ne "Jet30" &&
+    $jettrigger  ne "Jet40" &&
+    $jettrigger  ne "Jet50" &&
+    $jettrigger  ne "MB" &&
+    $jettrigger  ne "PhotonJet5" &&
+    $jettrigger  ne "PhotonJet10" &&
+    $jettrigger  ne "PhotonJet20")
+{
+    print "second argument has to be Jet5, Jet12, Jet20, Jet30, Jet40, Jet50, MB, PhotonJet5, PhotonJet10 or PhotonJet20\n";
+    exit(1);
+}
+
+my $condorlistfile =  sprintf("condor.list");
+if (-f $condorlistfile)
+{
+    unlink $condorlistfile;
+}
+
+my $outdir = `cat outdir.txt`;
+chomp $outdir;
+$jettrigger = sprintf("%s_%s",$jettrigger,$pileup);
+$outdir = sprintf("%s/run%04d/%s",$outdir,$runnumber,lc $jettrigger);
+if (! -d $outdir)
+{
+  mkpath($outdir);
+}
+
+my %outfiletype = ();
+$outfiletype{"DST_TRKR_HIT"} = 1;
+$outfiletype{"DST_TRUTH"} = 1;
+
+my %trkhash = ();
+my %truthhash = ();
+
+my $dbh = DBI->connect("dbi:ODBC:FileCatalog","phnxrc") || die $DBI::errstr;
+$dbh->{LongReadLen}=2000; # full file paths need to fit in here
+my $getfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRKR_G4HIT' and filename like 'DST_TRKR_G4HIT_Herwig_$jettrigger-%' and runnumber = $runnumber order by segment") || die $DBI::errstr;
+my $chkfile = $dbh->prepare("select lfn from files where lfn=?") || die $DBI::errstr;
+my $gettruthfiles = $dbh->prepare("select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like 'DST_TRUTH_G4HIT_Herwig_$jettrigger-%' and runnumber = $runnumber");
+if ($verbosity > 0)
+{
+  print "select filename,segment from datasets where dsttype = 'DST_TRKR_G4HIT' and filename like 'DST_TRKR_G4HIT_Herwig_$jettrigger-%' and runnumber = $runnumber order by segment\n";
+  print "select filename,segment from datasets where dsttype = 'DST_TRUTH_G4HIT' and filename like 'DST_TRUTH_G4HIT_Herwig_$jettrigger-%' and runnumber = $runnumber\n";
+}
+my $nsubmit = 0;
+$getfiles->execute() || die $DBI::errstr;
+my $ncal = $getfiles->rows;
+while (my @res = $getfiles->fetchrow_array())
+{
+    $trkhash{sprintf("%06d",$res[1])} = $res[0];
+}
+$getfiles->finish();
+$gettruthfiles->execute() || die $DBI::errstr;
+my $ntruth = $gettruthfiles->rows;
+while (my @res = $gettruthfiles->fetchrow_array())
+{
+    $truthhash{sprintf("%06d",$res[1])} = $res[0];
+}
+$gettruthfiles->finish();
+if ($verbosity > 0)
+{
+  print "input files, tracks: $ncal, truth: $ntruth\n";
+}
+foreach my $segment (sort { $a <=> $b } keys %trkhash)
+{
+    if (! exists $truthhash{$segment})
+    {
+	next;
+    }
+
+    my $lfn = $trkhash{$segment};
+    if ($verbosity > 1)
+    {
+	print "found $lfn\n";
+    }
+    if ($lfn =~ /(\S+)-(\d+)-(\d+).*\..*/ )
+    {
+	my $runnumber = int($2);
+	my $segment = int($3);
+        my $foundall = 1;
+	foreach my $type (sort keys %outfiletype)
+	{
+            my $lfn =  sprintf("%s_Herwig_%s-%010d-%06d.root",$type,$jettrigger,$runnumber,$segment);
+	    $chkfile->execute($lfn);
+	    if ($chkfile->rows > 0)
+	    {
+		next;
+	    }
+	    else
+	    {
+		$foundall = 0;
+		if ($verbosity > 0)
+		{
+		    print "did not find $lfn\n";
+		}
+		last;
+	    }
+	}
+	if ($foundall == 1)
+	{
+	    next;
+	}
+	my $tstflag="";
+	if (defined $test)
+	{
+	    $tstflag="--test";
+	}
+	if (defined $memory)
+	{
+	    $tstflag=sprintf("%s %s",$tstflag,$memory);
+	}
+        if (defined $overwrite)
+	{
+	    $tstflag= sprintf("%s --overwrite",$tstflag);
+	}
+	my $subcmd = sprintf("perl run_condor.pl %d %s %s %s %s %s %d %d %s", $outevents, $jettrigger, $lfn, $truthhash{sprintf("%06d",$segment)}, $outdir, $build, $runnumber, $segment, $tstflag);
+	print "cmd: $subcmd\n";
+	system($subcmd);
+	my $exit_value  = $? >> 8;
+	if ($exit_value != 0)
+	{
+	    if (! defined $incremental)
+	    {
+		print "error from run_condor.pl\n";
+		exit($exit_value);
+	    }
+	}
+	else
+	{
+	    $nsubmit++;
+	}
+	if (($maxsubmit != 0 && $nsubmit >= $maxsubmit) || $nsubmit > 20000)
+	{
+	    print "maximum number of submissions $nsubmit reached, exiting\n";
+	    last;
+	}
+    }
+}
+
+$chkfile->finish();
+$dbh->disconnect;
+
+my $jobfile = sprintf("condor.job");
+if (defined $shared)
+{
+ $jobfile = sprintf("condor.job.shared");
+}
+if (! -f $jobfile)
+{
+    print "could not find $jobfile\n";
+    exit(1);
+}
+
+if (-f $condorlistfile)
+{
+    if (defined $test)
+    {
+	print "would submit $jobfile\n";
+    }
+    else
+    {
+	system("condor_submit $jobfile");
+    }
+}
